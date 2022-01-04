5e9933ae6173969450211d41b54aaf3e706c8f44
Anand Tripathi <shadarsh23@gmail.com>
2022-01-04 07:19:21 +0000

    Time: 0 ms (100.00%), Space: 5.9 MB (46.64%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -4,8 +4,9 @@ public:
         int count = 0;
         while(n>0)
         {
-            count++;
-            n = n&(n-1);
+            if((n&1) !=0 )
+                count++;
+            n = n>> 1;
         }
         return count;
     }
