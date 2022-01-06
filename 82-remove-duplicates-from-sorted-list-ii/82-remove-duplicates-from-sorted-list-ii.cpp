d66029abc4e719db9c8f02b8f388bc7d1e5dd9d7
Anand Tripathi <shadarsh23@gmail.com>
2022-01-06 06:01:11 +0000

    Time: 10 ms (24.80%), Space: 11.2 MB (57.34%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -1,47 +1,24 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        if(head==NULL)
-            return head;
-        ListNode *dummy = new ListNode(-101);
-        dummy->next = head;
-        ListNode *prev = dummy;
-        ListNode *curr = head;
-        ListNode *right = head->next;
+    ListNode *deleteDuplicates(ListNode *head) {
+        ListNode **runner = &head;
         
-        while(right)
+        if(!head || !head->next)return head;
+        
+        while(*runner)
         {
-            if(right->val == curr->val)
+            if((*runner)->next && (*runner)->next->val == (*runner)->val)
             {
-                int val = curr->val;
-                while(curr && curr->val==val)
-                {
-                    ListNode* temp = curr;
-                    curr = right;
-                    prev->next = curr;
-                    delete temp;
-                    if(right)
-                    right = right->next;
-                }
+                ListNode *temp = *runner;
+                while(temp && (*runner)->val == temp->val)
+                    temp = temp->next;
                 
+                *runner = temp;
             }
             else
-            {
-            prev = prev->next;
-            curr = curr->next;
-            right = right->next;
-            }
+                runner = &((*runner)->next);
         }
-        return dummy->next;
+        
+        return head;
     }
 };
