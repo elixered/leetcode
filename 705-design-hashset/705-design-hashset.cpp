3bf6488838b602d5d762c8b8812940ab5f7b669c
Anand Tripathi <shadarsh23@gmail.com>
2022-01-06 07:42:51 +0000

    Time: 157 ms (28.64%), Space: 68.6 MB (26.55%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,62 +1,85 @@
-class Node {
-public:
-    int data;
-    Node* next;
-    
-    Node(int data) {
-        this->data = data;
-        next = nullptr;
-    }    
-};
-
 class MyHashSet {
-    Node* head;
 public:
+    struct node{
+        int val;
+        node *next;
+        node(int data)
+        {
+            val = data;
+        }
+    };
+    vector<node*> hm;
+    int sz = 100007;
     MyHashSet() {
-        head = nullptr;
+        hm = vector<node*>(sz,NULL);
     }
     
     void add(int key) {
-        Node* new_node = new Node(key);
-        new_node->next = head;
-        head = new_node;
-    }
-    
-    void remove(int key) {
-        bool isKeyPresent = contains(key);
-        if(isKeyPresent == false)
-            return;
-        
-        // Delete the key.
-        Node* dummy = new Node(1);
-        dummy->next = head;
-        
-        Node* temp = head, *prev = dummy;
-        // Remove all the occurences of key
-        while(temp != nullptr) {
-            if(temp->data == key) {
-                prev->next = temp->next;
-                Node* curr = temp;
-                temp = temp->next;
-                delete curr;
-            }
-            else {
+        int hash = key%sz;
+        node *newnode = new node(key);
+        if(hm[hash]==NULL)
+        {
+            hm[hash] = newnode;
+        }
+        else
+        {
+            node *temp = hm[hash];
+            node *prev = NULL;
+            while(temp)
+            {
+                if(temp->val==key)
+                    return;
                 prev = temp;
                 temp = temp->next;
             }
+            prev->next = newnode;
         }
-
-        head = dummy->next;
+    }
+    
+    void remove(int key) {
+        int hash = key%sz;
+        if(contains(key)==false)
+            return;
+        node *prev = NULL;
+        node *curr = hm[hash];
+        if(curr && curr->val==key)
+        {
+            node* temp = curr;
+            curr = curr->next;
+            delete temp;
+            hm[hash] = NULL;
+            return;
+        }
+        else
+        {
+            while(curr && curr->val!=key)
+            {
+                prev = curr;
+                curr  = curr->next;
+            }
+            node* temp = curr;
+            prev->next = curr->next;
+            delete temp;
+            return;
+        }
     }
     
     bool contains(int key) {
-        Node* temp = head;
-        while(temp != nullptr) {
-            if(temp->data == key)
+        int hash = key%sz;
+        node* curr = hm[hash];
+        while(curr)
+        {
+            if(curr->val==key)
                 return true;
-            temp = temp->next;
         }
-        
         return false;
     }
 };
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
