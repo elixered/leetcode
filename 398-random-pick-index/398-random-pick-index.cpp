b794878630e1f2721a60754dc34bd671cf59e9c9
Anand Tripathi <shadarsh23@gmail.com>
2022-01-07 10:18:31 +0000

    Time: 1052 ms (5.01%), Space: 35.6 MB (85.29%) - LeetHub

--- 398-random-pick-index/398-random-pick-index.cpp
+++ 398-random-pick-index/398-random-pick-index.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
-    unordered_map<int,vector<int>> map;
+    vector<int> nums;
     Solution(vector<int>& nums) {
         for(int i=0; i<nums.size(); i++)
-            map[nums[i]].push_back(i);
+            this->nums = nums;
     }
     
     int pick(int target) {
-        int i = rand()%map[target].size();
-        return map[target][i];
+        int count = 0;
+        int n = nums.size();
+        int ans = -1;
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]!=target)
+                continue;
+            count++;
+            int pb = rand()%count;if(pb==0)
+                ans = i;
+        }
+        return ans;
     }
 };
 
