7f709f1cd7e3a571d495e284d9ef25767f8078f0
Anand Tripathi <shadarsh23@gmail.com>
2022-01-08 14:18:40 +0530

    Time: 25 ms, Memory: 24 MB - LeetHub

--- product-of-array-except-self/product-of-array-except-self.cpp
+++ product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,23 +4,18 @@ public:
         int n = nums.size();
         if(n<=1)
             return nums;
-        vector<int> prefix(n),suffix(n);
-        prefix[0] = nums[0];
-        suffix[n-1] = nums[n-1];
-        for(int i=1; i<n; i++)
-            prefix[i] = nums[i]*prefix[i-1];
-        for(int i=n-2; i>=0; i--)
-            suffix[i] = nums[i]*suffix[i+1];
+        int res = 1;
         vector<int> ans(n);
         for(int i=0; i<n; i++)
         {
-            int curr = 1;
-            if(i-1>=0)
-                curr*=prefix[i-1];
-            if(i+1<n)
-                curr *= suffix[i+1];
-            ans[i] = curr;
-                
+            ans[i] = res;
+            res *= nums[i];
+        }
+        res = 1;
+        for(int i=n-1; i>=0; i--)
+        {
+            ans[i] *= res;
+            res *= nums[i];
         }
         return ans;
     }
