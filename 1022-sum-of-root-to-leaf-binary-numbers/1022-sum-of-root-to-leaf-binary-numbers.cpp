d83565f36d312df9bb63526008fd37c46c7d888b
Anand Tripathi <shadarsh23@gmail.com>
2022-01-11 10:37:36 +0530

    Time: 15 ms (14.81%), Space: 16.7 MB (72.35%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -1,41 +1,17 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void solve(TreeNode* root, string& s, int& ans)
-    {
-        if(root==NULL)
-            return;
-        s += to_string(root->val);
-        if(root->left==NULL && root->right==NULL)
-        {
-            int res = 0;
-            int n = s.size();
-            for(int i=s.size()-1; i>=0; i--)
-            {
-                if(s[i]=='1')
-                res += pow(2,n-i-1);
-            }
-            ans+=res;
-        }
-        solve(root->left,s,ans);
-        solve(root->right,s,ans);
-        s.pop_back();
-        return;
+    int sumRootToLeafHelper(TreeNode* rt, int cur_val) {
+        if(! rt) return 0;
+        
+        cur_val = (cur_val << 1) | rt->val;
+        
+        if(!rt->left && !rt->right)
+			return cur_val;
+			
+        return sumRootToLeafHelper(rt->left, cur_val) + sumRootToLeafHelper(rt->right, cur_val);
     }
-    int sumRootToLeaf(TreeNode* root) {
-        string s = "";
-        int ans = 0;
-        solve(root,s,ans);
-        return ans;
+    
+    int sumRootToLeaf(TreeNode* rt) {
+        return sumRootToLeafHelper(rt, 0);
     }
 };
