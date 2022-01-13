bfd16ff59a37cdc0de70d6961f39ec4da8f51262
Anand Tripathi <shadarsh23@gmail.com>
2022-01-13 14:02:21 +0530

    Time: 5 ms (18.32%), Space: 7.1 MB (83.08%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        int n = nums.size();
-        int numSubSets = 1<<n;
-        for(int bitmask=0; bitmask<numSubSets; bitmask++)
+    vector<vector<int>> ans;
+    void backtrack(vector<int>& nums, vector<int>& temp, int start)
+    {
+        ans.push_back(temp);
+        for(int i=start; i<nums.size(); i++)
         {
-            vector<int> temp;
-            for(int i=0; i<n; i++)
-            {
-                if((bitmask&(1<<i))!=0)
-                    temp.push_back(nums[i]);
-            }
-            ans.push_back(temp);
+            temp.push_back(nums[i]);
+            backtrack(nums,temp,i+1);
+            temp.pop_back();
         }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        vector<int> temp;
+        backtrack(nums,temp,0);
         return ans;
     }
 };
