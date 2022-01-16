adcf46a29412cc30d3236cd244fb5756e1f88e41
Anand Tripathi <shadarsh23@gmail.com>
2022-01-16 10:58:51 +0530

    Time: 16 ms (48.56%), Space: 17.9 MB (11.79%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -4,25 +4,22 @@ public:
         int n = seats.size();
         vector<int> left(n),right(n);
         int prev = -n;
-        for(int i=0; i<n; i++)
-        {
-            if(seats[i]==1)
-                prev = i;
-
-            left[i] = i-prev;
-        }
-        prev = 2*n;
-        for(int i=n-1; i>=0; i--)
-        {
-            if(seats[i]==1)
-                prev = i;
-            else right[i] = prev-i;
-        }
+        int next=0;
         int ans = 0;
         for(int i=0; i<n; i++)
         {
-            ans = max(ans,min(left[i],right[i]));
+            if(seats[i]==1)
+                prev = i;
+            else
+            {
+                while((next<n && seats[next]==0) or next<i)
+                    next++;
+            }
+            if(next>=n)
+                next = 2*n;
+            ans = max(ans,min(next-i,i-prev));
         }
+        
         return ans;
     }
 };
