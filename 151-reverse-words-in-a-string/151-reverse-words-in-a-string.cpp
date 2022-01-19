bae837e1c9db15cd24aeae5806a6fc37789a14cc
Anand Tripathi <shadarsh23@gmail.com>
2022-01-19 11:35:58 +0530

    Time: 19 ms (12.90%), Space: 20.7 MB (27.63%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -4,9 +4,8 @@ public:
         stringstream ss(s);
         string word;
         string ans;
-        while(getline(ss,word,' '))
+        while(ss>>word)
         {
-            if(word.size()==0) continue;
             ans = word+" "+ans;
         }
         ans.pop_back();
