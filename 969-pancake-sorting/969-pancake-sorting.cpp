d0642bb3782369bb63b7902dad90a398f9b97053
Anand Tripathi <shadarsh23@gmail.com>
2022-01-19 10:51:08 +0530

    Time: 4 ms (77.71%), Space: 11.3 MB (27.33%) - LeetHub

--- 969-pancake-sorting/969-pancake-sorting.cpp
+++ 969-pancake-sorting/969-pancake-sorting.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     
-    bool sorted(vector<int> arr)
-    {
-        int n = arr.size();
-        for(int i=0; i<n; i++)
-            if(arr[i]!=i+1)
-                return false;
-        return true;
-    }
+ 
     
     vector<int> pancakeSort(vector<int>& arr) {
         int n = arr.size();
@@ -19,8 +12,6 @@ public:
             {
                 if(arr[i]==x)
                 {
-                    if(sorted(arr))
-                        return ans;
                     ans.push_back(i+1);
                     ans.push_back(x);
                     reverse(arr.begin(),arr.begin()+i+1);
