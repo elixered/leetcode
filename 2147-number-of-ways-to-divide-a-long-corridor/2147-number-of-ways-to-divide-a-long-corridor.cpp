99057f060179c054631b9a01d3d55d5fb1db77b7
Anand Tripathi <shadarsh23@gmail.com>
2022-01-22 22:04:57 +0530

    Time: 250 ms (12.50%), Space: 55.7 MB (50.00%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -1,45 +1,27 @@
 class Solution {
 public:
-    int memo[100001][5];
-    int solve(string& s, int start, int curr)
-    {
-        int mod = pow(10,9)+7;
-        int n = s.size();
-        if(curr>2) return 0;
-        if(start==n-1)
-        {
-            curr+=(s[start]=='S'?1:0);
-            if(curr==2)
-                return 1;
-            else return 0;
-        }
-        if(memo[start][curr]!=-1) return memo[start][curr];
-        int ans;
-        if(curr<2)
-        {
-            ans = solve(s,start+1,curr+(s[start]=='S'?1:0))%mod;
-        }
-        else
-        if(curr==2)
-        {
-            if(s[start]=='P')
-                ans = (solve(s,start+1,curr)%mod+solve(s,start+1,0)%mod)%mod;
-            else
-            {
-                ans = solve(s,start+1,0+s[start]=='S'?1:0);
-            }
-                
-        }
-        return memo[start][curr] = ans;
-    }
+    int mod = 1e9 + 7;
     
-
-    int numberOfWays(string s) {
-        int n = s.size();
-        memset(memo,-1,sizeof(memo));
-  
-        if(n<2) return 0;
-       return solve(s,0,0);
-       
+    int numberOfWays(string corridor) {
+        vector<int> pos;
+        for (int i = 0; i < corridor.size(); i++) {
+            if (corridor[i] == 'S') {
+                pos.push_back(i);
+            }
+        }
+        
+        if (pos.size() % 2 || pos.size() == 0)
+            return 0;
+        
+        long long ans = 1;
+        int prev = pos[1];
+        for (int i = 2; i < pos.size(); i += 2) {
+            int length = pos[i] - prev;
+            ans = (ans * length) % mod;
+            
+            prev = pos[i + 1];
+        }
+        
+        return ans;
     }
 };
