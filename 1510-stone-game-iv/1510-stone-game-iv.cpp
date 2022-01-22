c722cac3b9ebff361de94c95486585c0bef61052
Anand Tripathi <shadarsh23@gmail.com>
2022-01-22 10:20:06 +0530

    Time: 265 ms (24.83%), Space: 7.9 MB (51.01%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,37 +1,15 @@
 class Solution {
 public:
-    
-    vector<vector<int>> memo;
-    
-    bool g(int n, int pl)
-    {
-        if(n==0)
-            return pl==0?false:true;
-        if(memo[n][pl]!=-1) return memo[n][pl];
-        int np = (pl==0 ? 1:0);
-        if(pl==0)
-        {
-            for(int i=1; i*i<=n; i++)
-            {
-                if(g(n-i*i,np)==true)
-                    return memo[n][pl] = true;
-            }
-            return memo[n][pl] = false;
-        }
-        if(pl==1)
-        {
-            for(int i=1; i*i<=n; i++)
-            {
-                if(g(n-i*i,np)==false)
-                    return memo[n][pl] = false;
-            }
-            return memo[n][pl] = true;
-        }
-        return memo[n][pl] = false;
-    }
-    
     bool winnerSquareGame(int n) {
-        memo = vector<vector<int>>(n+1,vector<int>(2,-1));
-        return g(n,0);
+        vector<int> dp(n+1,0);
+        for(int i=1; i<=n; i++)
+        {
+            for(int k=1; k*k<=i; k++)
+            {
+                if(dp[i-k*k]==false)
+                    dp[i] = true;
+            }
+        }
+        return dp[n];
     }
 };
