c996e445535ba33598dfd25e346593067d577736
Anand Tripathi <shadarsh23@gmail.com>
2022-01-22 10:25:05 +0530

    Time: 88 ms (49.33%), Space: 12.2 MB (26.17%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
-    bool winnerSquareGame(int n) {
-        vector<int> dp(n+1,0);
-        for(int i=1; i<=n; i++)
+    vector<int> dp;
+    bool solve(int n)
+    {
+        if(n==0)
+            return false;
+        if(n==1)
+            return true;
+        if(dp[n]!=-1) return dp[n];
+        for(int i=1; i*i<=n; i++)
         {
-            for(int k=1; k*k<=i; k++)
-            {
-                if(dp[i-k*k]==false)
-                    dp[i] = true;
-            }
+            if(solve(n-i*i)==false)
+                return dp[n]= true;
         }
-        return dp[n];
+        return dp[n]= false;
+    }
+    bool winnerSquareGame(int n) {
+        dp = vector<int>(n+1,-1);
+        return solve(n);
     }
 };
