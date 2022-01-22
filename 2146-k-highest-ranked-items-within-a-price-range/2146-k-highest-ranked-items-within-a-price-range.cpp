bdbac48a25bdd1ac0ffebf919d30f871d9c7a906
Anand Tripathi <shadarsh23@gmail.com>
2022-01-22 22:03:19 +0530

    Time: 1931 ms (5.88%), Space: 222.2 MB (5.88%) - LeetHub

--- 2146-k-highest-ranked-items-within-a-price-range/2146-k-highest-ranked-items-within-a-price-range.cpp
+++ 2146-k-highest-ranked-items-within-a-price-range/2146-k-highest-ranked-items-within-a-price-range.cpp
@@ -1,76 +1,97 @@
 class Solution {
 public:
 
-    class node{
-        public:
-        int dist;
-        int price;
-        int row;
-        int col;
-        node(int a, int b, int c,int d)
-        {
-            dist=a;
-            price=b;
-            row=c;
-            col=d;
+struct node{
+    int dist; // distance of current cell from start
+    int cost; // price at current cell
+    int r, c; // row and column of current cell
+};
+
+// storing least desired cell at top (max. Heap)
+struct compare{
+    bool operator()(const node &a, const node &b){
+        if(a.dist != b.dist){
+            return a.dist < b.dist;
+        }else if(a.cost != b.cost){
+            return a.cost < b.cost;
+        }else if(a.r != b.r){
+            return a.r < b.r;
         }
-    };
-    struct cmp{
-        bool operator()(node& a, node& b)
-        {
-            if(a.dist<b.dist) return true;
-            else if(a.dist>b.dist) return false;
-            else if(a.price<b.price) return true;
-            else if(a.price>b.price) return false;
-            else if(a.row<b.row) return true;
-            else if(a.row>b.row) return false;
-            else if(a.col<b.col) return true;
-            else return false;
-        }
-    };
-    priority_queue<node,vector<node>,cmp> pq;
-    vector<vector<int>> highestRankedKItems(vector<vector<int>>& grid, vector<int>& pricing, vector<int>& start, int k) {
-        vector<vector<int>> ans;
-        vector<int> dirx = {0,0,-1,1};
-        vector<int> diry = {-1,1,0,0};
-        if(grid[start[0]][start[1]]==0)
-            return ans;
-        int d = 0;
-        queue<vector<int>> q;
-        q.push(start);
-        if(grid[start[0]][start[1]]>1 && grid[start[0]][start[1]]>=pricing[0] &&  grid[start[0]][start[1]]<=pricing[1])
-            pq.push(node(d,grid[start[0]][start[1]],start[0],start[1]));
-         grid[start[0]][start[1]] = -1;
-        int m = grid.size();
-        int n = grid[0].size();
-        while(!q.empty())
-        {
-            d++;
-            int z = q.size();
-            while(z--)
-            {
-            auto it = q.front();
-            q.pop();
-            for(int i=0; i<4; i++)
-            {
-                int x = it[0]+dirx[i];
-                int y = it[1]+diry[i];
-                if(x<0 or y<0 or x>=m or y>=n or grid[x][y]<=0) continue;
-                q.push({x,y});
-                if(grid[x][y]>=pricing[0] && grid[x][y]<=pricing[1] && grid[x][y]!=1)
-                pq.push(node(d,grid[x][y],x,y));
-                if(pq.size()>k) pq.pop();
-                grid[x][y] = -1;
-            }
-            }
-        }
-        while(!pq.empty())
-        {
-            auto it = pq.top();
-            ans.push_back({it.row,it.col});
-            pq.pop();
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        return a.c < b.c;
     }
+};
+
+vector<vector<int>> highestRankedKItems(vector<vector<int>> &grid, vector<int> &price, vector<int> &start, int k) {
+    int n=grid.size(), m=grid[0].size();
+    
+    int dxy[4][2]={{-1,0},{0,1},{1,0},{0,-1}};
+    
+    queue<pair<int,int>> q;
+    q.push({start[0],start[1]});
+    int len=0;
+    
+    set<pair<int,int>> visited; // to keep track of visited cells
+    visited.insert({start[0],start[1]});
+    
+    int lb=price[0], ub=price[1];
+    
+    priority_queue<node,vector<node>,compare> pq;
+    
+    while(q.size()>0){
+        int size=q.size();
+        while(size--){
+            pair<int,int> p=q.front();
+            q.pop();
+            
+            int i=p.first, j=p.second;
+            if(grid[i][j]!=1){
+                int curCost=grid[i][j];
+                if(curCost>=lb && curCost<=ub){
+                    if(pq.size()<k){
+                        pq.push({len,curCost,i,j});
+                    }else if(pq.size()==k){ // checking if possible to replace with better ranked cell
+                        struct node tp=pq.top();
+                        if(tp.dist > len){
+                            pq.pop();
+                            pq.push({len,curCost,i,j});
+                        }else if(tp.dist==len){
+                            if(tp.cost > curCost){
+                                pq.pop();
+                                pq.push({len,curCost,i,j});
+                            }else if(tp.cost==curCost){
+                                if(tp.r > i){
+                                    pq.pop();
+                                    pq.push({len,curCost,i,j});
+                                }else if(tp.r==i && tp.c > j){
+                                    pq.pop();
+                                    pq.push({len,curCost,i,j});
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+			// moving in adjecent cells
+            for(int k=0;k<4;k++){
+                int x=i+dxy[k][0], y=j+dxy[k][1];
+                if(x>=0 && y>=0 && x<n && y<m && visited.find({x,y})==visited.end() && grid[x][y]!=0){
+                    visited.insert({x,y});
+                    q.push({x,y});
+                }
+            }
+        }
+        len++;
+    }
+    vector<vector<int>> ans;
+    while(pq.size()>0){
+        struct node cur=pq.top();
+        pq.pop();
+        
+        ans.push_back({cur.r,cur.c});
+        k--;
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
 };
