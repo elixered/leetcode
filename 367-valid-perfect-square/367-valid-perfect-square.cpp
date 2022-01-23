8eebd0ac440854ffbba0fcf15675d4844f1d349e
Anand Tripathi <shadarsh23@gmail.com>
2022-01-23 07:59:39 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (64.07%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        for(long i=1; i*i<=num; i++)
+        {
+            if(i*i == num)
+                return true;
+        }
+        return false;
+    }
+};
