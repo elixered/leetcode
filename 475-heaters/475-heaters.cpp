db954ac0c2d3d04fc5f21d1b473d725dce3c7e63
Anand Tripathi <shadarsh23@gmail.com>
2022-01-28 12:36:18 +0530

    Time: 124 ms (18.81%), Space: 25.4 MB (54.39%) - LeetHub

---
+++ 475-heaters/475-heaters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        sort(houses.begin(),houses.end());
+        sort(heaters.begin(),heaters.end());
+        int ans = 0;
+        for(auto it:houses)
+        {
+            int idx = lower_bound(heaters.begin(),heaters.end(),it)-heaters.begin();
+            int res = INT_MAX;
+            if(idx!=heaters.size())
+                res = min(res,abs(heaters[idx]-it));
+            idx--;
+            if(idx>=0)
+                res = min(res,abs(heaters[idx]-it));
+            ans = max(ans,res);
+        }
+        return ans;
+    }
+};
