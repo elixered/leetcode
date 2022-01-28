81ba4151e0b39c93102385247c5a1a839e33bc90
Anand Tripathi <shadarsh23@gmail.com>
2022-01-28 12:47:06 +0530

    Time: 48 ms (92.59%), Space: 25.8 MB (16.88%) - LeetHub

--- 475-heaters/475-heaters.cpp
+++ 475-heaters/475-heaters.cpp
@@ -3,18 +3,27 @@ public:
     int findRadius(vector<int>& houses, vector<int>& heaters) {
         sort(houses.begin(),houses.end());
         sort(heaters.begin(),heaters.end());
-        int ans = 0;
-        for(auto it:houses)
+        vector<int> res(houses.size(),INT_MAX);
+        for(int i=0,h=0; i<houses.size() && h<heaters.size(); )
         {
-            int idx = lower_bound(heaters.begin(),heaters.end(),it)-heaters.begin();
-            int res = INT_MAX;
-            if(idx!=heaters.size())
-                res = min(res,abs(heaters[idx]-it));
-            idx--;
-            if(idx>=0)
-                res = min(res,abs(heaters[idx]-it));
-            ans = max(ans,res);
+            if(heaters[h]>=houses[i])
+            {
+                res[i] =  min((abs(heaters[h]-houses[i])),res[i]);
+                i++;
+            }
+            else
+                h++;
         }
-        return ans;
+        for(int i=houses.size()-1,h=heaters.size()-1; i>=0 && h>=0; )
+        {
+            if(heaters[h]<=houses[i])
+            {
+                res[i] =  min((abs(heaters[h]-houses[i])),res[i]);
+                i--;
+            }
+            else
+                h--;
+        }
+        return *max_element(res.begin(),res.end());
     }
 };
