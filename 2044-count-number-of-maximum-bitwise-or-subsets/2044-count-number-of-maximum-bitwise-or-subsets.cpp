beda6146e19cc6abe5d36c8a9213a2e3f16edefa
Anand Tripathi <shadarsh23@gmail.com>
2022-01-29 18:34:45 +0530

    Time: 230 ms (19.38%), Space: 8.5 MB (60.03%) - LeetHub

---
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int countMaxOrSubsets(vector<int>& nums) {
+        int n = nums.size();
+        int total = 1<<n;
+        int maxi = 0;
+        for(int bitmask=1; bitmask<total; bitmask++)
+        {
+            int temp = 0;
+            for(int i=0; i<n; i++)
+            {
+                if((bitmask&(1<<i))!=0)
+                    temp = temp|nums[i];
+            }
+            maxi = max(maxi,temp);
+        }
+        int count = 0;
+        for(int bitmask=1; bitmask<total; bitmask++)
+        {
+            int temp = 0;
+            for(int i=0; i<n; i++)
+            {
+                if((bitmask&(1<<i))!=0)
+                    temp = temp|nums[i];
+            }
+            if(temp==maxi)
+                count++;
+        }
+        return count;
+    }
+};
