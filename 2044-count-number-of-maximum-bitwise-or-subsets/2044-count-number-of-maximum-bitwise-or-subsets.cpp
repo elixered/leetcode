62e774ee6c00cffee31ae67c193929e13ac40aa8
Anand Tripathi <shadarsh23@gmail.com>
2022-01-29 18:41:09 +0530

    Time: 92 ms (55.18%), Space: 8.4 MB (87.35%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -4,16 +4,8 @@ public:
         int n = nums.size();
         int total = 1<<n;
         int maxi = 0;
-        for(int bitmask=1; bitmask<total; bitmask++)
-        {
-            int temp = 0;
-            for(int i=0; i<n; i++)
-            {
-                if((bitmask&(1<<i))!=0)
-                    temp = temp|nums[i];
-            }
-            maxi = max(maxi,temp);
-        }
+        for(auto it:nums)
+            maxi = maxi|it;
         int count = 0;
         for(int bitmask=1; bitmask<total; bitmask++)
         {
