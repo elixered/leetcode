aa69e36c2b78c9b42a0c61a5f88c2b4a4361036a
Anand Tripathi <shadarsh23@gmail.com>
2022-02-02 18:01:20 +0530

    Time: 36 ms (18.85%), Space: 5.9 MB (60.70%) - LeetHub

--- 788-rotated-digits/788-rotated-digits.cpp
+++ 788-rotated-digits/788-rotated-digits.cpp
@@ -1,17 +1,42 @@
 class Solution {
 public:
-    int rotatedDigits(int n) {
-         int f[] = {1,1,2,0,0,2,2,0,1,2};
-        int res = 0;
-        for(int i = 1; i <= n; i++){
-            int p = i;
-            int s = 1;
-            while(p){
-                s *= f[p%10];
-                p /= 10;
-            }
-            if(s >=2) res+=1;
+    bool canReverse(string s)
+    {
+        for(auto& ch:s)
+        {
+            if(ch=='3' or ch=='4' or ch=='7')
+                return false;
         }
-        return res;
+        return true;
+    }
+    char get(char x)
+    {
+        if(x=='0' or x=='1' or x=='8')
+            return x;
+        if(x=='2') return '5';
+        if(x=='5') return '2';
+        if(x=='6') return '9';
+        if(x=='9') return '6';
+        return '7';
+    }
+    int reverse(string s)
+    {
+        string ans;
+        for(auto& ch:s)
+            ans += get(ch);
+        return stoi(ans);
+    }
+    
+    int rotatedDigits(int n) {
+        int count = 0;
+        for(int i=1; i<=n; i++)
+        {
+            if(!canReverse(to_string(i)))
+                continue;
+            if(reverse(to_string(i))==i)
+                continue;
+            count++;
+        }
+        return count;
     }
 };
