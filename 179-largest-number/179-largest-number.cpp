54dac37b8b0c19deef6a624365ddf87ea5def92f
Anand Tripathi <shadarsh23@gmail.com>
2022-02-05 00:11:32 +0530

    Time: 13 ms (47.63%), Space: 11.1 MB (87.64%) - LeetHub

--- 179-largest-number/179-largest-number.cpp
+++ 179-largest-number/179-largest-number.cpp
@@ -4,28 +4,7 @@ public:
     {
         string x = to_string(a);
         string y = to_string(b);
-        int m = min(x.size(),y.size());
-        for(int i=0; i<m; i++)
-        {
-            if(x[i]>y[i]) return true;
-            else if(x[i]<y[i]) return false;
-        }
-        if(x.size()>y.size())
-        {
-            for(int i=m; i<x.size(); i++)
-            {
-                if(x[i]>y[i%m]) return true;
-            else if(x[i]<y[i%m]) return false;
-            }
-        }
-        else if(x.size()<y.size())
-        {
-            for(int i=m; i<y.size(); i++)
-            {
-                if(x[i%m]>y[i]) return true;
-            else if(x[i%m]<y[i]) return false;
-            }
-        }
+        
         return (x+y)>(y+x)?true:false;
     }
     string largestNumber(vector<int>& nums) {
