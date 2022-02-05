013aff09d51a48154f356d4860ba872e145c59ee
Anand Tripathi <shadarsh23@gmail.com>
2022-02-05 08:41:38 +0530

    Time: 50 ms (36.30%), Space: 13.4 MB (58.51%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -1,40 +1,39 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
-public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int n = lists.size();
-        ListNode* dummy = new ListNode(-1);
-        ListNode *dummyHead = dummy;
-        bool flag = true;
-        while(flag)
-        {
-            flag = false;
-            int mini = INT_MAX;
-            int idx = -1;
-            for(int i=0; i<n; i++)
-            {
-                ListNode* temp = lists[i];
-                if(temp!=NULL && temp->val<mini)
-                {
-                    mini = temp->val;
-                    idx = i;
-                    flag = true;
-                }
-            }
-            if(flag==false) break;
-            dummy->next = lists[idx];
-            dummy = lists[idx];
-            lists[idx] = lists[idx]->next;
+    struct compare {
+        bool operator()(const ListNode* l, const ListNode* r) {
+            return l->val > r->val;
         }
-        return dummyHead->next;
+    };
+    
+public:
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        priority_queue<ListNode *, vector<ListNode *>, compare> q;
+        for (auto& l : lists) {
+            if (l) {
+                q.push(l);
+            }
+        }
+        
+        if (q.empty()) {
+            return NULL;
+        }
+        
+        ListNode* result = q.top();
+        q.pop();
+        if (result->next) {
+            q.push(result->next);
+        }
+
+        ListNode* tail = result;            
+        while (!q.empty()) {
+            tail->next = q.top();
+            q.pop();
+            tail = tail->next;
+            if (tail->next) {
+                q.push(tail->next);
+            }
+        }
+        
+        return result;
     }
 };
