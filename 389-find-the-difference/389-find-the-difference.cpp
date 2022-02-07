32ba28c286b4c4c4f01642a939f5d030cd8a8ebb
Anand Tripathi <shadarsh23@gmail.com>
2022-02-07 11:35:37 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (67.00%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int> hm(26,0);
+        int x = 0;
         for(auto& it:t)
-            hm[it-'a']++;
+            x ^= (it-'a');
         for(auto& it:s)
-            hm[it-'a']--;
-        for(char c='a'; c<='z'; c++)
-            if(hm[c-'a']>0)
-                return c;
-        return 'a';
+            x ^= (it-'a');
+        return (char)('a'+x);
     }
 };
