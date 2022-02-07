47c17fcc0ffebb5bd54a409acf5cbc339d14e066
Anand Tripathi <shadarsh23@gmail.com>
2022-02-07 12:50:28 +0530

    Time: 124 ms (28.49%), Space: 11.2 MB (56.20%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> memo;
-    bool solve(string& s, string& p, int i, int j)
-    {
-        if(i<0 && j<0) return true;
-        else
-        if(j<0) return false;
-        else
-        if(i<0)
-            return (p[j]=='*' && solve(s,p,i,j-1));
-        if(memo[i][j]!=-1) return memo[i][j];
-        bool ans = false;
-        if(s[i]==p[j] or p[j]=='?')
-            ans = ans or solve(s,p,i-1,j-1);
-        else if(p[j]=='*')
-            ans = ans or solve(s,p,i,j-1) or solve(s,p,i-1,j);
-        return memo[i][j] = ans;            
-    }
-    
     bool isMatch(string s, string p) {
-        memo = vector<vector<int>>(s.size(),vector<int>(p.size(),-1));
-        return solve(s,p,s.size()-1,p.size()-1);
+        int m = s.size();
+        int n = p.size();
+        vector<vector<bool>> dp(m+1,vector<bool>(n+1));
+        dp[0][0] = true;
+        for(int i=1; i<=m; i++)
+            dp[i][0] = false;
+        for(int j=1; j<=n; j++)
+            dp[0][j] = dp[0][j-1] && (p[j-1]=='*');
+        for(int i=1; i<=m; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                if(s[i-1]==p[j-1] or p[j-1]=='?')
+                    dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1]=='*')
+                    dp[i][j] = dp[i][j-1] or dp[i-1][j];
+                else dp[i][j] = false;
+            }
+        }
+        return dp[m][n];
     }
 };
