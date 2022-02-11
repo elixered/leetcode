1f0d5348b5994fa3047dd7096cb73729085f7003
Anand Tripathi <shadarsh23@gmail.com>
2022-02-11 12:22:00 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (79.27%) - LeetHub

--- 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
+++ 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int countSubstrings(string s, string t) {
-        int M = s.size(), N = t.size(), ans = 0;
-        for (int i = 0; i < M; ++i) {
-            for (int j = 0; j < N; ++j) {
-                if (s[i] == t[j]) continue;
-                int left = 1, right = 1;
-                while (i - left >= 0 && j - left >= 0 && s[i - left] == t[j - left]) ++left;
-                while (i + right < M && j + right < N && s[i + right] == t[j + right]) ++right;
-                ans += left * right;
-            }
-        }
-        return ans;
-    }
+   int countSubstrings(string &s, string &t) {
+    int res = 0;
+    int dpl[101][101] = {}, dpr[101][101] = {};
+    for (int i = 1; i <= s.size(); ++i)
+        for (int j = 1; j <= t.size(); ++j)
+            if (s[i - 1] == t[j - 1])
+                dpl[i][j] = 1 + dpl[i - 1][j - 1];
+    for (int i = s.size(); i > 0; --i)
+        for (int j = t.size(); j > 0; --j)
+            if (s[i - 1] == t[j - 1])
+                dpr[i - 1][j - 1] = 1 + dpr[i][j];
+    for (int i = 0; i < s.size(); ++i)
+        for (int j = 0; j < t.size(); ++j)
+            if (s[i] != t[j])
+                res += (dpl[i][j] + 1) * (dpr[i + 1][j + 1] + 1);
+    return res;
+}
 };
