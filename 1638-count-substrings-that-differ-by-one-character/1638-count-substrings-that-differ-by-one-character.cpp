7381627b731964d1bf15997904f6dd4ec8ac1aa1
Anand Tripathi <shadarsh23@gmail.com>
2022-02-11 12:17:32 +0530

    Time: 5 ms (43.29%), Space: 6.3 MB (50.30%) - LeetHub

--- 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
+++ 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
@@ -1,65 +1,41 @@
 class Solution {
 public:
-    int getDiff(list<char>& m1, list<char>& m2)
-    {
-        int count = 0;
-        string s,t;
-        for(auto& it:m1)
-            s += it;
-        for(auto& it:m2)
-            t += it;
-        for(int i=0; i<s.size(); i++)
-            if(s[i]!=t[i])
-                count++;
-        return count;
-    }
-    int solve(string& s, string& t)
-    {
-        list<char> a,b;
-        for(auto& it:s)
-            a.push_back(it);
-        int count = 0;
-        int i=0,j=0;
-        int n = t.size();
-        while(j<n)
-        {
-            b.push_back(t[j]);
-            if(j-i+1<s.size())
-            {
-                j++;
-                continue;
-            }
-            else
-            {
-                int diff = getDiff(a,b);
-                if(diff==1)
-                {
-                    count++;
-                }
-                b.pop_front();
-                i++;
-                j++;
-            }
-        }
-        return count;
-    }
-    
     int countSubstrings(string s, string t) {
-        vector<string> s1;
-        for(int i=0; i<s.size(); i++)
+        int m = s.size();
+        int n = t.size();
+        int ans = 0;
+        for(int i=0; i<m; i++)
         {
-            string a = "";
-            for(int j=i; j<s.size(); j++)
+            for(int j=0; j<n; j++)
             {
-                a += s[j];
-                s1.push_back(a);
+                if(s[i]!=t[j])
+                {
+                    int sleft = i-1;
+                    int tleft = j-1;
+                    int left = 0;
+                    while(sleft>=0 && tleft>=0 && s[sleft]==t[tleft])
+                    {
+                        sleft--;
+                        tleft--;
+                        left++;
+                    }
+                    int sright = i+1;
+                    int tright = j+1;
+                    int right = 0;
+                    while(sright<m && tright<n && s[sright]==t[tright])
+                    {
+                        right++;
+                        sright++;
+                        tright++;
+                    }
+                    if(left==0)
+                        ans += right+1;
+                    else if(right==0)
+                        ans += left+1;
+                    else ans += (left+1)*(right+1);
+                }
             }
         }
-        int count = 0;
-        for(auto& it:s1)
-        {
-            count += solve(it,t);
-        }
-        return count;
+        return ans;
     }
 };
