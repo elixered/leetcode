a7c28821f83fd991834780e28c344e7dd7edf14a
Anand Tripathi <shadarsh23@gmail.com>
2022-02-12 11:07:38 +0530

    Time: 32 ms (98.81%), Space: 13.7 MB (85.14%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {  
         unordered_set<string> words(wordList.begin(),wordList.end());
-        queue<string> q;
-        if(words.find(endWord)==words.end())
-            return 0;
-        int count = 0;
-        q.push(beginWord);
-        while(!q.empty())
+        if(words.find(endWord)==words.end()) return 0;
+        unordered_set<string> begin;
+        unordered_set<string> end;
+        begin.insert(beginWord);
+        end.insert(endWord);
+        int count = 1;
+        while(!begin.empty() && !end.empty())
         {
             count++;
-            int n = q.size();
-            for(int k=0; k<n; k++)
+            if(begin.size()>end.size())
+                swap(begin,end);
+            unordered_set<string> tbeg;
+            for(auto s:begin)
             {
-                auto s = q.front();
-                q.pop();
-                if(s==endWord) return count;
                 for(int i=0; i<s.size(); i++)
                 {
                     char temp = s[i];
                     for(char ch='a'; ch<='z'; ch++)
                     {
                         s[i] = ch;
-                        if(words.find(s)==words.end()) continue;
+                        if(end.find(s)!=end.end())
+                            return count;
+                        if(words.find(s)!=words.end())
                         {
-                            q.push(s);
                             words.erase(s);
+                            tbeg.insert(s);
                         }
+                        s[i] = temp;
                     }
-                    s[i] = temp;
                 }
             }
+            begin = tbeg;
         }
         return 0;
     }
