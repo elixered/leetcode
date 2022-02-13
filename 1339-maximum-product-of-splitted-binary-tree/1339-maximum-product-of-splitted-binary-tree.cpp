ad0b6e5d9a5a33934516ee334c61ca3c99fa4baf
Anand Tripathi <shadarsh23@gmail.com>
2022-02-14 01:26:49 +0530

    Time: 112 ms (94.00%), Space: 77.4 MB (70.09%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -1,16 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    long long ans = 0, totalSum = 0;
-    int maxProduct(TreeNode* root) {
-        totalSum = dfs(root); // First, get total sum of all nodes in the Binary Tree
-        dfs(root); // Then dfs in post order to calculate sum of each subtree and its complement
-        return ans % int(1e9+7);
+    #define ll long long
+    long long mod;
+    long long total;
+    long long prod;
+    void dfs(TreeNode* root)
+    {
+        if(root)
+        {
+            total += root->val;
+            dfs(root->left);
+            dfs(root->right);
+        }
     }
-    
-    int dfs(TreeNode* root) {
-        if (root == NULL) return 0;
-        int currSum = dfs(root->left) + dfs(root->right) + root->val;
-        ans = max(ans, (totalSum - currSum) * currSum);
-        return currSum;
+    long long postorder(TreeNode* root)
+    {
+        if(root==NULL) return 0;
+        ll curr = postorder(root->left) + postorder(root->right) + root->val;
+        prod = max(prod,(curr*(total-curr)));
+        return curr;
+    }
+    int maxProduct(TreeNode* root) {
+        mod = pow(10,9)+7;
+        total = 0;
+        dfs(root);
+        postorder(root);
+        return prod%mod;
     }
 };
