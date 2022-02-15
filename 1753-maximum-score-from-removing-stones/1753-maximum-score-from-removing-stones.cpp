3f96f41eaff3b375fde50ee632aa4ee8edbf264d
Anand Tripathi <shadarsh23@gmail.com>
2022-02-15 15:40:05 +0530

    Time: 5 ms (67.80%), Space: 5.9 MB (93.56%) - LeetHub

--- 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
-   int maximumScore(int a, int b, int c) {
-    if (a < b)
-        return maximumScore(b, a, c);
-    if (b < c)
-        return maximumScore(a, c, b);
-    return b == 0 ? 0 : 1 + maximumScore(a - 1, b - 1, c); 
-}
+    int maximumScore(int a, int b, int c) {
+        if(a>b)
+            return maximumScore(b,a,c);
+        if(b>c)
+            return maximumScore(a,c,b);
+        if(a+b<=c)
+            return a+b;
+        else
+        {
+            return (a+b-c)/2+c;
+        }
+        return a;
+    }
 };
