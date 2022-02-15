2515386d7a9662b744804903c1ac9019de9b15ef
Anand Tripathi <shadarsh23@gmail.com>
2022-02-15 15:42:13 +0530

    Time: 0 ms (100.00%), Space: 6 MB (71.21%) - LeetHub

--- 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -3,14 +3,11 @@ public:
     int maximumScore(int a, int b, int c) {
         if(a>b)
             return maximumScore(b,a,c);
-        if(b>c)
+        else if(b>c)
             return maximumScore(a,c,b);
-        if(a+b<=c)
+        else if(a+b<=c)
             return a+b;
         else
-        {
             return (a+b-c)/2+c;
-        }
-        return a;
     }
 };
