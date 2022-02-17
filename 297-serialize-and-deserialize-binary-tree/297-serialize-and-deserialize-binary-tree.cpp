849a2ed999b506241cb650cac872f6c70270e969
Anand Tripathi <shadarsh23@gmail.com>
2022-02-17 12:22:25 +0530

    Time: 64 ms (60.48%), Space: 29.1 MB (87.25%) - LeetHub

---
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
@@ -0,0 +1,71 @@
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+
+    string serialize(TreeNode* root) {
+       string s = "";
+        queue<TreeNode*> q;
+        if(root==NULL)
+            return s;
+        q.push(root);
+        while(!q.empty())
+        {
+            auto node = q.front();
+            q.pop();
+            if(node==NULL)
+                s.append("#,");
+            else s.append(to_string(node->val)+',');
+            if(node!=NULL)
+            {
+                q.push(node->left);
+                q.push(node->right);
+            }
+        }
+        
+        return s;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        if(data.size()==0 or data[0]=='#')
+            return NULL;
+        stringstream ss(data);
+        string curr;
+        getline(ss,curr,',');
+        TreeNode* root = new TreeNode(stoi(curr));
+        queue<TreeNode*> q;
+        q.push(root);
+        while(getline(ss,curr,','))
+        {
+            auto node = q.front();
+            q.pop();
+            if(curr=="#")
+            {
+                node->left = NULL;
+            }
+            else
+            {
+                node->left = new TreeNode(stoi(curr));
+                q.push(node->left);
+            }
+            if(getline(ss,curr,','))
+            {
+                 if(curr=="#")
+            {
+                node->right = NULL;
+            }
+            else
+            {
+                node->right = new TreeNode(stoi(curr));
+                q.push(node->right);
+            }
+            }
+        }
+        return root;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
