e9874ab0fce0dc5565a892f376c2f58ec46f415d
Anand Tripathi <shadarsh23@gmail.com>
2022-02-17 10:19:57 +0530

    Time: 15 ms (19.98%), Space: 12.9 MB (60.22%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -10,31 +10,39 @@
 class Solution {
 public:
     vector<int> ans;
-    unordered_map<TreeNode*,TreeNode*> map;
-    void dfs(TreeNode* root,TreeNode* prev, int curr, int k)
+    void dfs(TreeNode* root,TreeNode* prev, int curr, int k,unordered_map<TreeNode*,TreeNode*> &parent)
     {
         if(root==NULL) return;
         if(curr==k)
             ans.push_back(root->val);
         if(curr>k) return;
         if(root->left!=prev)
-        dfs(root->left,root,curr+1,k);
+        dfs(root->left,root,curr+1,k,parent);
         if(root->right!=prev)
-        dfs(root->right,root,curr+1,k);
-        if(map[root]!=prev)
-        dfs(map[root],root,curr+1,k);
+        dfs(root->right,root,curr+1,k,parent);
+        if(parent[root]!=prev)
+        dfs(parent[root],root,curr+1,k,parent);
     }
-    void level(TreeNode* root, TreeNode* par,TreeNode* target, int curr)
+    void getParents(TreeNode* root, unordered_map<TreeNode*,TreeNode*> &parent)
     {
-        if(root==NULL) return;
-        map[root] = par;
-        level(root->left,root,target,curr+1);
-        level(root->right,root,target,curr+1);
+        if(root==NULL)
+            return;
+        if(root->left)
+        {
+            parent[root->left] = root;
+            getParents(root->left,parent);
+        }
+        if(root->right)
+        {
+            parent[root->right] = root;
+            getParents(root->right,parent);
+        }
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        map[root] = NULL;
-        level(root,NULL,target,0);
-        dfs(target,NULL,0,k);
+        unordered_map<TreeNode*,TreeNode*> parent;
+        parent[root] = NULL;
+        getParents(root,parent);
+        dfs(target,NULL,0,k,parent);
         return ans;
     }
 };
