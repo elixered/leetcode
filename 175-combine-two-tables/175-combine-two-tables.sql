0198d6048fe1ba6f7adb05b53341f368bbfe6e89
Anand Tripathi <shadarsh23@gmail.com>
2022-02-18 13:48:50 +0530

    Time: 462 ms (25.46%), Space: 0B (100.00%) - LeetHub

--- 175-combine-two-tables/175-combine-two-tables.sql
+++ 175-combine-two-tables/175-combine-two-tables.sql
@@ -1,4 +1,1 @@
-# Write your MySQL query statement below
-SELECT firstName, lastName, City, State 
-FROM Person LEFT JOIN Address
-ON Person.PersonId = Address.PersonId;
+SELECT Person.FirstName, Person.LastName, Address.City, Address.State FROM Person NATURAL LEFT JOIN Address;
