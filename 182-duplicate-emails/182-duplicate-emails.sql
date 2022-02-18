87e044beedd9305655c17a49dca7c3abb555f1aa
Anand Tripathi <shadarsh23@gmail.com>
2022-02-18 14:11:13 +0530

    Time: 560 ms (9.65%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,4 +1,3 @@
-# Write your MySQL query statement below
-select distinct p1.email from Person as p1
-join Person as p2
-on p1.id != p2.id and p1.email = p2.email;
+select email from Person
+group by email
+having count(email)>1;
