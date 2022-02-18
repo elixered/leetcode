65a8ae58baa27a7feb4fdca468311d3805308fb9
Anand Tripathi <shadarsh23@gmail.com>
2022-02-18 11:32:12 +0530

    Time: 895 ms (12.27%), Space: 196.4 MB (7.14%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
-        sort(changed.begin(),changed.end());
-        unordered_multiset<int> set(changed.begin(),changed.end()),deleted;
-        vector<int> ans;
+        multiset<int> set(changed.begin(),changed.end());
         int n = changed.size();
         if(n%2!=0) return {};
-        int mid = n/2;
-        int i = n-1;
-        while(mid>0)
+        vector<int> ans;
+        while(set.size())
         {
-            if(deleted.find(changed[i])!=deleted.end())
-            {
-                deleted.erase(deleted.find(changed[i]));
-                i--;
-                continue;
-            }
-            if(changed[i]%2!=0 or set.find(changed[i]/2)==set.end() or set.find(changed[i])==set.end())
+            auto it = set.begin();
+            int y = 2*(*it);
+            set.erase(it);
+            if(set.find(y)==set.end())
                 return {};
             else
             {
-                ans.push_back(changed[i]/2);
-                set.erase(set.find(changed[i]));
-                set.erase(set.find(changed[i]/2));
-                deleted.insert(changed[i]/2);
-                i--;
-                mid--;
+                ans.push_back(y/2);
+                set.erase(set.find(y));
             }
         }
+        if(ans.size()==0) return {};
         return ans;
     }
 };
