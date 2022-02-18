862599541074a08cdc6102e53f4dac7e96925abf
Anand Tripathi <shadarsh23@gmail.com>
2022-02-18 11:53:26 +0530

    Time: 274 ms (13.05%), Space: 58.1 MB (59.49%) - LeetHub

--- 954-array-of-doubled-pairs/954-array-of-doubled-pairs.cpp
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    bool canReorderDoubled(vector<int>& changed) {
-        multiset<int> set(changed.begin(),changed.end());
-        int n = changed.size();
-        if(n%2!=0) return {};
-        while(set.size())
+    static bool cmp(int a, int b)
+    {
+        return abs(a)<abs(b);
+    }
+    bool canReorderDoubled(vector<int>& nums) {
+        sort(nums.begin(),nums.end(),cmp);
+        unordered_map<int,int> map;
+        for(auto& it:nums)
+            map[it]++;
+        for(auto& it:nums)
         {
-            auto it = set.begin();
-            int y;
-            if(*it<0)
-            {
-                if(*it%2!=0) return false;
-                else y = *it/2;
-            }
-            else y = 2*(*it);
-            set.erase(it);
-            if(set.find(y)==set.end())
-                return false;
-            else
-            {
-                set.erase(set.find(y));
-            }
+            if(map[it]==0) continue;
+            map[it]--;
+            int y = 2*it;
+            if(map[y]<=0) return false;
+            map[y]--;
         }
-       return set.size()==0;
+        return true;
     }
 };
