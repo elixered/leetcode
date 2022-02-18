2e904f7690e6e2cb4f1fb3b1a5988f6834ef051c
Anand Tripathi <shadarsh23@gmail.com>
2022-02-18 11:00:40 +0530

    Time: 407 ms (6.13%), Space: 121.3 MB (5.47%) - LeetHub

--- 738-monotone-increasing-digits/738-monotone-increasing-digits.cpp
+++ 738-monotone-increasing-digits/738-monotone-increasing-digits.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    int monotoneIncreasingDigits(int n) {
-        if(n<10) return n;
-        string s = to_string(n);
-        int sz = s.size();
-        for(int i=1; i<sz; i++)
-        {
-            char curr = s[i];
-            char prev = s[i-1];
-            if(curr>=prev)
-                continue;
-            else
-            {
-                int idx = i-1;
-                while(idx>=0 && s[idx]==s[i-1])
-                    idx--;
-                idx++;
-                s[idx]--;
-                for(int j=idx+1; j<sz; j++)
-                    s[j] = '9';
-                break;
-            }
+    int monotoneIncreasingDigits(int N) {
+        
+        queue<long>q;
+        for (int i = 1; i <= 9; i++)
+            q.push(i);
+        
+        int res =0;
+        while (!q.empty()) {
+            int t = q.front();
+            q.pop();
+            
+            if (t > N) break;
+            res = max(res, t);
+            
+            int r = t % 10;
+            for (int i = r; i <= 9; i++)
+                q.push((long)t*10 + i);
         }
-        return stoi(s);
+        
+        return res;
     }
 };
