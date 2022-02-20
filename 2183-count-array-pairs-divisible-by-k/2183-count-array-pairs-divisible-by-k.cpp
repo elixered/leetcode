9d3b2fe6d97f97c84c8a4e71fa9f59cae56f0e5c
Anand Tripathi <shadarsh23@gmail.com>
2022-02-20 11:15:01 +0530

    Time: 544 ms (70.00%), Space: 82 MB (20.00%) - LeetHub

--- 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.cpp
+++ 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     long long coutPairs(vector<int>& nums, int k) {
-        unordered_map<long long,long long> res_cnt;
-        for(int num:nums){
-            res_cnt[num%k]++;
+        unordered_map<long long,long long> map;
+        for(auto it: nums)
+        {
+            map[it%k]++;
         }
-        long long ret = 0;
-        for(auto [n1,c1]:res_cnt){
-
-            long long g = gcd(n1,(long long)(k));
+        long long res = 0;
+        for(auto [n1,c1]: map)
+        {
+            long long g = gcd(n1,(long long)k);
             long long another = k/g;
-            if(n1==0){
+            if(n1==0)
+            {
                 long long l = nums.size();
-                ret += c1*(l-c1) + c1*(c1-1)/2;
+                res += c1*(l-c1) + c1*(c1-1)/2;
                 continue;
-            }            
-            
-            for(long long n2=another;n2<k;n2+= another){
-                if(n2<n1){
+            }
+            for(long long n2=another; n2<k; n2+=another)
+            {
+                if(n2<n1)
                     continue;
+                if(n2==n1)
+                {
+                    res += c1*(c1-1)/2;
                 }
-
-                if(n2==n1){
-                    ret += c1*(c1-1)/2;
-                    continue;
-                }
-                if(res_cnt.count(n2)==0){
-                    continue;
-                }
-                ret += c1*res_cnt[n2];
+                else if(map.find(n2)!=map.end())
+                    res += c1*map[n2];
             }
         }
-        return ret;
+        return res;
     }
 };
