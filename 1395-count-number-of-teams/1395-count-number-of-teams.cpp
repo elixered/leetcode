6ae9e00cb33d141708170d2e7286de3915fa66d3
Anand Tripathi <shadarsh23@gmail.com>
2022-02-21 12:24:55 +0530

    Time: 0 ms (100.00%), Space: 18.5 MB (5.68%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -1,31 +1,47 @@
 class Solution {
-public:
+public:    
+    void update(vector<int>& bit,int n,int x, int delta)
+    {
+        for(; x<=n; x += x&-x)
+            bit[x] += delta;
+    }
+    int sum(vector<int>& bit,int x)
+    {
+        int s = 0;
+        for(; x>0; x -= x&-x)
+            s += bit[x];
+        return s;
+    }
     int numTeams(vector<int>& arr) {
         int n = arr.size();
-        int result = 0;
-        for(int i = 1 ; i < n-1 ; i++){
-            int leftSmall = 0, leftLarge = 0;
-            int rightSmall = 0, rightLarge = 0;
-            //left part
-            for(int j = 0 ; j < i ; j++){
-                if(arr[j] < arr[i]){
-                    leftSmall++;
-                }
-                if(arr[j] > arr[i]){
-                    leftLarge++;
-                }
-            }
-            //right part
-            for(int j = i+1 ; j < n ; j++){
-                if(arr[j] < arr[i]){
-                    rightSmall++;
-                }
-                if(arr[j] > arr[i]){
-                    rightLarge++;
-                }
-            }
-            result += leftSmall * rightLarge + leftLarge * rightSmall;
-        }
-        return result;
+        int m = *max_element(arr.begin(),arr.end());
+        vector<int> right(m+1,0),left(m+1,0);
+        
+        for(int i=0;i<n;++i)
+            update(right, m, arr[i], 1);
+        
+         int ans = 0, l, r;
+        
+         for(int i=0;i<n;++i)
+         {
+             update(right, m, arr[i], -1);
+             
+            //smaller to left
+            l = sum(left, arr[i]-1);
+            //greter to right
+            r = sum(right, m) - sum(right, arr[i]);
+            
+             ans += l*r;
+             
+            //larget to left
+            l = sum(left, m) - sum(left, arr[i]);
+            //smaller to right
+            r = sum(right, arr[i] -1);
+             
+            ans += l*r;
+            
+            update(left, m, arr[i], 1);
+         }
+        return ans;
     }
 };
