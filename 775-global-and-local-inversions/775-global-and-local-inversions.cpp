bff224f288691791412655ced8d0b07920e4110b
Anand Tripathi <shadarsh23@gmail.com>
2022-02-21 13:06:15 +0530

    Time: 165 ms (68.42%), Space: 83.4 MB (33.83%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.cpp
+++ 775-global-and-local-inversions/775-global-and-local-inversions.cpp
@@ -1,33 +1,9 @@
 class Solution {
-vector<int> BIT;
-int m;
-void update(int x, int delta)
-{
-      for(; x <= m; x += x&-x)
-        BIT[x] += delta;
-}
-int query(int x)
-{
-     int sum = 0;
-     for(; x > 0; x -= x&-x)
-        sum += BIT[x];
-     return sum;
-}
 public:
-    bool isIdealPermutation(vector<int>& nums) {
-        int local = 0;
-        int global = 0;
-        int n = nums.size();
-        m = n+2;
-        BIT = vector<int>(m+2,0);
-        for(int i=0; i<n; i++)
-        {
-            if(i<n-1 && nums[i]>nums[i+1])
-                local++;
-            global += query(m-1)-query(nums[i]);
-            if(global>n) return false;
-            update(nums[i]+1,1);
+    bool isIdealPermutation(vector<int>& A) {
+	for (int i = 0; i < A.size(); ++i) {
+            if (abs(A[i] - i) > 1) return false;
         }
-        return global == local;
+	return true;
     }
 };
