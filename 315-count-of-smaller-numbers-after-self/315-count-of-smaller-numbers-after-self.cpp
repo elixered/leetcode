08029035b8da6a30bdecebfa7f39654042e206f3
Anand Tripathi <shadarsh23@gmail.com>
2022-02-22 12:04:29 +0530

    Time: 726 ms (51.41%), Space: 216.3 MB (29.82%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,45 +1,50 @@
 class Solution {
 public:
+void invCount(vector<int> &count, vector<pair<int, int>> &v, int low, int high)
+{
+    int mid=low+(high-low)/2, i=low, k=0, j=mid+1;
+    vector<pair<int, int>> temp(high-low+1);
     
-struct BIT{
-    vector<int> bit;
-    int n;
-    BIT(){
-        n = 1e4 * 2 + 2;
-        bit.assign(n, 0);
-    }        
-    void update(int idx, int val){
-        while(idx < n){
-            bit[idx]+=val;
-            idx += idx&-idx;
+    while(i<=mid && j<=high)
+    {
+        if(v[i].first>v[j].first)
+        {
+            count[v[i].second] += high-j+1;
+            temp[k++]=v[i++];
         }
+        else    temp[k++]=v[j++];
+        
     }
-	
-    
-    int query(int idx){
-        int s = 0;
-        while(idx > 0){
-            s += bit[idx];
-            idx -= idx&-idx;
-        }
-        return s;
-    }
-};
+    while(i<=mid)
+        temp[k++]=v[i++];
 
-int getidx(int idx){
-    return idx + 1e4+1;
+    while(j<=high)
+        temp[k++]=v[j++];
+    
+    for(int x=low; x<=high; x++)
+    {
+            v[x]=temp[x-low];
+    }
+    
+}
+
+void mergeSort(vector<int> &count, vector<pair<int, int>> &v, int low, int high)
+{
+    if(low>=high)   return;
+    int mid=low+(high-low)/2;
+    mergeSort(count, v, low, mid);
+    mergeSort(count, v, mid+1, high);
+    invCount(count, v, low, high);
 }
 
 vector<int> countSmaller(vector<int>& nums) {
-    int n = nums.size();
-    vector<int> ans(n,0);
-    BIT bit;
-    for(int i=n-1; i>=0; i--)
-    {
-        ans[i] = bit.query(getidx(nums[i])-1);
-        bit.update(getidx(nums[i]),1);
-    }
-    return ans;
-    
+    int n=nums.size();
+    vector<pair<int, int>> v;
+    vector<int> count(n,0);
+    for(int i=0; i<n; i++)
+        v.push_back(make_pair(nums[i], i));
+
+    mergeSort(count, v, 0, n-1);
+    return count;    
 }
 };
