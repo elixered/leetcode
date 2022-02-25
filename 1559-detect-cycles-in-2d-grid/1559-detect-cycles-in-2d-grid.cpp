e635aa31a5dd7f80d145f96786f80b41875951b1
Anand Tripathi <shadarsh23@gmail.com>
2022-02-25 14:00:06 +0530

    Time: 553 ms (25.00%), Space: 58.9 MB (63.16%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -1,26 +1,28 @@
 class Solution {
     public:
- vector<int> dir = { 0, 1, 0, -1, 0 }; 
-    bool isCyclic(vector<vector<char>>& grid, vector<vector<bool>>& visited, int i, int j, int x, int y)
-    {
-        visited[i][j] = true;
-        for(int d = 0; d < 4; ++d)
-        {
-            int a = i+dir[d];
-            int b = j+dir[d+1];
-            if(a >= 0 && a < grid.size() && b >= 0 && b < grid[0].size() && grid[a][b] == grid[i][j] && !(x == a && y == b))
-                if(visited[a][b] || isCyclic(grid, visited, a,b,i,j))
-                    return true;
+vector<int> d = {1, 0, -1, 0, 1};
+bool containsCycle(vector<vector<char>>& g) {
+    for (int i = 0; i < g.size(); ++i)
+        for (int j = 0; j < g[i].size(); ++j) {
+            if (g[i][j] >= 'a') {
+                char val = g[i][j];
+                vector<pair<int, int>> q = {{i, j}};
+                while (!q.empty()) {
+                    vector<pair<int, int>> q1;
+                    for (auto [x, y] : q) {
+                        if (g[x][y] < 'a')
+                            return true;
+                        g[x][y] -= 26;
+                        for (auto k = 0; k < 4; ++k) {
+                            int dx = x + d[k], dy = y + d[k + 1];
+                            if (dx >= 0 && dy >= 0 && dx < g.size() && dy < g[dx].size() && g[dx][dy] == val)
+                                q1.push_back({dx, dy});
+                        }
+                    }
+                    swap(q, q1);
+                }
+            }
         }
-        return false;
-    }
-    bool containsCycle(vector<vector<char>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
-        for(int i = 0; i < n; ++i)
-            for(int j = 0; j < m; ++j)
-                if(!visited[i][j] && isCyclic(grid, visited, i, j, -1, -1))
-                    return true;
-        return false;
-    }
+    return false;
+}
 };
