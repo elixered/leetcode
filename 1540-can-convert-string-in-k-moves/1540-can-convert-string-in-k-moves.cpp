f65018e26d69dd625848a589207f42bdd316dc6d
Anand Tripathi <shadarsh23@gmail.com>
2022-02-25 10:26:27 +0530

    Time: 55 ms (95.12%), Space: 17.8 MB (51.22%) - LeetHub

--- 1540-can-convert-string-in-k-moves/1540-can-convert-string-in-k-moves.cpp
+++ 1540-can-convert-string-in-k-moves/1540-can-convert-string-in-k-moves.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
     bool canConvertString(string s, string t, int k) {
-        if(s.size()!=t.size()) return false;
-        int n = s.size();
-        vector<int> map(27,0);
-        int mini = k/26;
-        int rest = k%26;
-        for(int i=1; i<=26; i++)
-        {
-            map[i] = mini;
-            if(i<=rest)
-                map[i]++;
-        }
-
-        for(int i=0; i<n; i++)
-        {
-            int diff = (t[i]-s[i]);
-            if(diff==0) continue;
-            if(diff<0) diff += 26;
-            if(map[diff]==0)
-                return false;
-            else
-            {
-                map[diff]--;
-            }
-        }
-        return true;
+    if (s.size() != t.size())
+        return false;
+    int mul[26] = {};
+    for (int i = 0; i < s.size(); ++i) {
+        int shift = t[i] - s[i] + (t[i] < s[i] ? 26 : 0);
+        if (shift != 0 && shift + mul[shift] * 26 > k)
+            return false;
+        ++mul[shift];
     }
+    return true;
+}
 };
