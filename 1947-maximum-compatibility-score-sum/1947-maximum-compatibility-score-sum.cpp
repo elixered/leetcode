388e175f4e7b8f70e210e08ca25c6b9a7ea4e347
Anand Tripathi <shadarsh23@gmail.com>
2022-02-26 23:19:18 +0530

    Time: 7 ms (90.43%), Space: 10.1 MB (37.62%) - LeetHub

--- 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.cpp
+++ 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.cpp
@@ -2,6 +2,7 @@ class Solution {
 private:
     int m;
     int n;
+    int memo[10][1000];
 public:
     int match(vector<vector<int>>& stu, vector<vector<int>>& ment, int i, int j)
     {
@@ -12,24 +13,27 @@ public:
         return c;
     }
     
-    int dfs(vector<vector<int>>& stu, vector<vector<int>>& ment, vector<int>& vis, int curr)
+    int dfs(vector<vector<int>>& stu, vector<vector<int>>& ment, vector<int>& vis, int curr, int mask)
     {
         if(curr >= m) return 0;
+        if(memo[curr][mask]!=-1)
+             return memo[curr][mask];
         int ans = 0;
         for(int i=0; i<m; i++)
         {
             if(vis[i]) continue;
             vis[i] = 1;
-            ans = max(ans,match(stu,ment,curr,i)+ dfs(stu,ment,vis,curr+1));
+            ans = max(ans,match(stu,ment,curr,i)+ dfs(stu,ment,vis,curr+1, mask|(1<<i)));
             vis[i] = 0;
         }
-        return ans;
+        return memo[curr][mask] = ans;
     }
     
     int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) {
         m = mentors.size();
         n = mentors[0].size();
+        memset(memo,-1,sizeof(memo));
         vector<int> vis(m,0);
-        return dfs(students,mentors,vis,0);
+        return dfs(students,mentors,vis,0,0);
     }
 };
