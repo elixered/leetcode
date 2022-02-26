9dbb12948a586b1466577215ff82dd8fda8df3b1
Anand Tripathi <shadarsh23@gmail.com>
2022-02-26 22:52:26 +0530

    Time: 231 ms (38.78%), Space: 10 MB (52.64%) - LeetHub

---
+++ 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    int m;
+    int n;
+public:
+    int match(vector<vector<int>>& stu, vector<vector<int>>& ment, int i, int j)
+    {
+        int c = 0;
+        for(int k=0; k<n; k++)
+            if(stu[i][k]==ment[j][k])
+                c++;
+        return c;
+    }
+    
+    int dfs(vector<vector<int>>& stu, vector<vector<int>>& ment, vector<int>& vis, int curr)
+    {
+        if(curr >= m) return 0;
+        int ans = 0;
+        for(int i=0; i<m; i++)
+        {
+            if(vis[i]) continue;
+            vis[i] = 1;
+            ans = max(ans,match(stu,ment,curr,i)+ dfs(stu,ment,vis,curr+1));
+            vis[i] = 0;
+        }
+        return ans;
+    }
+    
+    int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) {
+        m = mentors.size();
+        n = mentors[0].size();
+        vector<int> vis(m,0);
+        return dfs(students,mentors,vis,0);
+    }
+};
