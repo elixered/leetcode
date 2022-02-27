3f5e4d91659d7a5644258f96ca5a5ce301b7d1d7
Anand Tripathi <shadarsh23@gmail.com>
2022-02-27 20:07:14 +0530

    Time: 356 ms (63.30%), Space: 60.1 MB (89.91%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,34 +1,31 @@
 class Solution {
-    vector<vector<int>>dp;
-    bool dfs(unordered_map<int, vector<int>>& mp, int a, int b) {
-        
-        if (dp[a][b] != -1)
-            return dp[a][b];
-        
-        for (int i = 0; i < mp[a].size(); i++) {
-            if (mp[a][i] == b || dfs(mp, mp[a][i], b))
-                return dp[a][b] = 1;
-        }
-        
-        return dp[a][b] = 0;
-    }
 public:
     vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
         
-        unordered_map<int, vector<int>>mp;
-        
-        dp.resize(n, vector<int>(n, -1));
-        for (int i = 0; i < prerequisites.size(); i++) {
-            mp[prerequisites[i][1]].push_back(prerequisites[i][0]);
-            dp[prerequisites[i][1]][prerequisites[i][0]] = 1;
-        }
-            
-        vector<bool>res;
-        for (int i = 0; i < queries.size(); i++) {
-            bool p = dfs(mp, queries[i][1], queries[i][0]);
-            res.push_back(p);
+        vector<vector<int>> adjMatrix(n, vector<int>(n));
+        for (int i = 0; i < prerequisites.size(); ++i){
+            adjMatrix[prerequisites[i][0]][prerequisites[i][1]] = 1;
         }
         
-        return res;
+        FloydWarshall(n, adjMatrix);
+        vector<bool> ans;
+        
+        for (int i = 0; i < queries.size(); ++i){
+            ans.push_back(adjMatrix[queries[i][0]][queries[i][1]]);
+        }
+        
+        return ans;
+        
+    }
+    
+    void FloydWarshall(int n, vector<vector<int>>& m){
+        
+        for (int k = 0; k < n ; ++k){
+            for (int i = 0; i < n ; ++i){
+                for (int j = 0; j < n ; ++j){
+                    m[i][j] = m[i][j] or (m[i][k] and m[k][j]);
+                }
+            }
+        }
     }
 };
