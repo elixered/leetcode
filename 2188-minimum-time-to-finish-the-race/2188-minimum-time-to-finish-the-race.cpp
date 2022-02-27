fb9fbcb149dad4457736f16da28698a19bcefc8b
Anand Tripathi <shadarsh23@gmail.com>
2022-02-27 10:51:24 +0530

    Time: 796 ms (22.73%), Space: 179.3 MB (18.18%) - LeetHub

---
+++ 2188-minimum-time-to-finish-the-race/2188-minimum-time-to-finish-the-race.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    #define ll long long
+    ll M = pow(10,6);
+    int minimumFinishTime(vector<vector<int>>& tires, int change, int laps) {
+        vector<ll> best(2000,INT_MAX);
+        best[0] = 0;
+        for(auto& t:tires)
+        {
+            ll curr = t[0];
+            ll last = 0;
+            for(int d=1; d<=20; d++)
+            {
+                best[d] = min(best[d], last+curr);
+                last += curr;
+                curr *= t[1];
+                if(curr>M) break;
+            }
+        }
+        for(ll d=1; d<=laps; d++)
+        {
+            for(ll l = min(d,(ll)20); l>=1; l--)
+            {
+                best[d] = min(best[d], best[l]+best[d-l]+change);
+            }
+        }
+        return best[laps];
+    }
+};
