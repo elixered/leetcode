c546310d9b708773e2a569a5e01e0ad8f9e129f8
Anand Tripathi <shadarsh23@gmail.com>
2022-03-02 12:46:11 +0530

    Time: 14 ms (40.41%), Space: 8.2 MB (9.80%) - LeetHub

--- 649-dota2-senate/649-dota2-senate.cpp
+++ 649-dota2-senate/649-dota2-senate.cpp
@@ -1,52 +1,24 @@
 class Solution {
 public:
     string predictPartyVictory(string senate) {
-        int rad = 0, dire = 0;
-        int disabledRad = 0, disabledDire = 0;
-        for(auto c:senate)
-        {
-            if(c=='D')
-                dire++;
-            else rad++;
-        }
-        vector<int> vis(senate.size(),0);
-while(true){
+        queue<int> rq, dq;
+        
         for(int i=0; i<senate.size(); i++)
-        {
-            char it = senate[i];
-            if(!vis[i])
-            {
-            if(it=='D')
-            {
-                if(disabledDire>0)
-                {
-                    disabledDire--;
-                    vis[i] = 1;
-                }
-                else
-                {
-                    disabledRad++;
-                    rad--;
-                }
-            }
+            senate[i] == 'R' ? rq.push(i): dq.push(i);
+        
+        int a, b;
+        while(!rq.empty() and !dq.empty()){
+            a = rq.front(), b = dq.front();
+            rq.pop(), dq.pop();
+            if (a < b) // taking min and pushing it to queue again
+                rq.push(a+senate.size());
             else
-            {
-                if(disabledRad>0)
-                {
-                    disabledRad--;
-                    vis[i] = 1;
-                }
-                else
-                {
-                    disabledDire++;
-                    dire--;
-                }
-            }
-            }
-            if(dire<=0) return "Radiant";
-            if(rad<=0) return "Dire";
+                dq.push(b+senate.size());
         }
-}
-        return "LOL";
+        
+		if (rq.size() > dq.size())
+			return "Radiant";
+		else
+			return "Dire";
     }
 };
