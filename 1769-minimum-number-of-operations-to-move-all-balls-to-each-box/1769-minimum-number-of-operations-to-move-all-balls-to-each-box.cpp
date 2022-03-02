9156b8e5f1cdf5c7af1fe8abc4c9c64334780d12
Anand Tripathi <shadarsh23@gmail.com>
2022-03-02 20:36:00 +0530

    Time: 3 ms (98.49%), Space: 9 MB (62.35%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,35 +2,22 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) {
         int n = boxes.size();
-        vector<int> leftcount(n,0),rightcount(n,0),leftsum(n,0),rightsum(n,0);
-        if(boxes[0]=='1')
-        {
-            leftcount[0] = 1;
-        }
-        if(boxes[n-1]=='1')
-        {
-            rightcount[n-1] = 1;
-            rightsum[n-1] = n-1;
-        }
-        for(int i=1; i<n; i++)
-        {
-            leftcount[i] = leftcount[i-1] + (boxes[i]-'0');
-            leftsum[i] += leftsum[i-1] + (boxes[i]=='1' ? i:0);
-        }
-        for(int i=n-2; i>=0; i--)
-        {
-            rightcount[i] = rightcount[i+1] + (boxes[i]-'0');
-            rightsum[i] = rightsum[i+1] + (boxes[i]=='1' ? i:0);
-        }
         vector<int> ans(n,0);
-        for(int i=0; i<n; i++)
+        
+        for(int i=0,cnt=0,sum=0; i<n; i++)
         {
-            int lcount = (i>0?leftcount[i-1]:0);
-            int lsum = (i>0?leftsum[i-1]:0);
-            int rcount = (i<n-1?rightcount[i+1]:0);
-            int rsum = (i<n-1?rightsum[i+1]:0);
-            ans[i] = rsum - i*rcount + (i*lcount-lsum);
+            cnt += (boxes[i]-'0');
+            sum += (boxes[i]=='1' ? i:0);
+            ans[i] += i*cnt - sum;
         }
+        
+        for(int i=n-1,cnt=0,sum=0; i>=0; i--)
+        {
+            cnt += (boxes[i]-'0');
+            sum += (boxes[i]=='1' ? i:0);
+            ans[i] += sum - cnt*i;
+        }
+        
         return ans;
     }
 };
