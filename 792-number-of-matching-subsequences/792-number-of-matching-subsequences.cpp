c49dc7ef07fd6f12a4cb1bcd3233ae4f0cb30c65
Anand Tripathi <shadarsh23@gmail.com>
2022-03-02 11:47:16 +0530

    Time: 533 ms (31.14%), Space: 49.6 MB (56.16%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,62 +1,25 @@
 class Solution {
 public:
-        
     int numMatchingSubseq(string s, vector<string>& words) {
-        
-        // stores positions of each character in 2D vector
-        vector<vector<int>>char_positions(26,vector<int>());
-        
-        int l = s.length();
-        for(int i=0;i<l;i++)
-        {
-            char_positions[s[i]-'a'].push_back(i);
+        unordered_map<char,vector<int>> mp;
+        int ans = 0;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]].push_back(i);
         }
-        
-        // count the valid subsequences
-        int count = 0;
-        int wordsl = words.size();
-        
-        // loop through all the given words
-        for(int i=0;i<wordsl;i++)
-        {
-            int wl = words[i].length();
+        for(auto word : words){
             bool flag = true;
-            
-            // if the first character's vector of a word is empty then subsequence does not exists
-            if(!char_positions[words[i][0]-'a'].empty())
-            {
-                // index of first character of a word in string S
-                int prevIndex = char_positions[words[i][0]-'a'][0];
-                for(int j=1;j<wl;j++)
-                {
-                    // if the jth character's vector of a word is empty then subsequence does not exists
-                    if(char_positions[words[i][j]-'a'].empty())
-                    {
-                        flag = false;
-                        break;
-                    }
-                    
-                    // find the index of the position in character vector where position is greater than prevIndex
-                    int index = lower_bound(char_positions[words[i][j] - 'a'].begin(), char_positions[words[i][j] - 'a'].end(),prevIndex+1) - char_positions[words[i][j] - 'a'].begin();
-
-                    // no position greater than prevIndex exists in vector of current character 
-                    if(index == char_positions[words[i][j] - 'a'].size())
-                    {
-                        flag = false;
-                        break;
-                    }
-                    else
-                    {
-                        // position greater than prevIndex exists in vector of current character and so update the prevIndex with found position
-                        prevIndex = char_positions[words[i][j] - 'a'][index];
-                    }
+            int prev = -1;
+            for(auto i : word){
+                auto pos = upper_bound(mp[i].begin(),mp[i].end(),prev);
+                if(pos == mp[i].end()){
+                    flag = false;
+                    break;
+                }else{
+                    prev = *pos;
                 }
-
-                // update valid subsequences 
-                if(flag) count++;           
             }
+            if(flag) ans++;
         }
-        
-        return count;
+        return ans;
     }
 };
