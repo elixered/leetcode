c3af001f26f4bf42af459d0948017ba23dee7290
Anand Tripathi <shadarsh23@gmail.com>
2022-03-02 14:18:33 +0530

    Time: 0 ms (100.00%), Space: 6 MB (75.64%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
+    
+    int recur(int n, int k)
+    {
+        if(n==1) return 0;
+        return (k+recur(n-1,k))%n;
+    }
+    
     int findTheWinner(int n, int k) {
-        queue<int> q;
-        for(int i=1; i<=n; i++)
-            q.push(i);
-        while(q.size()>1)
-        {
-            for(int i=1; i<k; i++)
-            {
-                q.push(q.front());
-                q.pop();
-            }
-            q.pop();
-        }
-        return q.front();
+        return recur(n,k)+1;
     }
 };
