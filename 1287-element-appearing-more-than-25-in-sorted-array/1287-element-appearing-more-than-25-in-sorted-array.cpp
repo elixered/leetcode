2787aaab3251409a0cfe7325ded30233f89e7728
Anand Tripathi <shadarsh23@gmail.com>
2022-03-03 10:45:15 +0530

    Time: 14 ms (73.08%), Space: 12.5 MB (40.82%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     int findSpecialInteger(vector<int>& nums) {
         int n = nums.size();
-        for(int i=0; i<n; i++)
+        if(n<4) return nums[0];
+        int tf = n/4-1;
+        for(int i=0; i<4; i++)
         {
-            int j = i;
-            while(j<n && nums[j]==nums[i])
-                j++;
-            if(j-i > n/4) return nums[i];
-            i = j-1;
+            int curr = nums[tf];
+            int max = upper_bound(nums.begin(),nums.end(),curr)-nums.begin()-1;
+            int min = lower_bound(nums.begin(),nums.end(),curr) - nums.begin();
+            if(max-min+1 > n/4) return curr;
+            tf += n/4;
         }
         return -1;
     }
