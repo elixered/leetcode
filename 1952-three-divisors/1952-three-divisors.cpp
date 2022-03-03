36a6001d50a3e1ba2b61f5d3f5c3aa796d87c47e
Anand Tripathi <shadarsh23@gmail.com>
2022-03-03 19:56:01 +0530

    Time: 3 ms (34.59%), Space: 5.8 MB (89.58%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        int count = 0;
+        for(int i=1; i<=n; i++)
+        {
+            if(n%i==0) count++;
+            if(count>3) return false;
+        }
+        return count==3;
+    }
+};
