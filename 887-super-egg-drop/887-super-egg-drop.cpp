99e4fef1b16da19c99a499f389254634c4ea630a
Anand Tripathi <shadarsh23@gmail.com>
2022-03-04 16:36:52 +0530

    Time: 98 ms (52.08%), Space: 10.2 MB (78.69%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
     
-    int superEggDrop(int K, int N) {
-        if(N==0||N==1) return N;     //Think for base case
-        if(K==1) return N;
-        
-        vector<vector<int>> dp(K+1,vector<int> (N+1,0));
-      for(int i=0;i<=K;i++)
-        dp[i][0]=0,dp[i][1]=1;   //Filling from base case as if N==0 ans=0 , N==1 ans=1
-      for(int i=0;i<=N;i++)
-        dp[0][i]=0,dp[1][i]=i;   //Filling from base case as if K==0 ans=0 , K==1 ans=N, number of current floor (i)
-        
-        for(int i=2;i<K+1;i++)
+    int m = pow(10,9);
+    int dp[10001][101];
+    int solve(int n, int eggs)
+    {
+        if(n<=1) return n;
+        if(eggs==1) return n;
+        if(dp[n][eggs]!=-1) return dp[n][eggs];
+        int res = m;
+        int low = 1;
+        int high = n;
+        while(low<=high)
         {
-            for(int j=2;j<N+1;j++)
-            {    int l=1,h=j,temp=0,ans=100000;
-                 while(l<=h)
-                 {
-                    int mid=(l+h)/2;
-                    int left= dp[i-1][mid-1]; //egg broken check for down floors of mid
-                    int right= dp[i][j-mid];  // not broken check for up floors of mid
-                    temp=1+max(left,right);
-                    if(left<right){
-                      l=mid+1;                       
-                    }
-                    else                             
-                    {    
-                        h=mid-1;
-                    }
-                    ans=min(ans,temp);               
-                 }
-                 dp[i][j]=ans;
-            }
+            int mid = (high+low)/2;
+            int left = solve(mid-1,eggs-1);
+            int right = solve(n-mid,eggs);
+            res = min(res,max(left,right)+1);
+            if(left<right) low = mid+1;
+            else high = mid-1;
         }
-        return dp[K][N];
-         
+        return dp[n][eggs] = res;
+    }
+    
+    int superEggDrop(int k, int n) {
+        memset(dp,-1,sizeof(dp));
+        return solve(n,k);
     }
 };
