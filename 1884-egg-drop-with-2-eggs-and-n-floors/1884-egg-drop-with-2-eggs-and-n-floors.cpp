fa65fb22002ac4f6c218877a6713704939d14564
Anand Tripathi <shadarsh23@gmail.com>
2022-03-04 16:33:41 +0530

    Time: 9 ms (57.10%), Space: 6 MB (63.48%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -2,21 +2,27 @@ class Solution {
 public:
     int m = pow(10,9);
     int dp[1001][3];
-    int solve(int s, int n, int eggs)
+    int solve(int n, int eggs)
     {
-        if(s>n) return 0;
-        if(eggs==0) return m;
-        if(dp[n-s+1][eggs]!=-1) return dp[n-s+1][eggs];
+        if(n<=1) return n;
+        if(eggs==1) return n;
+        if(dp[n][eggs]!=-1) return dp[n][eggs];
         int res = m;
-        for(int i=s; i<=n; i++)
+        int low = 1;
+        int high = n;
+        while(low<=high)
         {
-            int curr = max(solve(i+1,n,eggs),solve(s,i-1,eggs-1))+1;
-            res = min(res,curr);
+            int mid = (high+low)/2;
+            int left = solve(mid-1,eggs-1);
+            int right = solve(n-mid,eggs);
+            res = min(res,max(left,right)+1);
+            if(left<right) low = mid+1;
+            else high = mid-1;
         }
-        return dp[n-s+1][eggs] = res;
+        return dp[n][eggs] = res;
     }
     int twoEggDrop(int n) {
         memset(dp,-1,sizeof(dp));
-        return solve(1,n,2);
+        return solve(n,2);
     }
 };
