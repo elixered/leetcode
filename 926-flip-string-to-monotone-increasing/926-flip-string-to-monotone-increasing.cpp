7fffa722459c57a69e381ed7f6a2ea1309aac2d2
Anand Tripathi <shadarsh23@gmail.com>
2022-03-04 12:30:03 +0530

    Time: 28 ms (84.85%), Space: 11 MB (80.01%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
@@ -2,22 +2,16 @@ class Solution {
 public:
     int minFlipsMonoIncr(string s) {
         int n = s.size();
-        vector<int> zero(n,0),one(n,0);
-        if(s[0]=='0') zero[0]++;
-        else one[0]++;
-        for(int i=1; i<n; i++)
+        int one = 0;
+        int flip = 0;
+        for(auto c:s)
         {
-            zero[i] = zero[i-1];
-            one[i] = one[i-1];
-            s[i]=='0'?zero[i]++:one[i]++;
+            if(c=='1')
+                one++;
+            else
+                flip++;
+            flip = min(one,flip);
         }
-        int ans = n;
-        for(int i=0; i<n; i++)
-        {
-            int prevone = one[i];
-            int nextzero = zero[n-1]-zero[i];
-            ans = min(ans,prevone+nextzero);
-        }
-        return min(ans,zero[n-1]);
+        return min(flip,one);
     }
 };
