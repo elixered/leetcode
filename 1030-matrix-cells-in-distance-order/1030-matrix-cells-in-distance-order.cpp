af4cb984ceac0308cb86296c65c01689cf9a0be7
Anand Tripathi <shadarsh23@gmail.com>
2022-03-07 21:27:40 +0530

    Time: 69 ms (62.87%), Space: 28 MB (31.63%) - LeetHub

--- 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.cpp
+++ 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    struct cmp{
-        bool operator()(const vector<int>& v1, const vector<int>& v2)
-        {
-            return v1[2] < v2[2];
-        }
-    };
     
     vector<vector<int>> allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
         vector<vector<int>> ans;
+        vector<vector<vector<int>>> hm(210);
         for(int i=0; i<rows; i++)
         {
             for(int j=0; j<cols; j++)
             {
-                ans.push_back({i,j,abs(rCenter-i)+abs(cCenter-j)});
+                int k = abs(rCenter-i) + abs(cCenter-j);
+                hm[k].push_back({i,j});
             }
         }
-        sort(ans.begin(),ans.end(),cmp());
-        for(auto& row:ans)
-            row.pop_back();
+        for(int i=0; i<210; i++)
+        {
+            for(auto it:hm[i])
+                ans.push_back(it);
+        }
         return ans;
     }
 };
