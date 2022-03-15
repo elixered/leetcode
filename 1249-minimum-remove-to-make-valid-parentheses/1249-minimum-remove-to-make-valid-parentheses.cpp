163d42d972fd318af24e667bb7553cae1561f6fe
Anand Tripathi <shadarsh23@gmail.com>
2022-03-15 10:57:42 +0530

    Time: 32 ms (53.31%), Space: 11.6 MB (54.07%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -2,49 +2,31 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         string ans;
-        stack<char> st;
-        for(auto c:s)
+        stack<int> st;
+        int n = s.size();
+        for(int i=0; i<n; i++)
         {
-            if(c==')')
+            char ch = s[i];
+            if(ch=='(')
+            {
+                s[i] = '*';
+                st.push(i);
+            }
+            else if(ch==')')
             {
                 if(st.empty())
-                    continue;
+                    s[i] = '*';
                 else 
                 {
+                    auto j = st.top();
                     st.pop();
-                    ans += c;
+                    s[j] = '(';
                 }
             }
-            else
-            {
+        }
+        for(auto c:s)
+            if(c!='*')
                 ans += c;
-                if(c=='(')
-                    st.push(c);
-            }
-        }
-        string res;
-        stack<char> st1;
-        for(int i=ans.size()-1; i>=0; i--)
-        {
-            char c = ans[i];
-            if(c=='(')
-            {
-                if(st1.empty())
-                    continue;
-                else 
-                {
-                    st1.pop();
-                    res.push_back(c);
-                }
-            }
-            else
-            {
-                res.push_back(c);
-                if(c==')')
-                    st1.push(c);
-            }
-        }
-        reverse(res.begin(),res.end());
-        return res;
+        return ans;
     }
 };
