b3fc9b6d2e7188c3326356663f76260b5cca190a
Anand Tripathi <shadarsh23@gmail.com>
2022-04-04 18:51:45 +0530

    Time: 8 ms (29.47%), Space: 8.3 MB (93.24%) - LeetHub

--- 522-longest-uncommon-subsequence-ii/522-longest-uncommon-subsequence-ii.cpp
+++ 522-longest-uncommon-subsequence-ii/522-longest-uncommon-subsequence-ii.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    unordered_map<string,int> mp;
-    void solve(string& s, int idx, string curr)
-    {
-        if(idx>=s.size())
-        {
-            mp[curr]++;
-            return;
-        }
-        else
-        {
-            solve(s,idx+1,curr);
-            curr.push_back(s[idx]);
-            solve(s,idx+1,curr);
-        }
-    }
     int findLUSlength(vector<string>& strs) {
-        for(auto s:strs)
-        {
-            solve(s,0,"");
+        if (strs.empty()) return -1;
+        int rst = -1;
+        for(auto i = 0; i < strs.size(); ++i) {
+            int j = 0;
+            for(; j < strs.size(); ++j) {
+                if(i==j) continue;
+                if(LCS(strs[i], strs[j])) break;  // strs[j] is a subsequence of strs[i]
+            }
+            // strs[i] is not any subsequence of the other strings.
+            if(j==strs.size()) rst = max(rst, static_cast<int>(strs[i].size()));
         }
-        int ans = -1;
-        for(auto it:mp)
-        {
-            if(it.second==1)
-                ans = max(ans,(int)it.first.size());
+        return rst;
+    }
+    // iff a is a subsequence of b
+    bool LCS(const string& a, const string& b) {
+        if (b.size() < a.size()) return false;
+        int i = 0;
+        for(auto ch: b) {
+            if(i < a.size() && a[i] == ch) i++;
         }
-        return ans;
+        return i == a.size();
     }
 };
