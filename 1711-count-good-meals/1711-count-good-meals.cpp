3e1ea8070f3e5648e8056093e0da514bb3cd9fa5
Anand Tripathi <shadarsh23@gmail.com>
2022-04-04 19:19:14 +0530

    Time: 428 ms (76.30%), Space: 58 MB (64.19%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.cpp
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -1,35 +1,14 @@
 class Solution {
 public:
-    int countPairs(vector<int>& nums) {
-        int p = 1;
-        int ans = 0;
-        sort(nums.begin(),nums.end());
-        unordered_map<int,long> ump;
-        for(auto it:nums)
-            ump[it]++;
-        int m = pow(10,9)+7;
-        for(int k=1; k<=29; k++)
-        {
-            if(k>1) p *= 2;
-            int i=0, j = nums.size()-1;
-            while(i<j)
-            {
-                int s = nums[i]+nums[j];
-                if(s>p)
-                    j-=ump[nums[j]];
-                else if(s<p)
-                    i+=ump[nums[i]];
-                else
-                {
-                    if(nums[i]==nums[j])
-                        ans += (ump[nums[i]]*(ump[nums[i]]-1)/2)%m;
-                    else ans += ump[nums[i]]*ump[nums[j]];
-                    i += ump[nums[i]];
-                    j -= ump[nums[j]];
-                    ans %= m;
-                }
-            }    
+    int countPairs(vector<int>& a) {
+        unordered_map<int,int> lks;
+        long long ans=0;
+        for(int x : a){
+            for(int i=1;i<=(1<<22);i*=2){
+                if(lks.count(i-x)) ans+=lks[i-x];
+            }
+            lks[x]+=1;
         }
-        return ans;
+        return ans % (int)(1e9 + 7);
     }
 };
