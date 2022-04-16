b8bd1354d5b8b71e3a1f91ee0128964a07772aa2
Anand Tripathi <shadarsh23@gmail.com>
2022-04-16 21:37:44 +0530

    Time: 584 ms (100.00%), Space: 107.9 MB (100.00%) - LeetHub

---
+++ 2241-design-an-atm-machine/2241-design-an-atm-machine.cpp
@@ -0,0 +1,20 @@
+class ATM {
+public:
+    long long bank[5] = {}, val[5] = {20, 50, 100, 200, 500};
+    void deposit(vector<int> banknotesCount) {
+        for (int i = 0; i < 5; ++i)
+            bank[i] += banknotesCount[i];
+    }
+    vector<int> withdraw(int amount) {
+        vector<int> take(5);
+        for (int i = 4; i >= 0; --i) {
+            take[i] = min(bank[i], amount / val[i]);
+            amount -= take[i] * val[i];
+        }
+        if (amount)
+            return { -1 };
+        for (int i = 0; i < 5; ++i)
+            bank[i] -= take[i];            
+        return take;
+    }
+};
