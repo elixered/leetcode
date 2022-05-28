632667d310a95ba259bf0b01603149074184f917
Anand Tripathi <shadarsh23@gmail.com>
2022-05-28 03:54:22 +0000

    Time: 94 ms (21.79%), Space: 19.1 MB (56.93%) - LeetHub

--- 477-total-hamming-distance/477-total-hamming-distance.cpp
+++ 477-total-hamming-distance/477-total-hamming-distance.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> diff(32,vector<int>(2,0));
+        int ans = 0;
         for(int i=0; i<32; i++)
         {
-            for(auto it:nums)
+            int ones = 0, zeroes = 0;
+            for(auto n:nums)
             {
-                if((it&(1<<i))!=0)
-                    diff[i][1]++;
-                else diff[i][0]++;
+                if((n&(1<<i))!=0)
+                    ones++;
+                else zeroes++;
             }
+            ans += ones*zeroes;
         }
-        int ans = 0;
-        for(auto it:diff)
-            ans += it[0]*it[1];
         return ans;
     }
 };
