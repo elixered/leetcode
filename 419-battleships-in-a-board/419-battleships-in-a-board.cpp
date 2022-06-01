17b1fb7958280ca77375347a389943b7551a9fdf
Anand Tripathi <shadarsh23@gmail.com>
2022-06-01 06:16:41 +0000

    Time: 15 ms (24.70%), Space: 8.2 MB (53.58%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.cpp
+++ 419-battleships-in-a-board/419-battleships-in-a-board.cpp
@@ -1,58 +1,13 @@
 class Solution {
 public:
-    int countBattleships(vector<vector<char>>& board) {
-        int m = board.size();
-        int n = board[0].size();
-        int count = 0;
-        for(int i=0; i<m; i++)
-        {
-            int j = 0;
-            int c = 0;
-            while(j<n)
-            {
-                bool flag = true;
-                if(board[i][j]!='X') flag = false;
-                if(i<m-1 && board[i+1][j]=='X') flag = false;
-                if(i>0 && board[i-1][j]=='X') flag = false;
-                if(flag == false)
-                {
-                    j++;
-                    continue;
-                }
-                c++;
-                while(j<n && board[i][j]=='X')
-                {
-                    j++;
-                }
-            }
-            count += c;
-        }
-        for(int j=0; j<n; j++)
-        {
-            int i = 0;
-            int c = 0;
-            while(i<m)
-            {
-                bool flag = true;
-                if(board[i][j]!='X') flag = false;
-                if(j<n-1 && board[i][j+1]=='X') flag = false;
-                if(j>0 && board[i][j-1]=='X') flag = false;
-                if(flag==false)
-                {
-                    i++;
-                    continue;
-                }
-                c++;
-                int sz = 0;
-                while(i<m && board[i][j]=='X')
-                {
-                    i++;
-                    sz++;
-                }
-                if(sz==1) c--;
-            }
-            count += c;
-        }
-        return count;
-    }
+   int countBattleships(vector<vector<char>>& board) {
+        if (board.empty() || board[0].empty()) { return 0; }
+        int m = board.size(), n = board[0].size(), cnt = 0;
+        
+        for (int r = 0; r < m; r++)
+            for (int c = 0; c < n; c++)
+                cnt += board[r][c] == 'X' && (r == 0 || board[r - 1][c] != 'X') && (c == 0 || board[r][c - 1] != 'X');
+        
+        return cnt;
+}
 };
