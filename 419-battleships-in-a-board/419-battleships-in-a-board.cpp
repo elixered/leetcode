4b67583552a010342bee17ca0f231ad57c2e975b
Anand Tripathi <shadarsh23@gmail.com>
2022-06-01 06:13:06 +0000

    Time: 13 ms (37.68%), Space: 8 MB (87.14%) - LeetHub

---
+++ 419-battleships-in-a-board/419-battleships-in-a-board.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+        int count = 0;
+        for(int i=0; i<m; i++)
+        {
+            int j = 0;
+            int c = 0;
+            while(j<n)
+            {
+                bool flag = true;
+                if(board[i][j]!='X') flag = false;
+                if(i<m-1 && board[i+1][j]=='X') flag = false;
+                if(i>0 && board[i-1][j]=='X') flag = false;
+                if(flag == false)
+                {
+                    j++;
+                    continue;
+                }
+                c++;
+                while(j<n && board[i][j]=='X')
+                {
+                    board[i][j] = '0';
+                    j++;
+                }
+            }
+            count += c;
+        }
+        for(int j=0; j<n; j++)
+        {
+            int i = 0;
+            int c = 0;
+            while(i<m)
+            {
+                bool flag = true;
+                if(board[i][j]!='X') flag = false;
+                if(j<n-1 && board[i][j+1]=='X') flag = false;
+                if(j>0 && board[i][j-1]=='X') flag = false;
+                if(flag==false)
+                {
+                    i++;
+                    continue;
+                }
+                c++;
+                while(i<m && board[i][j]=='X')
+                    i++;
+            }
+            count += c;
+        }
+        return count;
+    }
+};
