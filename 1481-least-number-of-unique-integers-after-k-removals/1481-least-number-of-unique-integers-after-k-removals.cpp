0fcec033a1a3fe2024ef752066fa14ddbd469059
Anand Tripathi <shadarsh23@gmail.com>
2022-06-01 06:38:10 +0000

    Time: 313 ms (33.51%), Space: 62.6 MB (66.62%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-        int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
-        unordered_map<int, int> counter;
-        priority_queue<int, vector<int>, greater<>> pq;
-        
-        for (auto num : arr)
-            counter[num]++;
-        
-        for (auto it : counter)
-            pq.push(it.second);
-        
-        while (k) {
-            if (pq.top() > k)
-                break;
-            
-            k -= pq.top(); pq.pop();
-        }
-        
-        return pq.size();                    
+int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+    unordered_map<int, int> counts;
+    vector<int> heap;
+    for (auto n : arr)
+        ++counts[n];
+    for (auto &p: counts)
+        heap.push_back(p.second);
+    make_heap(begin(heap), end(heap), greater<int>());
+    while (k > 0) {
+        k -= heap.front();
+        pop_heap(begin(heap), end(heap), greater<int>()); 
+        if (k >= 0)
+            heap.pop_back();
     }
+    return heap.size();
+}
 };
