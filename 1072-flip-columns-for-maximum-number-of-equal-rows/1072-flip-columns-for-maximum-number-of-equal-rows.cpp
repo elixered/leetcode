611256be426006dc51a053e773a772861725a368
Anand Tripathi <shadarsh23@gmail.com>
2022-06-02 11:18:24 +0530

    Time: 340 ms (35.65%), Space: 78.6 MB (34.16%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.cpp
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
-    bool matched(vector<int>& a, vector<int>& b)
-    {
-        return a==b;
-    }
     int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>> mp0(m),mp1(m);
-        for(int i=0; i<m; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                if(matrix[i][j]==0)
-                    mp0[i].push_back(j);
-                else mp1[i].push_back(j);
+        unordered_map<string,int> hmap;
+        for(auto x: matrix){
+            string s=""; 
+            //s keeps track of relative ordering of elements in a row w.r.t the first element
+            int top=x[0];
+            for(int i=0;i<x.size();i++){
+                if(x[i]==top)
+                    s+='1';
+                else
+                    s+='0';
             }
+            hmap[s]++; 
         }
-        int ans = 0;
-        for(int i=0; i<m; i++)
-        {
-            int zero = 0, one = 0;
-            for(int j=0; j<m; j++)
-            {
-                if(matched(mp0[i],mp0[j]) or matched(mp0[i],mp1[j]))
-                    zero++;
-                if(matched(mp1[i],mp0[j]) or matched(mp1[i],mp1[j]))
-                    one++;
-            }
-            ans = max({ans,zero,one});
-        }
-        return ans;
+        
+        int result=0;
+        for(auto &[key,val]:hmap)
+            result=max(result,val); 
+        //rows that have identical relative ordering, can be simultaneously toggled columnwise in order to make all
+        //elements of any of those rows unique. So the answer is maximum number of rows with identical relative ordering.
+        return result;
     }
 };
