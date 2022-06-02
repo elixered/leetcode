98aaf775985d79617fe969097ae6a8ea8638c157
Anand Tripathi <shadarsh23@gmail.com>
2022-06-02 10:29:38 +0530

    Time: 20 ms (57.24%), Space: 10.6 MB (87.89%) - LeetHub

--- 1366-rank-teams-by-votes/1366-rank-teams-by-votes.cpp
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.cpp
@@ -1,41 +1,24 @@
-int mat[26][26];
-
-
-
-
-
 class Solution {
 public:
-   static bool comp_(char a, char b, int i){
-    if(i==26){
-        return a<b;
-    }
-    if(mat[a-'A'][i]>mat[b-'A'][i]){
-        return true;
-    }
-    if(mat[a-'A'][i]<mat[b-'A'][i]){
-        return false;
-    }
-    return comp_(a,b,i+1);
-    }
-    
-    static bool comp(char a, char b){
-    return comp_(a,b,0);
-}
     string rankTeams(vector<string>& votes) {
-        int n = votes[0].length();
-        for(int i=0;i<26;i++){
-            for(int j=0;j<26;j++)
-                mat[i][j]=0;
-        }
-        for(auto vote:votes){
-            for(int i=0;i<vote.size();i++){
-                mat[vote[i]-'A'][i]++;
+        string ans(votes[0]);
+        const int n(votes[0].length());
+        vector<vector<int>> ranks(26, vector<int>(n));
+        
+        for (const string& vote: votes)
+            for (int i = 0; i < n; ++i)
+                ranks[vote[i] - 'A'][i]++;
+
+        function<bool(const char&, const char&)> compare = [&](const char& x, const char& y) {
+            for (int i = 0; i < n; ++i) {
+                if (ranks[x - 'A'][i] == ranks[y - 'A'][i]) continue;
+                return ranks[x - 'A'][i] > ranks[y - 'A'][i];
             }
-        }
-        string s = votes[0];
-        sort(s.begin(),s.end(),comp);
-      
-        return s;
+            return x < y;
+        };
+        
+        sort(ans.begin(), ans.end(), compare);
+		
+        return ans;
     }
 };
