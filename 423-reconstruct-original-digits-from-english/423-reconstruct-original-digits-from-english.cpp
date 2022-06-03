1d5210cfc6494113d4b0b70143e4cf257de2afa7
Anand Tripathi <shadarsh23@gmail.com>
2022-06-03 10:52:10 +0530

    Time: 39 ms (15.49%), Space: 9 MB (36.14%) - LeetHub

--- 423-reconstruct-original-digits-from-english/423-reconstruct-original-digits-from-english.cpp
+++ 423-reconstruct-original-digits-from-english/423-reconstruct-original-digits-from-english.cpp
@@ -1,51 +1,20 @@
 class Solution {
 public:
     string originalDigits(string s) {
-        string ans;
-        map<char,int> mp;
-        for(auto c:s)
-            mp[c]++;
-        ans.append(mp['z'],'0');
-        int x = mp['z'];
-        for(auto i:"zero")
-            mp[i] -= x;
-        ans.append(mp['w'],'2');
-        x = mp['w'];
-        for(auto i:"two")
-            mp[i] -= x;
-        ans.append(mp['u'],'4');
-        x = mp['u'];
-        for(auto i:"four")
-            mp[i] -= x;
-        ans.append(mp['x'],'6');
-        x = mp['x'];
-        for(auto i:"six")
-            mp[i] -= x;
-        ans.append(mp['g'],'8');
-        x = mp['g'];
-        for(auto i:"eight")
-            mp[i] -= x;
-        ans.append(mp['o'],'1');
-        x = mp['o'];
-        for(auto i:"one")
-            mp[i] -= x;
-        ans.append(mp['r'],'3');
-        x = mp['r'];
-        for(auto i:"three")
-            mp[i] -= x;
-        ans.append(mp['f'],'5');
-        x = mp['f'];
-        for(auto i:"five")
-            mp[i] -= x;
-        ans.append(mp['s'],'7');
-        x = mp['s'];
-        for(auto i:"seven")
-            mp[i] -= x;
-        ans.append(mp['i'],'9');
-        x = mp['i'];
-        for(auto i:"nine")
-            mp[i] -= x;
-        sort(ans.begin(),ans.end());
-        return ans;
+        vector<string> words = {"zero", "two", "four", "six", "eight", "one", "three", "five", "seven", "nine"};
+        vector<int> nums = {0, 2, 4, 6, 8, 1, 3, 5, 7, 9};
+        vector<int> distinct_char = {'z', 'w', 'u', 'x', 'g', 'o', 'r', 'f', 'v', 'i'};
+        vector<int> counts(26, 0);
+        string result;
+        for(auto ch : s){ counts[ch-'a']++;}
+        for(int i = 0; i < 10; i++){
+            int count = counts[distinct_char[i]-'a'];
+            for(int j = 0; j < words[i].size(); j++)
+                counts[words[i][j]-'a'] -= count;
+            while(count--)
+                result += to_string(nums[i]);
+        }
+        sort(result.begin(), result.end());
+        return result;
     }
 };
