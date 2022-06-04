839bcd19f8139764477396d1dfc971fcb69d54a0
Anand Tripathi <shadarsh23@gmail.com>
2022-06-04 20:47:38 +0530

    Time: 1091 ms (5.04%), Space: 199.1 MB (5.04%) - LeetHub

--- 2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.cpp
+++ 2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.cpp
@@ -8,7 +8,7 @@ public:
         for(int i=1; i<nums.size()-1; i++)
         {
             after.erase(after.find(nums[i]));
-            if(after.upper_bound(nums[i])==after.begin() && before.lower_bound(nums[i])==before.end())
+            if(*after.begin()>nums[i] && *before.rbegin()<nums[i])
                 ans+=2;
             else if(nums[i-1]<nums[i] && nums[i]<nums[i+1])
                 ans++;
