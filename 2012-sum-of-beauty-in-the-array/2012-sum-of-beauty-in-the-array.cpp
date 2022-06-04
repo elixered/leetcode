c729e646ee2a551156d640f0b7defd38284f9524
Anand Tripathi <shadarsh23@gmail.com>
2022-06-04 20:54:56 +0530

    Time: 223 ms (38.37%), Space: 81.9 MB (99.28%) - LeetHub

--- 2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.cpp
+++ 2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     int sumOfBeauties(vector<int>& nums) {
         int n = nums.size();
-        vector<int> minimum = nums;
+        vector<bool> minimum(n);
+        int mini = nums[n-1];
         int maximum = nums[0];
-        for(int i=1; i<n; i++)
+        for(int i=n-2; i>-0; i--)
         {
-            minimum[n-i-1] = min(minimum[n-1-i],minimum[n-i]);
+            if(mini <= nums[i])
+                minimum[i] = false;
+            else minimum[i] = true;
+            mini = min(mini,nums[i]);
         }
         int ans = 0;
         for(int i=1; i<n-1; i++)
         {
-            if(nums[i]>maximum && nums[i]<minimum[i+1])
+            if(nums[i]>maximum && minimum[i])
                 ans+=2;
             else if(nums[i]>nums[i-1] && nums[i]<nums[i+1])
                 ans+=1;
