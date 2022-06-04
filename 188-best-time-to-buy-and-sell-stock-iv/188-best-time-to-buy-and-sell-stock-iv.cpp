1fd4e8a141773f7242cf986d9ac66f869201b32c
Anand Tripathi <shadarsh23@gmail.com>
2022-06-04 23:07:58 +0530

    Time: 6 ms (88.09%), Space: 10.8 MB (82.29%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    int maxProfit(int K, vector<int>& prices) {
-        if(prices.size()<=1) return 0;
-        int n = prices.size();
-        vector<vector<int>> bought(n,vector<int>(K+1,0));
-        vector<vector<int>> sold(n,vector<int>(K+1,0));
-        for(int k=1;k<=K; k++)
-            bought[0][k] = -prices[0];
-        
-        for(int k=1; k<=K; k++)
-        {
-            for(int i=1; i<n; i++)
-            {
-                bought[i][k] = max(bought[i-1][k],sold[i][k-1]-prices[i]);
-                sold[i][k] = max(sold[i-1][k],bought[i-1][k]+prices[i]);
-            }
-        }
-        return sold[n-1][K];
+    int maxProfit(vector<int>& p, int res = 0) {
+  for (int i = 1; i < p.size(); ++i) res += max(0, p[i] - p[i - 1]);
+  return res;
+}
+int maxProfit(int k, vector<int>& prices) {
+  if (k >= prices.size() / 2) return maxProfit(prices);
+  vector<int> buys(k + 1, INT_MIN), sells(k + 1, 0);
+  for (auto p : prices)
+    for (auto i = 1; i <= k; ++i) {
+      buys[i] = max(buys[i], sells[i - 1] - p);
+      sells[i] = max(sells[i], buys[i] + p);  
     }
+  return sells[k];
+}
 };
