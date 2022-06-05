7da728b40953fcfb54e7568564282d923eecbd21
Anand Tripathi <shadarsh23@gmail.com>
2022-06-05 13:33:38 +0530

    Time: 3 ms (88.84%), Space: 7.4 MB (65.99%) - LeetHub

--- 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.cpp
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.cpp
@@ -1,42 +1,23 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* prev;
-    Node* next;
-    Node* child;
-};
-*/
 
 class Solution {
 public:
     Node* flatten(Node* head) {
-        if(head==NULL) return NULL;
-        Node* chi = flatten(head->child);
-        Node* nex = flatten(head->next);
-        head->child = NULL;
-        if(!chi && !nex) return head;
-        else
-        if(chi==NULL)
+        Node* h = head;
+        for(h; h!=NULL; h = h->next)
         {
-            head->next = nex;
-            nex->prev = head;
-        }
-        else if(nex==NULL)
-        {
-            head->next = chi;
-            chi->prev = head;
-        }
-        else
-        {
-            head->next = chi;
-            chi->prev = head;
-            Node* last = chi;
-            while(last->next)
-                last = last->next;
-            last->next = nex;
-            nex->prev = last;
+            if(h->child)
+            {
+                Node* temp = h->next;
+                h->next = h->child;
+                h->child = NULL;
+                h->next->prev = h;
+                Node* last = h->next;
+                while(last->next)
+                    last = last->next;
+                last->next = temp;
+                if(temp)
+                temp->prev = last;
+            }
         }
         return head;
     }
