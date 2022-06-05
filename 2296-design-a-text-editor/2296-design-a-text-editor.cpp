53f3d3add00683422b9cc5dccd6972137edbf3d0
Anand Tripathi <shadarsh23@gmail.com>
2022-06-05 10:08:55 +0530

    Time: 464 ms (11.11%), Space: 109.7 MB (33.33%) - LeetHub

--- 2296-design-a-text-editor/2296-design-a-text-editor.cpp
+++ 2296-design-a-text-editor/2296-design-a-text-editor.cpp
@@ -1,57 +1,70 @@
 class TextEditor {
-    stack<char> left;
-    stack<char> right;
 public:
+    stack<char> before,after;
     TextEditor() {
         
     }
     
     void addText(string text) {
-        for(auto &c : text){
-            left.push(c);
-        }
+        for(int i=0; i<text.size(); i++)
+            before.push(text[i]);
     }
     
     int deleteText(int k) {
-        int cnt=0;
-        while(!left.empty() and k>0){
-            left.pop();
-            cnt++;
+        int x = 0;
+        while(!before.empty() && k>0)
+        {
             k--;
+            before.pop();
+            x++;
         }
-        return cnt;
+        return x;
     }
     
     string cursorLeft(int k) {
-        while(!left.empty() and k>0){
-            char c = left.top();left.pop();
-            right.push(c);
+        while(!before.empty() && k>0)
+        {
             k--;
+            after.push(before.top());
+            before.pop();
         }
-        return cursorShiftString();
+        string ans = "";
+        while(!before.empty() && ans.size()<10)
+        {
+            ans.push_back(before.top());
+            before.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        for(auto it:ans)
+            before.push(it);
+        return ans;
     }
     
     string cursorRight(int k) {
-        while(!right.empty() and k>0){
-            char c = right.top();right.pop();
-            left.push(c);
+        while(!after.empty() && k>0)
+        {
             k--;
+            before.push(after.top());
+            after.pop();
         }
-        return cursorShiftString();
-    }
-    
-    string cursorShiftString(){
-        string rtn = "";
-        int cnt=10;
-        while(!left.empty() and cnt>0){
-            char c = left.top();left.pop();
-            rtn += c;
-            cnt--;
+        string ans = "";
+        while(!before.empty() && ans.size()<10)
+        {
+            ans.push_back(before.top());
+            before.pop();
         }
-        reverse(rtn.begin(),rtn.end());
-        for(int i=0;i<rtn.size();i++){
-            left.push(rtn[i]);
-        }
-        return rtn;
+        reverse(ans.begin(),ans.end());
+        for(auto it:ans)
+            before.push(it);
+        return ans;
     }
 };
+
+/**
+ * Your TextEditor object will be instantiated and called as such:
+ * TextEditor* obj = new TextEditor();
+ * obj->addText(text);
+ * int param_2 = obj->deleteText(k);
+ * string param_3 = obj->cursorLeft(k);
+ * string param_4 = obj->cursorRight(k);
+ */
