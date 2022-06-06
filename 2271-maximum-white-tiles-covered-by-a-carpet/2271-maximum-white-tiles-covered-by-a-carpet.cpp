6af31b16770ae5ec70ba4fe4881f3fadd803d2ec
Anand Tripathi <shadarsh23@gmail.com>
2022-06-06 10:56:22 +0530

    Time: 367 ms (94.67%), Space: 69.9 MB (65.30%) - LeetHub

--- 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
+++ 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
-    
-    int upper(vector<vector<int>>& tiles, int e)
-    {
-        int low = 0;
-        int high = tiles.size()-1;
-        int ans = tiles.size();
-        while(low <= high)
-        {
-            int mid = low + (high-low)/2;
-            if(tiles[mid][1]>e)
-            {
-                ans = mid;
-                high = mid-1;
-            }
-            else low = mid+1;
-        }
-        return ans;
-    }
-    
     int maximumWhiteTiles(vector<vector<int>>& tiles, int k) {
         sort(tiles.begin(),tiles.end());
-        int n = tiles.size();
-        vector<int> p(n);
-        p[0] = tiles[0][1]-tiles[0][0]+1;
-        for(int i=1; i<n; i++)
-            p[i] += p[i-1] + tiles[i][1]-tiles[i][0]+1;
-        int ans = 0;
-        for(int i=0; i<n; i++)
+        int res = 0, j = 0, cover = 0;
+        for(int i=0; i<tiles.size() && res<k; )
         {
-            int s = tiles[i][0];
-            int e = s+k-1;
-            int idx = upper(tiles,e);
-            int curr = idx==0?0:p[idx-1];
-            if(i!=0)
-                curr -= p[i-1];
-            if(idx!=n)
-                curr += max(0,e-tiles[idx][0]+1);
-            ans = max(ans,curr);
+            if(tiles[j][0] + k > tiles[i][1])
+            {
+                cover += tiles[i][1] - tiles[i][0] + 1;
+                res = max(res, cover);
+                ++i;
+            }
+            else
+            {
+                int partial = max(0,tiles[j][0]+k-tiles[i][0]);
+                res = max(res,cover+partial);
+                cover -= (tiles[j][1]-tiles[j][0]+1);
+                ++j;
+            }
         }
-        return ans;
+        return res;
     }
 };
