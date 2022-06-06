0fb08df48e87a6fd6ce811338d99109c7e07c67a
Anand Tripathi <shadarsh23@gmail.com>
2022-06-06 21:46:48 +0530

    Time: 11 ms (58.17%), Space: 6 MB (50.30%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    string intToRoman(int num) {
-        map<int,string,greater<int>> mp = {
-            {1000,"M"}, {900,"CM"}, {500,"D"}, {400,"CD"}, {100,"C"}, {90,"XC"}, {50,"L"}, {40,"XL"}, {10,"X"}, {9,"IX"}, {5,"V"}, {4,"IV"}, {1,"I"}};
-            string ans;
-            for(auto it:mp)
+    string intToRoman(int num) 
+    {
+        string res;
+        string sym[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        
+        for(int i=0; num != 0; i++)
+        {
+            while(num >= val[i])
             {
-                int x = num/it.first;
-                for(int i=0; i<x; i++)
-                    ans += it.second;
-                num %= it.first;
+                num -= val[i];
+                res += sym[i];
             }
-            return ans;
+        }
+        
+        return res;
     }
 };
