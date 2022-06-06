a104ddf4d7c71b0185b535556a305bc771d1b958
Anand Tripathi <shadarsh23@gmail.com>
2022-06-06 21:47:50 +0530

    Time: 4 ms (89.51%), Space: 6.1 MB (50.30%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
-    string intToRoman(int num) 
-    {
-        string res;
-        string sym[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    string intToRoman(int num) {
+        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string ths[]={"","M","MM","MMM"};
         
-        for(int i=0; num != 0; i++)
-        {
-            while(num >= val[i])
-            {
-                num -= val[i];
-                res += sym[i];
-            }
-        }
-        
-        return res;
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
     }
 };
