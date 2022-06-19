843fe23412d4f6b1f1595ad070d00fd198ff5b0c
Anand Tripathi <shadarsh23@gmail.com>
2022-06-19 11:00:02 +0530

    Time: 3 ms (52.89%), Space: 6.3 MB (57.43%) - LeetHub

--- 791-custom-sort-string/791-custom-sort-string.cpp
+++ 791-custom-sort-string/791-custom-sort-string.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        unordered_map<char,int> mp;
-        for(int i=0; i<order.size(); i++)
-            mp[order[i]] = i;
-        auto cmp = [&mp](char a, char b){
-            return mp[a] < mp[b];
-        };
-        sort(s.begin(),s.end(),cmp);
-        return s;
+        vector<int> count(26,0);
+        for(auto c:s)
+            count[c-'a']++;
+        string ans;
+        for(auto c:order){
+            if(count[c-'a']){
+                ans += string(count[c-'a'],c);
+                count[c-'a'] = 0;
+            }
+        }
+        for(int i=0; i<26; i++){
+            if(count[i]){
+                ans += string(count[i],(i+'a'));
+            }
+        }
+        return ans;
     }
 };
