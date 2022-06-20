dc9685e839cfe8c350783c019b816801225a80a9
Anand Tripathi <shadarsh23@gmail.com>
2022-06-20 11:29:56 +0530

    Time: 6 ms (68.71%), Space: 7.7 MB (43.83%) - LeetHub

--- 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.cpp
+++ 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.cpp
@@ -1,95 +1,38 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-
-    Node() {}
-
-    Node(int _val) {
-        val = _val;
-        left = NULL;
-        right = NULL;
-    }
-
-    Node(int _val, Node* _left, Node* _right) {
-        val = _val;
-        left = _left;
-        right = _right;
-    }
-};
-*/
-
 class Solution {
-public:
-    Node* treeToDoublyList(Node* root) {
-        if(!root) return root;
-        Node* prev = NULL;
-        Node *first=NULL, *last=NULL;
-        stack<Node*> st;
-        while(root or !st.empty()){
-            while(root){
-                st.push(root);
-                root = root->left;
-            }
-            auto curr = st.top();
-            st.pop();
-            if(prev){
-                prev->right = curr;
-                curr->left = prev;
-            }
-            else first = curr;
-            prev = curr;
-            root = curr->right;
-        }
-        last = first;
-        Node* head = first;
-        while(last && last->right){
-            if(last->val < head->val)
-                head = last;
-            last = last->right;
-        }
-        first->left = last;
-        last->right = first;
-        return head;
+  public:
+  // the smallest (first) and the largest (last) nodes
+  Node* first = NULL;
+  Node* last = NULL;
+
+  void helper(Node* node) {
+    if (node) {
+      // left
+      helper(node->left);
+      // node 
+      if (last) {
+        // link the previous node (last)
+        // with the current one (node)
+        last->right = node;
+        node->left = last;
+      }
+      else {
+        // keep the smallest node
+        // to close DLL later on
+        first = node;
+      }
+      last = node;
+      // right
+      helper(node->right);
     }
+  }
+
+  Node* treeToDoublyList(Node* root) {
+    if (!root) return NULL;
+
+    helper(root);
+    // close DLL
+    last->right = first;
+    first->left = last;
+    return first;
+  }
 };
-// class Solution {
-// public:
-//     Node* treeToDoublyList(Node* root) {
-//         if (!root) { return root; }
-        
-//         stack<Node*> nodes;
-//         // we need to keep track of first and last nodes
-//         Node* first = nullptr;
-//         Node* last = nullptr;
-
-//         // standard iterative in-order traversal using a stack
-//         while (root || !empty(nodes)) {
-//             while (root) {
-//                 nodes.emplace(root);
-//                 root = root->left;
-//             }
-
-//             root = nodes.top();
-//             nodes.pop();
-            
-//             if (last) {
-//                 last->right = root;
-//                 root->left = last;
-//             } else {
-//                 first = root;  // only set once
-//             }
-//             last = root; // keep track of last node
-//             root = root->right;
-//         }
-        
-//         // connect first/last to make list circular
-//         first->left = last;
-//         last->right = first;
-        
-//         return first;
-//     }
-// };
