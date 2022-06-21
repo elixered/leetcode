e69349cdd984dfaf491abc1f63d378681f764ef1
Anand Tripathi <shadarsh23@gmail.com>
2022-06-21 11:49:59 +0530

    Time: 88 ms (82.78%), Space: 40.3 MB (60.80%) - LeetHub

--- 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
+++ 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-   int maxTurbulenceSize(vector<int>& A) {
-        vector<vector<int>> dp(A.size(), vector<int>(2, 1));
-        int res = 1;
-        for (int i = 1; i < A.size(); i++) {
-            if (A[i] > A[i - 1])
-                dp[i][0] = dp[i - 1][1] + 1;
-            else if (A[i] < A[i - 1]) 
-                dp[i][1] = dp[i - 1][0] + 1;
-            res = max(res, max(dp[i][0], dp[i][1]));   
+    int maxTurbulenceSize(vector<int>& A) {
+        int increase = 1; int decrease = 1; int max_len = 1;
+        for(int i=0; i < A.size()-1; i++){
+            if(A[i] > A[i+1]){
+                increase = decrease + 1;
+                decrease = 1;
+            }
+            else if(A[i] < A[i+1]){
+                decrease = increase + 1;
+                increase = 1;
+            }
+            else{
+                increase = 1;
+                decrease = 1;
+            }
+            max_len = max(max_len, max(increase, decrease));
         }
-        return res;
+        return max_len;
     }
-
 };
