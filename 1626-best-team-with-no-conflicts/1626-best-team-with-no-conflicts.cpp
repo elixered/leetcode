0dd50804ff40b806603991df738dd33e8e008de4
Anand Tripathi <shadarsh23@gmail.com>
2022-06-22 23:17:11 +0530

    Time: 216 ms (70.03%), Space: 19.8 MB (66.87%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,34 +1,23 @@
-typedef pair<int,int> pii;
 class Solution {
-    static bool cmp(pii p1,pii p2)
-    {
-        if(p1.second==p2.second)
-            return p1.first<p2.first;//if age is same sort by score to order weaker peer before stronger peer
-        return p1.second<p2.second;//sort by ages
-    }
 public:
+    
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
-       vector<pii> psa;
-        int n=scores.size(),i,j;
-        for( i=0;i<n;i++)
-        {
-            psa.push_back({scores[i],ages[i]});
+        vector<pair<int,int>> p;
+        int n = ages.size();
+        vector<int> dp(n,0);
+        for(int i=0; i<n; i++)
+            p.push_back({ages[i],scores[i]});
+        sort(begin(p),end(p));
+        for(int i=0; i<n; i++)
+            dp[i] = p[i].second;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(p[j].second>=p[i].second)
+                {
+                    dp[j] = max(dp[j],dp[i]+p[j].second);
+                }
+            }
         }
-        sort(psa.begin(),psa.end(),cmp);
-        
-         int lis[n]; 
-   
-   
-    for (int i = 0; i < n; i++ )  
-    { 
-        lis[i] = psa[i].first; 
-        for (int j = 0; j < i; j++ )   
-            if ( (psa[i].first >= psa[j].first  )&&lis[i]<lis[j]+psa[i].first)  
-                lis[i] = lis[j] + psa[i].first; 
-      
-    } 
-  
-    // Return maximum value in lis[] 
-    return *max_element(lis, lis+n); 
+        return *max_element(dp.begin(),dp.end());
     }
 };
