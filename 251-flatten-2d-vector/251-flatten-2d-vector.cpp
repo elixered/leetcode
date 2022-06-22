7f30ff0a0800ddea6b7e0eb3d410e284f79bf2fb
Anand Tripathi <shadarsh23@gmail.com>
2022-06-22 14:47:16 +0530

    Time: 53 ms (9.98%), Space: 19.5 MB (70.06%) - LeetHub

--- 251-flatten-2d-vector/251-flatten-2d-vector.cpp
+++ 251-flatten-2d-vector/251-flatten-2d-vector.cpp
@@ -1,32 +1,20 @@
 class Vector2D {
+    vector<vector<int>>::iterator i, iEnd;
+    int j = 0;
 public:
-    stack<pair<vector<int>::iterator,vector<int>::iterator>> st;
-    Vector2D(vector<vector<int>>& vec) {
-        for(int i=vec.size()-1; i>=0; i--){
-            auto& it = vec[i];
-            if(it.begin() != it.end())
-            st.push({it.begin(),it.end()});
-        }
+    Vector2D(vector<vector<int>>& vec2d) {
+        i = vec2d.begin();
+        iEnd = vec2d.end();
     }
-    
+
     int next() {
-        auto f = st.top().first;
-        auto e =st.top().second;
-        st.pop();
-        if(f+1 != e){
-            st.push({f+1,e});
-        }
-        return *f;
+        hasNext();
+        return (*i)[j++];
     }
-    
+
     bool hasNext() {
-        return st.size() > 0;
+        while (i != iEnd && j == (*i).size())
+            i++, j = 0;
+        return i != iEnd;
     }
 };
-
-/**
- * Your Vector2D object will be instantiated and called as such:
- * Vector2D* obj = new Vector2D(vec);
- * int param_1 = obj->next();
- * bool param_2 = obj->hasNext();
- */
