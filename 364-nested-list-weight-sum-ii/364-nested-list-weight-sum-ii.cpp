f62a4e96f6e0fe8088bac2ceae715eaaf81f81ce
Anand Tripathi <shadarsh23@gmail.com>
2022-06-22 13:15:10 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (53.09%) - LeetHub

--- 364-nested-list-weight-sum-ii/364-nested-list-weight-sum-ii.cpp
+++ 364-nested-list-weight-sum-ii/364-nested-list-weight-sum-ii.cpp
@@ -1,50 +1,21 @@
-/**
- * // This is the interface that allows for creating nested lists.
- * // You should not implement it, or speculate about its implementation
- * class NestedInteger {
- *   public:
- *     // Constructor initializes an empty nested list.
- *     NestedInteger();
- *
- *     // Constructor initializes a single integer.
- *     NestedInteger(int value);
- *
- *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
- *     bool isInteger() const;
- *
- *     // Return the single integer that this NestedInteger holds, if it holds a single integer
- *     // The result is undefined if this NestedInteger holds a nested list
- *     int getInteger() const;
- *
- *     // Set this NestedInteger to hold a single integer.
- *     void setInteger(int value);
- *
- *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
- *     void add(const NestedInteger &ni);
- *
- *     // Return the nested list that this NestedInteger holds, if it holds a nested list
- *     // The result is undefined if this NestedInteger holds a single integer
- *     const vector<NestedInteger> &getList() const;
- * };
- */
 class Solution {
 public:
-    int maxDepth = 1;
-    int products = 0;
-    int sum = 0;
-    void depthSum(vector<NestedInteger>& nestedList, int depth) {
-        maxDepth = max(maxDepth,depth);
-        for(auto it:nestedList){
-            if(it.isInteger()){
-                products += depth*it.getInteger();
-                sum += it.getInteger();
-            }
-            else depthSum(it.getList(),depth+1);
-        }
-        return;
-    }
     int depthSumInverse(vector<NestedInteger>& nestedList) {
-        depthSum(nestedList,1);
-        return maxDepth*sum + sum - products;
+        int unweighted = 0, weighted = 0;
+        while(!nestedList.empty()){
+            vector<NestedInteger> tmp;
+            for(auto& iter : nestedList){
+                if(iter.isInteger())
+                    unweighted += iter.getInteger();
+                else{
+                    vector<NestedInteger>& t = iter.getList();
+                    for(int i = 0; i < t.size(); i++)
+                        tmp.push_back(t[i]);
+                }
+            }
+            weighted += unweighted;
+            nestedList = tmp;
+        }
+        return weighted;
     }
 };
