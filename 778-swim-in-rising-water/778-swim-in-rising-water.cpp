836e732ea6a9dfaab4f749bd3a8480c2e325a2ac
Anand Tripathi <shadarsh23@gmail.com>
2022-06-23 18:49:26 +0530

    Time: 121 ms (11.31%), Space: 11.3 MB (29.70%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.cpp
+++ 778-swim-in-rising-water/778-swim-in-rising-water.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
-    vector<int> dir = {-1,0,1,0,-1};
-    bool dfs(vector<vector<int>>& grid, int x, int y, int mid,vector<vector<int>>& visited){
-        int n = grid.size();
-        visited[x][y] = 1;
-        for(int i=0; i<4; i++){
-            int nx = x+dir[i];
-            int ny = y+dir[i+1];
-            if(nx>=0 && ny>=0 && nx<n && ny<n && !visited[nx][ny] && grid[nx][ny]<=mid){
-                if(nx==n-1 && ny==n-1)
-                    return true;
-                if(dfs(grid,nx,ny,mid,visited))
-                    return true;
-            }
-        }
-        return false;
-    }
-    
     int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int low = grid[0][0];
-        int high =n*n-1;
-        int ans = n*n-1;
-        while(low <= high){
-            int mid = (low+high)/2;
-            vector<vector<int>> visited(n, vector<int>(n, 0));
-            if( dfs(grid,0,0,mid,visited)){
-                ans = mid;
-                high = mid-1;
+        int n = grid.size(), ans = max(grid[0][0], grid[n-1][n-1]);
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        vector<vector<int>> visited(n, vector<int>(n, 0));
+        visited[0][0] = 1;
+        vector<int> dir({-1, 0, 1, 0, -1});
+        pq.push({ans, 0, 0});
+        while (!pq.empty()) {
+            auto cur = pq.top();
+            pq.pop();
+            if (cur[1] == n-1 && cur[2] == n-1) return ans;
+            ans = max(ans, cur[0]);
+            for (int i = 0; i < 4; ++i) {
+                int r = cur[1] + dir[i], c = cur[2] + dir[i+1];
+                if (r >= 0 && r < n && c >= 0 && c < n && visited[r][c] == 0) {
+                    
+                    pq.push({grid[r][c], r, c});
+                    visited[r][c] = 1;
+                }
             }
-            else low = mid+1;
         }
-        return ans;
+        return -1;
     }
 };
