f2432dd725263fbe77dad0244a7f3a8c8243798f
Anand Tripathi <shadarsh23@gmail.com>
2022-06-23 12:38:44 +0530

    Time: 14 ms (56.63%), Space: 8.6 MB (44.30%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.cpp
+++ 227-basic-calculator-ii/227-basic-calculator-ii.cpp
@@ -1,58 +1,39 @@
 class Solution {
 public:
     int calculate(string s) {
-        vector<string> eq;
-        int n = s.size();
-        for(int i=0; i<n; i++){
-            if(s[i]==' ') continue;
-            string temp;
-            temp.push_back(s[i]);
-            if(!isdigit(s[i]))
-                eq.push_back(temp);
-            else{
-                while(i+1<n && isdigit(s[i+1])){
-                temp.push_back(s[i+1]);
-                i++;
+        int len = s.length();
+        if (len == 0) return 0;
+        stack<int> stack;
+        int currentNumber = 0;
+        char operation = '+';
+        for (int i = 0; i < len; i++) {
+            char currentChar = s[i];
+            if (isdigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            if (!isdigit(currentChar) && !iswspace(currentChar) || i == len - 1) {
+                if (operation == '-') {
+                    stack.push(-currentNumber);
+                } else if (operation == '+') {
+                    stack.push(currentNumber);
+                } else if (operation == '*') {
+                    int stackTop = stack.top();
+                    stack.pop();
+                    stack.push(stackTop * currentNumber);
+                } else if (operation == '/') {
+                    int stackTop = stack.top();
+                    stack.pop();
+                    stack.push(stackTop / currentNumber);
                 }
-            eq.push_back(temp);
+                operation = currentChar;
+                currentNumber = 0;
             }
         }
-        int ans = 0;
-        stack<string> st;
-        vector<string> v = {"/","*"};
-            for(auto c:eq){
-                if(!st.empty() && (st.top()==v[0] or st.top()==v[1])){
-                    string top = st.top();
-                    st.pop();
-                    int first = stoi(st.top());
-                    st.pop();
-                    int second = stoi(c);
-                    string curr;
-                    if(top==v[0])
-                        curr = to_string(first/second);
-                    else 
-                        curr = to_string(first*second);
-                    st.push(curr);
-                }
-                else st.push(c);
-            }
-            vector<string> temp;
-            while(!st.empty())
-            {
-                temp.push_back(st.top());
-                st.pop();
-            }
-            eq = temp;
-            reverse(eq.begin(),eq.end());
-        for(int i=0; i<eq.size(); i++){
-            if(eq[i]=="+" or eq[i]=="-"){
-                if(eq[i]=="+")
-                    ans += stoi(eq[i+1]);
-                else ans -= stoi(eq[i+1]);
-                i++;
-            }
-        else ans += stoi(eq[i]);
+        int result = 0;
+        while (stack.size() != 0) {
+            result += stack.top();
+            stack.pop();
         }
-        return ans;
+        return result;
     }
 };
