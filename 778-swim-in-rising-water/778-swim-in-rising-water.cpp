0c8832be3db9d0c7211d94a7c6df1b0a8868b953
Anand Tripathi <shadarsh23@gmail.com>
2022-06-23 18:42:42 +0530

    Time: 21 ms (81.45%), Space: 10.5 MB (48.21%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.cpp
+++ 778-swim-in-rising-water/778-swim-in-rising-water.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    vector<int> dir = {-1,0,1,0,-1};
-    bool dfs(vector<vector<int>>& grid, int x, int y, int mid,vector<vector<int>>& visited){
+    int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
-        visited[x][y] = 1;
-        for(int i=0; i<4; i++){
-            int nx = x+dir[i];
-            int ny = y+dir[i+1];
-            if(nx>=0 && ny>=0 && nx<n && ny<n && !visited[nx][ny] && grid[nx][ny]<=mid){
-                if(nx==n-1 && ny==n-1)
-                    return true;
-                if(dfs(grid,nx,ny,mid,visited))
-                    return true;
+        int low = grid[0][0], hi = n*n-1;
+        while (low < hi) {
+            int mid = low + (hi-low)/2;
+            if (valid(grid, mid)) 
+               hi = mid;
+            else
+               low = mid+1;
+        }
+        return low;
+    }
+private:
+    bool valid(vector<vector<int>>& grid, int waterHeight) {
+        int n = grid.size();
+        vector<vector<int>> visited(n, vector<int>(n, 0));
+        vector<int> dir({-1, 0, 1, 0, -1});
+        return dfs(grid, visited, dir, waterHeight, 0, 0, n);
+    }
+    bool dfs(vector<vector<int>>& grid, vector<vector<int>>& visited, vector<int>& dir, int waterHeight, int row, int col, int n) {
+        visited[row][col] = 1;
+        for (int i = 0; i < 4; ++i) {
+            int r = row + dir[i], c = col + dir[i+1];
+            if (r >= 0 && r < n && c >= 0 && c < n && visited[r][c] == 0 && grid[r][c] <= waterHeight) {
+                if (r == n-1 && c == n-1) return true;
+                if (dfs(grid, visited, dir, waterHeight, r, c, n)) return true;
             }
         }
         return false;
-    }
-    
-    int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int low = 0;
-        int high =n*n-1;
-        int ans = n*n-1;
-        while(low <= high){
-            int mid = (low+high)/2;
-            vector<vector<int>> visited(n, vector<int>(n, 0));
-            if(grid[0][0]<=mid && dfs(grid,0,0,mid,visited)){
-                ans = mid;
-                high = mid-1;
-            }
-            else low = mid+1;
-        }
-        return ans;
     }
 };
