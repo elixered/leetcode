43a51c9f6bb29080711f8389ba7a8be8c4843ddb
Anand Tripathi <shadarsh23@gmail.com>
2022-06-23 17:42:38 +0530

    Time: 298 ms (33.33%), Space: 41.4 MB (59.65%) - LeetHub

--- 1168-optimize-water-distribution-in-a-village/1168-optimize-water-distribution-in-a-village.cpp
+++ 1168-optimize-water-distribution-in-a-village/1168-optimize-water-distribution-in-a-village.cpp
@@ -1,37 +1,65 @@
 class Solution {
 public:
-    typedef pair<int,int> ppi;
+    class UnionFind {
+public:
+    UnionFind(int sz) : root(sz), rank(sz) {
+        size = sz;
+        for (int i = 0; i < sz; i++) {
+            root[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+
+    void connect(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+        }
+        size--;
+    }
+
+    bool connected(int x, int y) {
+        return find(x) == find(y);
+    }
+        
+        int getSize()
+        {
+            return size;
+        }
+
+private:
+    int size;
+    vector<int> root;
+    vector<int> rank;
+};
     int minCostToSupplyWater(int n, vector<int>& wells, vector<vector<int>>& pipes) {
-        vector<vector<ppi>> adj(n+1);
-        for(auto& pipe : pipes){
-            adj[pipe[0]].push_back({pipe[1],pipe[2]});
-            adj[pipe[1]].push_back({pipe[0],pipe[2]});
-        }
-        for(int i=1; i<=n; i++){
-            adj[0].push_back({i,wells[i-1]});
-        }
-        priority_queue<ppi,vector<ppi>,greater<ppi>> pq;
-        vector<int> mstSet(n+1,0);
-        vector<int> minDist(n+1,INT_MAX);
-        minDist[0] = 0;
-        pq.push({0,0});
-        int size = 0;
+        for(int i=0; i<n; i++)
+            pipes.push_back({0,i+1,wells[i]});
+        sort(pipes.begin(),pipes.end(),[&](const vector<int>& a, const vector<int>& b){
+            return a[2] < b[2];
+        });
+        UnionFind uf(n+1);
         int cost = 0;
-        while(!pq.empty()){
-            auto w = pq.top().first;
-            auto u = pq.top().second;
-            pq.pop();
-            if(mstSet[u]) continue;
-            mstSet[u] = 1;
-            cost += w;
-            size++;
-            for(auto nb:adj[u]){
-                int v = nb.first;
-                int weight = nb.second;
-                if(mstSet[v]==0 && weight<minDist[v]){
-                    minDist[v] = weight;
-                    pq.push({minDist[v],v});
-                }
+        for(auto pipe:pipes){
+            if(!uf.connected(pipe[0],pipe[1])){
+                cost += pipe[2];
+                uf.connect(pipe[0],pipe[1]);
+                if(uf.getSize() == 1) break;
             }
         }
         return cost;
