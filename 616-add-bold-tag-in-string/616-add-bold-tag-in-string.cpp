54b40cdce5a3d6b25a611289f4c01ecd158bba4a
Anand Tripathi <shadarsh23@gmail.com>
2022-06-24 10:30:04 +0530

    Time: 21 ms (76.40%), Space: 10.2 MB (82.77%) - LeetHub

---
+++ 616-add-bold-tag-in-string/616-add-bold-tag-in-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    typedef pair<int,int> ppi;
+    string addBoldTag(string s, vector<string>& words) {
+        vector<ppi> pos;
+        for(const auto& w: words) {
+            int c = w.length();
+            for(int i = 0; (i = s.find(w,i)) != string::npos; ++i) {
+                pos.emplace_back(i, i+c);
+            }
+        }
+        int m = pos.size();
+        sort(pos.begin(),pos.end());
+        vector<ppi> merged;
+        for(int i=0; i<m; i++){
+            if(merged.empty() or merged.back().second < pos[i].first)
+                merged.push_back(pos[i]);
+            else{
+                merged.back().second = max(merged.back().second,pos[i].second);
+            }
+        }
+       for(auto it = merged.rbegin(); it != merged.rend(); ++it) {
+            s.insert(it->second, "</b>"); s.insert(it->first, "<b>");
+        }
+        
+        return s;
+    }
+};
