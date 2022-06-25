f2706fed0d9f543b71056d8c47cf514ec981effd
Anand Tripathi <shadarsh23@gmail.com>
2022-06-25 15:47:22 +0530

    Time: 802 ms (11.70%), Space: 51 MB (25.63%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,29 +1,65 @@
 class Solution {
 public:
-  int minimumEffortPath(vector<vector<int>> &heights) {
-    int m = heights.size(), n = heights[0].size();
-    int dirs[5] = {-1, 0, 1, 0, -1};
-
-    std::vector<vector<int>> efforts(m, vector<int>(n, INT_MAX));
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-    pq.emplace(0, 0); // First item is effort, second is row * 100 + col
-    while (!pq.empty()) {
-      int effort = pq.top().first;
-      int x = pq.top().second / 100, y = pq.top().second % 100;
-      pq.pop();
-
-      if (x == m - 1 && y == n - 1) return effort;
-      if (effort >= efforts[x][y]) continue;
-      efforts[x][y] = effort;
-
-      for (int i = 0; i < 4; i++) {
-        int nx = x + dirs[i], ny = y + dirs[i + 1];
-        if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-        int n_effort = max(effort, abs(heights[x][y] - heights[nx][ny]));
-        pq.emplace(n_effort, nx * 100 + ny);
-      }
+    int minimumEffortPath(vector<vector<int>> &heights) {
+        int left = 0;
+        int right = 1000000;
+        int result = right;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (canReachDestinaton(heights, mid)) {
+                result = min(result, mid);
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return result;
     }
-    return -1;
-  }
+
+    int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    // use bfs to check if we can reach destination with max absolute difference
+    // k
+    bool canReachDestinaton(vector<vector<int>> &heights, int mid) {
+        int row = heights.size();
+        int col = heights[0].size();
+        queue<Cell> queue;
+        vector<vector<bool>> visited(row, vector<bool>(col, false));
+        queue.push(Cell(0, 0));
+        visited[0][0] = true;
+        while (!queue.empty()) {
+            Cell curr = queue.front();
+            queue.pop();
+            if (curr.x == row - 1 && curr.y == col - 1) {
+                return true;
+            }
+            for (auto direction : directions) {
+                int adjacentX = curr.x + direction[0];
+                int adjacentY = curr.y + direction[1];
+                if (isValidCell(adjacentX, adjacentY, row, col) &&
+                    !visited[adjacentX][adjacentY]) {
+                    int currentDifference = abs(heights[adjacentX][adjacentY] -
+                                                heights[curr.x][curr.y]);
+                    if (currentDifference <= mid) {
+                        visited[adjacentX][adjacentY] = true;
+                        queue.push(Cell(adjacentX, adjacentY));
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    bool isValidCell(int x, int y, int row, int col) {
+        return x >= 0 && x <= row - 1 && y >= 0 && y <= col - 1;
+    }
+
+    class Cell {
+    public:
+        int x, y;
+        Cell(int x, int y) {
+            this->x = x;
+            this->y = y;
+        }
+    };
 };
