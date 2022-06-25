5f5cf40bb79edcda3bfca08b6c5e20eb8f3116c8
Anand Tripathi <shadarsh23@gmail.com>
2022-06-25 11:21:17 +0530

    Time: 160 ms (86.76%), Space: 86.8 MB (79.58%) - LeetHub

--- 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.cpp
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-    vector<int> goodDaysToRobBank(vector<int>& nums, int time) {
-        vector<int> ans;
-        int n = nums.size();
-        vector<int> increasing(n,1), decreasing(n,1);
-        for(int i=1; i<n; i++){
-            if(nums[i] <= nums[i-1])
-                decreasing[i]+=decreasing[i-1];
+    vector<int> goodDaysToRobBank(vector<int>& A, int time) {
+        int N = A.size(), left = 1;
+        vector<int> right(N, 1), ans;
+        for (int i = N - 2; i >= 0; --i) {
+            if (A[i] <= A[i + 1]) right[i] += right[i + 1];
         }
-        for(int i=n-2; i>=0; i--){
-            if(nums[i] <= nums[i+1])
-                increasing[i]+=increasing[i+1];
-        }
-        for(int i = time; i + time < n; i++){
-            if(increasing[i] >= time+1 && decreasing[i]>=time+1)
-                ans.push_back(i);
+        for (int i = 0; i + time < N; ++i) {
+            if (i - 1 >= 0 && A[i] <= A[i - 1]) left++;
+            else left = 1;
+            if (left > time && right[i] > time) ans.push_back(i);
         }
         return ans;
     }
