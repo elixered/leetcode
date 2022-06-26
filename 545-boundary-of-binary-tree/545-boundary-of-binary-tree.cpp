beca26d3d4b442e2eb6433eb779a79c05d7ffa1a
Anand Tripathi <shadarsh23@gmail.com>
2022-06-26 14:33:12 +0530

    Time: 16 ms (45.87%), Space: 21 MB (57.94%) - LeetHub

--- 545-boundary-of-binary-tree/545-boundary-of-binary-tree.cpp
+++ 545-boundary-of-binary-tree/545-boundary-of-binary-tree.cpp
@@ -1,54 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    
-    bool isLeaf(TreeNode* node){
-        return !node->left && !node->right;
-    }
-    
-    void getLeftBoundary(TreeNode* root){
-        if(!root or isLeaf(root)) return;
-        ans.push_back(root->val);
-        if(root->left)
-            getLeftBoundary(root->left);
-        else if(root->right)
-            getLeftBoundary(root->right);
-    }
-    
-    void getRightBoundary(TreeNode* root){
-        if(!root or isLeaf(root)) return;
-        if(root->right)
-            getRightBoundary(root->right);
-        else if(root->left)
-            getRightBoundary(root->left);
-        ans.push_back(root->val);
-    }
-    
-    void getLeaves(TreeNode* root){
-        if(!root) return;
-        getLeaves(root->left);
-        if(isLeaf(root)) ans.push_back(root->val);
-        getLeaves(root->right);
-    }
-    
-    vector<int> ans;
     vector<int> boundaryOfBinaryTree(TreeNode* root) {
-        if(!root) return ans;
-        ans.push_back(root->val);
-        getLeftBoundary(root->left);
-        getLeaves(root->left);
-        getLeaves(root->right);
-        getRightBoundary(root->right);
-        return ans;
+        vector<int> bounds;
+        if (root) {
+            bounds.push_back(root->val);
+            getBounds(root->left, bounds, true, false);
+            getBounds(root->right, bounds, false, true);
+        }
+        return bounds;
+    }
+
+private:
+    void getBounds(TreeNode* node, vector<int>& res, bool lb, bool rb) {
+        if (!node)  return;
+        if (lb) res.push_back(node->val);
+        if (!lb && !rb && !node->left && !node->right)  res.push_back(node->val);
+        getBounds(node->left, res, lb, rb && !node->right);
+        getBounds(node->right, res, lb && !node->left, rb);
+        if (rb) res.push_back(node->val);
     }
 };
