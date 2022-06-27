b20647da5c5ebbed467e7a7fc348ae42f8030206
Anand Tripathi <shadarsh23@gmail.com>
2022-06-27 22:22:27 +0530

    Time: 334 ms (92.17%), Space: 82.2 MB (69.14%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& persons) {
-        sort(flowers.begin(),flowers.end());
-        vector<pair<int,int>> p;
-        int n = persons.size();
-        for(int i=0; i<n; ++i){
-            p.push_back({persons[i],i});
+        vector<int> start, end;
+        for (auto& t : flowers)
+            start.push_back(t[0]), end.push_back(t[1]);
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+        vector<int> res;
+        for (int t : persons) {
+            int started = upper_bound(start.begin(), start.end(), t) - start.begin();
+            int ended = lower_bound(end.begin(), end.end(), t) - end.begin();
+            res.push_back(started - ended);
         }
-        sort(begin(p),end(p));
-        int f = 0;
-        multiset<int> set;
-        vector<int> ans(n,0);
-        for(auto& [person,i] : p){
-            for(; f<flowers.size() && person>=flowers[f][0]; ++f){
-                set.insert(flowers[f][1]);
-            }
-            while(set.size() && *set.begin() < person){
-                set.erase(set.begin());
-            }
-            ans[i] = set.size();
-        }
-        return ans;
+        return res;
     }
 };
