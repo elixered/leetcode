0fd7c830b7b8e12e6ac69a241ea7c2d345c4d026
Anand Tripathi <shadarsh23@gmail.com>
2022-06-28 16:02:17 +0530

    Time: 786 ms (17.18%), Space: 115.7 MB (29.37%) - LeetHub

--- 282-expression-add-operators/282-expression-add-operators.cpp
+++ 282-expression-add-operators/282-expression-add-operators.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-   void addOperators(vector<string>& result, string nums, string t, long long last, long long curVal, int target) {
-	if (nums.length() == 0) {
-		if (curVal == target)
-			result.push_back(t);
-		return;
-	}
-
-	for (int i = 1; i<=nums.length(); i++) {
-		string num = nums.substr(0, i);
-		if(num.length() > 1 && num[0] == '0')
-		    return;
-		
-		string nextNum = nums.substr(i);
-
-		if (t.length() > 0) {
-			addOperators(result, nextNum, t + "+" + num, stoll(num), curVal + stoll(num), target);
-			addOperators(result, nextNum, t + "-" + num, -stoll(num), curVal - stoll(num), target);
-			addOperators(result, nextNum, t + "*" + num, last * stoll(num), (curVal - last) + (last * stoll(num)), target);
-		}
-		else 
-			addOperators(result, nextNum, num, stoll(num), stoll(num), target);
-	}
-}
-
-vector<string> addOperators(string num, int target) {
-	vector<string> result;
-	addOperators(result, num, "", 0, 0, target);
-	return result;
-}
+    vector<string> ans;
+    string s;
+    int target;
+    vector<string> addOperators(string s, int target) {
+        this->s = s;
+        this->target = target;
+        backtrack( 0, "", 0, 0);
+        return ans;
+    }
+    void backtrack(int i, const string& path, long resSoFar, long prevNum) {
+        if (i == s.length()) {
+            if (resSoFar == target) ans.push_back(path);
+            return;
+        }
+        string numStr;
+        long num = 0;
+        for (int j = i; j < s.length(); j++) {
+            if (j > i && s[i] == '0') break; // Skip leading zero number
+            numStr += s[j];
+            num = num * 10 + s[j] - '0';
+            if (i == 0) {
+                backtrack(j + 1, path + numStr, num, num); // First num, pick it without adding any operator!
+            } else {
+                backtrack(j + 1, path + "+" + numStr, resSoFar + num, num);
+                backtrack(j + 1, path + "-" + numStr, resSoFar - num, -num);
+                backtrack(j + 1, path + "*" + numStr, resSoFar - prevNum + prevNum * num, prevNum * num); // Can imagine with example: 1-2*3
+            }
+        }
+    }
 };
