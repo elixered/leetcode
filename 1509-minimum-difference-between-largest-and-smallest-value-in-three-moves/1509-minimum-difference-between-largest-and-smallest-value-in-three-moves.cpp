ad9b767ba64f0f9f07742688a4ce30a5ef2787e3
Anand Tripathi <shadarsh23@gmail.com>
2022-06-28 10:06:56 +0530

    Time: 109 ms (78.93%), Space: 35.3 MB (60.14%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
-    int minDifference(vector<int>& nums) {
-        int n = nums.size();
-        if(n < 5) return 0;
-        sort(nums.begin(),nums.end());
-        int ans = nums[n-1] - nums[0];
-        for(int i=0; i<=3; ++i){
-            ans = min(ans,nums[n-4+i]-nums[i]);
-        }
-        return ans;
-    }
+int minDifference(vector<int>& nums) {
+  int const N = nums.size();
+
+  // For up to four numbers, we can always make them all equal.
+  if (N <= 4) return 0;
+
+  // Shift the four smallest numbers to the beginning
+  // and the four largest numbers to the end.
+  std::partial_sort(nums.begin(), nums.begin() + 4, nums.end());
+  std::partial_sort(nums.rbegin(), nums.rbegin() + 4, nums.rend(), std::greater<int>());
+
+  // Try all four possible ways to pick our 3 moves.
+  int result = INT_MAX;
+  for (int l = 0; l <= 3; ++l)
+    result = std::min(result, nums[N-4+l] - nums[l]);
+  return result;
+}
 };
