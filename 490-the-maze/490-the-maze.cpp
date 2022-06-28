c1734ec6b49ba8186a403aaf152e93158afe6b95
Anand Tripathi <shadarsh23@gmail.com>
2022-06-28 18:59:52 +0530

    Time: 1067 ms (5.03%), Space: 84.9 MB (5.03%) - LeetHub

--- 490-the-maze/490-the-maze.cpp
+++ 490-the-maze/490-the-maze.cpp
@@ -6,20 +6,8 @@ public:
         return x>=0 && y>=0 && x<m && y<n;
     }
     
-    bool dfs(vector<vector<int>>& maze, vector<int>& start, vector<int>& dest){
-        if(!valid(start[0],start[1])) return false;
-        if(start == dest) return true;
-        if(visited.find(start) != visited.end()) return false;
-        visited.insert(start);
-        vector<vector<int>> dir = {{-1,0},{1,0},{0,-1},{0,1}};
-        for(int i=0; i<4; ++i){
-            vector<int> end = getEnd(maze,start,dir[i]);
-            if(end==dest or dfs(maze,end,dest)) return true;
-        }
-        return false;
-    }
     
-    vector<int> getEnd(vector<vector<int>>& maze, vector<int>& start, vector<int>& dir){
+    vector<int> getEnd(vector<vector<int>>& maze, vector<int> start, vector<int>& dir){
         int x = start[0], y = start[1];
         int dx = dir[0], dy = dir[1];
         while(valid(x,y) && maze[x][y]!=1){
@@ -32,6 +20,20 @@ public:
     bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& dest) {
         m = maze.size();
         n = maze[0].size();
-        return dfs(maze,start,dest);
+        vector<vector<int>> dir = {{-1,0},{1,0},{0,-1},{0,1}};
+        queue<vector<int>> q;
+        q.push(start);
+        while(!q.empty()){
+            auto curr = q.front();
+            q.pop();
+            if(curr == dest) return true;
+            visited.insert(curr);
+            for(int i=0; i<4; ++i){
+                vector<int> end = getEnd(maze,curr,dir[i]);
+                if(visited.find(end) == visited.end())
+                    q.push(end);
+            }
+        }
+        return false;
     }
 };
