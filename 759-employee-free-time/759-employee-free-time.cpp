4908ac58061872c3439dbe05a093d84992026164
Anand Tripathi <shadarsh23@gmail.com>
2022-06-29 23:00:30 +0530

    Time: 58 ms (37.01%), Space: 9.8 MB (89.61%) - LeetHub

--- 759-employee-free-time/759-employee-free-time.cpp
+++ 759-employee-free-time/759-employee-free-time.cpp
@@ -1,44 +1,38 @@
-/*
-// Definition for an Interval.
-class Interval {
-public:
-    int start;
-    int end;
-
-    Interval() {}
-
-    Interval(int _start, int _end) {
-        start = _start;
-        end = _end;
-    }
-};
-*/
-
 class Solution {
 public:
     vector<Interval> employeeFreeTime(vector<vector<Interval>> schedule) {
-        map<int,int> mp;
-        for(auto employee:schedule){
-            for(auto interval:employee){
-                mp[interval.start]++;
-                mp[interval.end]--;
+        //priority_queue Node is pair<start, <employeeIndex, intervalIndex>>
+        //pair.second is used to get to next interval
+        using PQ_Node = pair<int, pair<int, int>>;
+        vector<Interval> ans;
+        if (schedule.empty()) return ans;
+        priority_queue<PQ_Node, vector<PQ_Node>, greater<>> pq;
+        for(int i=0; i<schedule.size(); ++i) {
+            if (schedule[i].size()) {
+                pq.push({schedule[i][0].start, {i, 0}});
             }
         }
-        int start = -1;
-        int prev = 0;
-        vector<Interval> ans;
-        for(auto it:mp){
-            int curr = prev + it.second;
-            if(curr == 0){
-                if(prev!=0)
-                    start = it.first;
+        //minimum value of end time of any employees schedule which started earliest
+        int end = schedule[pq.top().second.first][0].start;
+        while(pq.size()) {
+            //find employee's schedule with minimum end time
+            auto p = pq.top();
+            pq.pop();
+            int intervalIndex = p.second.second;
+            int employeeIndex = p.second.first;
+            if (p.first > end) {
+                ans.push_back(Interval{end, p.first});
+                end = schedule[employeeIndex][intervalIndex].end;
+            } else {
+                end = max(end, schedule[employeeIndex][intervalIndex].end);
             }
-            else{
-                if(prev == 0 && start != -1){
-                    ans.push_back(Interval(start,it.first));
-                }
+            ++intervalIndex;
+            if (intervalIndex < schedule[employeeIndex].size()) {
+            
+                pq.push({schedule[employeeIndex][intervalIndex].start,
+                         {employeeIndex, intervalIndex}});
             }
-            prev = curr;
+            
         }
         return ans;
     }
