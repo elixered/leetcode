9a098fb59d41f5faaa20ba69dfb4b52150f0b087
Anand Tripathi <shadarsh23@gmail.com>
2022-06-30 13:21:30 +0530

    Time: 9 ms (43.26%), Space: 13.8 MB (10.27%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.cpp
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.cpp
@@ -1,47 +1,16 @@
 class Solution {
 public:
-    
-    struct node{
-        int idx;
-        string target;
-        string source;
-        node(int a, string b, string c){
-            idx = a;
-            target = b;
-            source = c;
+    string findReplaceString(string S, vector<int>& indexes, vector<string>& sources, vector<string>& targets) {
+        vector<pair<int, int>> sorted;
+        for (int i = 0 ; i < indexes.size(); i++)
+            sorted.push_back({indexes[i], i});
+        sort(sorted.rbegin(), sorted.rend());
+        for (auto ind : sorted) {
+            int i = ind.first;
+            string s = sources[ind.second], t = targets[ind.second];
+            if (S.substr(i, s.length()) == s)
+                S = S.substr(0, i) + t + S.substr(i + s.length());
         }
-    };
-    
-    bool match(string& s, int start, string source){
-        for(int i=0; i<source.size(); ++i){
-            if(i+start >= s.size() || s[i+start]!=source[i])
-                return false;
-        }
-        return true;
-    }
-    
-    string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
-        vector<node> vec;
-        int n = indices.size();
-        int m = s.size();
-        for(int i=0; i<n; ++i){
-            if(match(s,indices[i],sources[i]))
-                vec.push_back(node(indices[i],targets[i], sources[i]));
-        }
-        auto cmp = [](node& a, node& b){
-            return a.idx < b.idx;
-        };
-        sort(begin(vec),end(vec),cmp);
-        string ans;
-        int index = 0;
-        for(auto v:vec){
-            while(index < v.idx)
-                ans += s[index++];
-            ans += v.target;
-            index += v.source.size();
-        }
-        while(index < s.size())
-            ans += s[index++];
-        return ans;
+        return S;
     }
 };
