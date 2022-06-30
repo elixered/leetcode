84815267f9510d2769a786c08e7695b1e3d232f0
Anand Tripathi <shadarsh23@gmail.com>
2022-06-30 16:43:26 +0530

    Time: 358 ms (41.22%), Space: 70.5 MB (85.85%) - LeetHub

--- 715-range-module/715-range-module.cpp
+++ 715-range-module/715-range-module.cpp
@@ -1,50 +1,52 @@
 class RangeModule {
+private:
+    map<int,int> mp;
 public:
-   void addRange(int left, int right) {
-        int n = invals.size();
-        vector<pair<int, int>> tmp;
-        for (int i = 0; i <= n; i++) {
-            if (i == n || invals[i].first > right) {
-                tmp.push_back({left, right});
-                while (i < n) tmp.push_back(invals[i++]);
-            }
-            else if (invals[i].second < left) 
-                tmp.push_back(invals[i]);
-            else {
-                left = min(left, invals[i].first);
-                right = max(right, invals[i].second);
-            }
-        }
-        swap(invals, tmp);
+    RangeModule() {
+        
+    }
+    
+    void addRange(int left, int right) {
+        auto l = mp.upper_bound(left);
+        if(l != mp.begin() && prev(l)->second >= left)
+            left = prev(l)->first;
+        right = removeIntervals(left,right);
+        mp[left] = right;
     }
     
     bool queryRange(int left, int right) {
-        int n = invals.size(), l = 0, r = n-1;
-        while (l <= r) {
-            int m = l+(r-l)/2;
-            if (invals[m].first >= right)
-                r = m-1;
-            else if (invals[m].second <= left)
-                l = m+1;
-            else 
-                return invals[m].first <= left && invals[m].second >= right;
-        }
-        return false;
+        auto it = mp.upper_bound(left);
+        if(it != mp.begin())
+            it = prev(it);
+        return it->first <= left && it->second >= right;
     }
     
     void removeRange(int left, int right) {
-        int n = invals.size();
-        vector<pair<int, int>> tmp;
-        for (int i = 0; i < n; i++) {
-            if (invals[i].second <= left || invals[i].first >= right)
-                tmp.push_back(invals[i]);
-            else {
-                if (invals[i].first < left)  tmp.push_back({invals[i].first, left});
-                if (invals[i].second > right) tmp.push_back({right, invals[i].second});
-            }
+        int right1 = removeIntervals(left,right);
+        auto it = mp.upper_bound(left);
+        if(it != mp.begin()){
+            right1 = max(right1, prev(it)->second);
+            prev(it)->second = min(prev(it)->second, left);
         }
-        swap(invals, tmp);
+        if(right1 > right)
+            mp[right] = right1;
     }
-private:
-    vector<pair<int, int>> invals;
+    
+    int removeIntervals(int left, int right){
+        auto it = mp.lower_bound(left);
+        for(it; it!=mp.end() && it->first <= right; ){
+            right = max(right,it->second);
+            mp.erase(it++);
+        }
+        return right;
+    }
+    
 };
+
+/**
+ * Your RangeModule object will be instantiated and called as such:
+ * RangeModule* obj = new RangeModule();
+ * obj->addRange(left,right);
+ * bool param_2 = obj->queryRange(left,right);
+ * obj->removeRange(left,right);
+ */
