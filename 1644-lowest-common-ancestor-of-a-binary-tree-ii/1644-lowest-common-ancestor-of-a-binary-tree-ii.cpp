e2621e86c4692c737c21ddd07452e4b16def7934
Anand Tripathi <shadarsh23@gmail.com>
2022-07-01 13:07:53 +0530

    Time: 297 ms (5.04%), Space: 61.7 MB (83.53%) - LeetHub

--- 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.cpp
+++ 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     
-    bool checkIfExists(TreeNode* root, TreeNode* node){
-        if(root==NULL || node == NULL)
-            return false;
-        if(root->val == node->val)
-            return true;
-        if(checkIfExists(root->left, node) || checkIfExists(root->right, node))
-            return true;
-        else
-            return false;
-    }
+    bool foundp = false, foundq = false;
     
     TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q){
         if(root==NULL)
             return NULL;
         
-        if(root== p || root == q)
-            return root;
+       
         
         TreeNode* leftLCA = lca(root->left, p, q);
         TreeNode* rightLCA = lca(root->right, p, q);
+        
+         if(root== p || root == q){
+            if(root ==p && !foundp){
+                foundp = true;
+            }
+            else if(root==q && !foundq){
+                foundq= true;
+            }
+            return root;
+        }
         
         if(leftLCA && rightLCA)
             return root;
@@ -32,11 +32,8 @@ public:
     }
     
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(checkIfExists(root, p) && checkIfExists(root, q)){
-            return lca(root, p, q);
-        }
-        else{
-            return NULL;
-        }
+        auto ans =  lca(root,p,q);
+        if(foundp && foundq) return ans;
+        else return NULL;
     }
 };
