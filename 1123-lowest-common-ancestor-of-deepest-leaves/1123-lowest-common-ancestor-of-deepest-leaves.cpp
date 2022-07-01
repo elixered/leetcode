2084d5853f27fec0b52e0df0510b6be8aafb0dad
Anand Tripathi <shadarsh23@gmail.com>
2022-07-01 12:47:30 +0530

    Time: 11 ms (94.73%), Space: 21.2 MB (33.33%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -1,26 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private:
-	unordered_map<TreeNode*, int> depth;
 public:
+    TreeNode* lca;
+    int deepest = 0;
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        if(!root)
-            return NULL;
-        int l = countDepth(root->left);
-        int r = countDepth(root->right);
-        if(l == r)
-            return root;
-        else if(l < r)
-            return lcaDeepestLeaves(root->right);
-        else
-            return lcaDeepestLeaves(root->left);
+        helper(root, 0);
+        return lca;
     }
-    
-    int countDepth(TreeNode* root){
-        if(!root)
-            return 0;
-        if(!depth.count(root))
-            depth[root] = 1 + max(countDepth(root->left), countDepth(root->right));
-        return depth[root];
-		
+
+    int helper(TreeNode* node, int depth) {
+        deepest = max(deepest, depth);
+        if (!node) {
+            return depth;
+        }
+        int left = helper(node->left, depth + 1);
+        int right = helper(node->right, depth + 1);
+        if (left == deepest && right == deepest) {
+            lca = node;
+        }
+        return max(left, right);
     }
 };
