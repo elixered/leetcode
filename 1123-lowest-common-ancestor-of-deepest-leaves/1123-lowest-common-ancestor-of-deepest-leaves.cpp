956db7d592c6c5357261ab0569a5daa35fe303bb
Anand Tripathi <shadarsh23@gmail.com>
2022-07-01 12:41:14 +0530

    Time: 47 ms (5.12%), Space: 23.8 MB (12.33%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -1,36 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
+private:
+	unordered_map<TreeNode*, int> depth;
 public:
-    
-    bool leaf(TreeNode* root){
-        return !root->left && !root->right;
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        if(!root)
+            return NULL;
+        int l = countDepth(root->left);
+        int r = countDepth(root->right);
+        if(l == r)
+            return root;
+        else if(l < r)
+            return lcaDeepestLeaves(root->right);
+        else
+            return lcaDeepestLeaves(root->left);
     }
     
-    int maxheight = 0;
-    
-    void solve(TreeNode* root, int h){
-        if(root == NULL) return;
-        maxheight = max(maxheight,h);
-        solve(root->left,h+1);
-        solve(root->right,h+1);
-    }
-    
-    TreeNode* lcaDeepestLeaves(TreeNode* root,int h = 0) {
-        solve(root,0);
-        if(!root or (leaf(root) && h == maxheight)) return root;
-        auto l = lcaDeepestLeaves(root->left,1+h);
-        auto r = lcaDeepestLeaves(root->right,1+h);
-        if(l && r) return root;
-        return l ? l : r;
+    int countDepth(TreeNode* root){
+        if(!root)
+            return 0;
+        if(!depth.count(root))
+            depth[root] = 1 + max(countDepth(root->left), countDepth(root->right));
+        return depth[root];
+		
     }
 };
