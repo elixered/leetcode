1e2086de67cda96a106eed5a7bd65637e834339b
Anand Tripathi <shadarsh23@gmail.com>
2022-07-02 12:20:17 +0530

    Time: 416 ms (16.77%), Space: 94 MB (14.69%) - LeetHub

--- 164-maximum-gap/164-maximum-gap.cpp
+++ 164-maximum-gap/164-maximum-gap.cpp
@@ -5,29 +5,24 @@ int maximumGap(vector<int>& nums)
     if (nums.empty() || nums.size() < 2)
         return 0;
 
-    int maxVal = *max_element(nums.begin(), nums.end());
+    int maxval = *max_element(nums.begin(), nums.end());
 
     int exp = 1;                                 // 1, 10, 100, 1000 ...
     int radix = 10;                              // base 10 system
 
     vector<int> aux(nums.size());
 
-    /* LSD Radix Sort */
-    while (maxVal / exp > 0) {                   // Go through all digits from LSD to MSD
-        vector<int> count(radix, 0);
-
-        for (int i = 0; i < nums.size(); i++)    // Counting sort
-            count[(nums[i] / exp) % 10]++;
-
-        for (int i = 1; i < count.size(); i++)   // you could also use partial_sum()
-            count[i] += count[i - 1];
-
-        for (int i = nums.size() - 1; i >= 0; i--)
-            aux[--count[(nums[i] / exp) % 10]] = nums[i];
-
-        for (int i = 0; i < nums.size(); i++)
-            nums[i] = aux[i];
-
+    while(maxval){
+        vector<int> count(10,0);
+        vector<int> aux(nums.size(),0);
+        for(auto it:nums)
+            count[(it/exp)%10]++;
+        for(int i=1; i<10; ++i)
+            count[i] += count[i-1];
+        for(int i=nums.size()-1; i>=0; --i)
+            aux[--count[(nums[i]/exp)%10]] = nums[i];
+        nums = aux;
+        maxval /= 10;
         exp *= 10;
     }
 
