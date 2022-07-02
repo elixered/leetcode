d375a583e344667845b3a6593b69fd339933f8b7
Anand Tripathi <shadarsh23@gmail.com>
2022-07-03 00:20:27 +0530

    Time: 4 ms (67.86%), Space: 8.6 MB (17.69%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.cpp
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     
-    unordered_map<char,vector<int>> mp;
-    
+    vector<vector<int>> dp;    
     int move(string& source, string& target, int i){
         int j = 0;
         int n = source.size();
         int m = target.size();
         while(i<m && j<n){
-            char curr = target[i];
-            if(mp.find(curr) == mp.end()) return i;
-            auto& v = mp[curr];
-            auto idx = lower_bound(begin(v),end(v),j)-begin(v);
-            if(idx == v.size()) return i;
+            int curr = target[i] - 'a';
+            int nextIdx = dp[j][curr];
+            if(nextIdx == -1) return i;
             else{
                 i++;
-                j = v[idx]+1;
+                j = nextIdx+1;
             }
         }
         return i;
@@ -25,8 +22,15 @@ public:
         int i = 0;
         int n = target.size();
         int s = 0;
-        for(int j=0; j<source.size(); j++)
-            mp[source[j]].push_back(j);
+        dp = vector<vector<int>>(source.size(),vector<int>(26,-1));
+        int m = source.size();
+        dp[m-1][source.back()-'a'] = m-1;
+        for(int i=m-2; i>=0; i--){
+            for(int j=0; j<26; ++j){
+                dp[i][j] = dp[i+1][j];
+            }
+            dp[i][source[i]-'a'] = i;
+        }
         while(i<n){
             int j = move(source,target,i);
             if(i==j) return -1;
