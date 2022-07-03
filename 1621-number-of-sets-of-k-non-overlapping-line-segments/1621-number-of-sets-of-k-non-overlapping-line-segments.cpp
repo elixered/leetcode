c04616b1975946f4c15f4b3a8981471f35939136
Anand Tripathi <shadarsh23@gmail.com>
2022-07-03 10:42:42 +0530

    Time: 879 ms (31.61%), Space: 9.9 MB (83.87%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,14 +1,21 @@
-int dp[1001][1001] = {}; 
+int dp[1001][1001] = {};
+int m = pow(10,9)+7;
 class Solution {
 public:
-    int numberOfSets(int n, int k) {
-        if (n - k < 1 || k == 0)
-            return k == 0;
-        if (dp[n][k] == 0) {
-            dp[n][k] = 1 + numberOfSets(n - 1, k);
-            for (int i = n; i - k > 0; --i)
-                dp[n][k] = ((long)dp[n][k] + numberOfSets(i - 1, k - 1)) % 1000000007;
+    
+    int solve(int n, int k){
+        if(k==0) return 1;
+        if(n<=1 or k>n) return 0;
+        if(dp[n][k]!=0) return dp[n][k];
+        int ans = solve(n-1,k);
+        for(int len=2; len<=n-k+1; len++){
+            ans += solve(n-len+1,k-1);
+            ans %= m;
         }
-        return dp[n][k] - 1;
+        return dp[n][k] = ans;
+    }
+    
+    int numberOfSets(int n, int k) {
+        return solve(n,k);
     }
 };
