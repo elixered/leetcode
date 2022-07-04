35c08d7173df59591af15822ffd903b2225ea7fd
Anand Tripathi <shadarsh23@gmail.com>
2022-07-04 16:18:04 +0530

    Time: 619 ms (75.04%), Space: 11.9 MB (83.15%) - LeetHub

--- 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.cpp
+++ 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    
-    unordered_map<int,unordered_map<int,int>> mp;
-    
-    int solve(vector<int>& arr, int idx, int first, int second){
-        if(idx == arr.size()) return 0;
-        if(mp[idx].find(first+second) != mp[idx].end()) return mp[idx][first+second];
-        auto itr = lower_bound(arr.begin()+idx,arr.end(),first+second);
-        if(itr != arr.end() && *itr == first+second)
-            return mp[idx][first+second] = solve(arr,itr-arr.begin()+1,second,first+second)+1;
-        else return 0;
-    }
-    
-    int lenLongestFibSubseq(vector<int>& arr) {
-        int n = arr.size();
+    int lenLongestFibSubseq(vector<int>& A) {
+        int N = A.size();
+        unordered_set<int> S(A.begin(), A.end());
+
         int ans = 0;
-        for(int i=0; i<n-2; ++i){
-            for(int j=i+1; j<n-1; ++j){
-                ans = max(ans,solve(arr,j+1,arr[i],arr[j]));
+        for (int i = 0; i < N; ++i)
+            for (int j = i+1; j < N; ++j) {
+                int x = A[j], y = A[i] + A[j];
+                int length = 2;
+                while (S.find(y) != S.end()) {
+                    int z = x + y;
+                    x = y;
+                    y = z;
+                    ans = max(ans, ++length);
+                }
             }
-        }
-        return ans+2>=3?ans+2:0;
+
+        return ans >= 3 ? ans : 0;
     }
 };
