1e3e526fcd65a77d64dbeeaf19384469a1f66800
Anand Tripathi <shadarsh23@gmail.com>
2022-07-04 18:59:05 +0530

    Time: 2 ms (57.55%), Space: 7.6 MB (75.56%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -4,12 +4,13 @@ public:
         int n = nums.size();
         if(n==1) return nums[0];
         if(n==2) return max(nums[0],nums[1]);
-        vector<int> dp(n,0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[1],nums[0]);
+        int prev = nums[0];
+        int curr = max(nums[0],nums[1]);
         for(int i=2; i<n; ++i){
-            dp[i] = max(dp[i-1],dp[i-2]+nums[i]);
+            int next = max(prev+nums[i],curr);
+            prev = curr;
+            curr = next;
         }
-        return dp[n-1];
+        return curr;
     }
 };
