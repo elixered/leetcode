1dd3b2ea0064c72ed078209289c3d68809f8941b
Anand Tripathi <shadarsh23@gmail.com>
2022-07-06 00:21:22 +0530

    Time: 10 ms (36.22%), Space: 9.9 MB (8.05%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
-    bool isAlienSorted(vector<string>& words, string order) {
-        vector<int> mp(26,0);
-        for(int i=0; i<order.size(); ++i)
-            mp[order[i]-'a'] = i;
-        for(int i=1; i<words.size(); ++i){
-            string& curr = words[i];
-            string& prev = words[i-1];
-            bool flag = false;
-            for(int i=0; i<min(curr.size(),prev.size()); ++i){
-                if(mp[curr[i]-'a'] < mp[prev[i]-'a'])
-                    return false;
-                else if(prev[i]!=curr[i]){
-                    flag = true;
-                    break;
-                }
-            }
-            if(flag == false && prev.size() > curr.size()) return false;
-        }
-        return true;
+    bool isAlienSorted(vector<string> words, string order) {
+        int mapping[26];
+        for (int i = 0; i < 26; i++)
+            mapping[order[i] - 'a'] = i;
+        for (string &w : words)
+            for (char &c : w)
+                c = mapping[c - 'a'];
+        return is_sorted(words.begin(), words.end());
     }
 };
