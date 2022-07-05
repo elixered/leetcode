e996cd1c5eee38846a2df71257df17b36c5d3981
Anand Tripathi <shadarsh23@gmail.com>
2022-07-05 14:44:19 +0530

    Time: 217 ms (86.70%), Space: 94.3 MB (62.84%) - LeetHub

--- 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int dp[100001][2];
-    int solve(vector<int>& nums1, vector<int>& nums2, int idx, bool swapped){
-        if(idx == nums1.size()) return 0;
-        if(dp[idx][swapped] != -1) return dp[idx][swapped];
-        int ans = 1e5;
-        if(idx==0 or nums1[idx] > nums1[idx-1] && nums2[idx] > nums2[idx-1])
-            ans = solve(nums1,nums2,idx+1,false);
-        if(idx==0 or nums1[idx] > nums2[idx-1] && nums2[idx] > nums1[idx-1]){
-            swap(nums1[idx],nums2[idx]);
-            ans = min(ans,solve(nums1,nums2,idx+1,true)+1);
-            swap(nums1[idx],nums2[idx]);
-        }
-        return dp[idx][swapped] = ans;
-    }
-    
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
-        memset(dp,-1,sizeof(dp));
-        return solve(nums1,nums2,0,false);
+        int n = nums1.size();
+        vector<int> swap(n,1e9), no_swap(n,1e9);
+        swap[0] = 1;
+        no_swap[0] = 0;
+        for(int i=1; i<n; ++i){
+            for(int j=0; j<2; ++j){
+                if(nums1[i] > nums1[i-1] && nums2[i] > nums2[i-1]){
+                    no_swap[i] = no_swap[i-1];
+                    swap[i] = swap[i-1]+1;
+                }
+                if(nums1[i] > nums2[i-1] && nums2[i] > nums1[i-1]){
+                    no_swap[i] = min(swap[i-1],no_swap[i]);
+                    swap[i] = min(swap[i],no_swap[i-1]+1);
+                }
+            }
+        }
+        return min(swap[n-1],no_swap[n-1]);
     }
 };
