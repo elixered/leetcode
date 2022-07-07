19dcccd904398add98999742e813da378fcc3491
Anand Tripathi <shadarsh23@gmail.com>
2022-07-07 12:06:24 +0530

    Time: 4 ms (49.07%), Space: 9.6 MB (27.02%) - LeetHub

--- 397-integer-replacement/397-integer-replacement.cpp
+++ 397-integer-replacement/397-integer-replacement.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    unordered_map<long,int> mp;
-    int solve(long n){
+    unordered_map<int,int> mp;
+    int solve(int n){
         if(mp.count(n)) return mp[n];
         if(n==1) return 0;
         if(n<1) return 1e5;
         int ans = 1e5;
         if(n%2==0)
         ans = min(ans,solve(n/2)+1);
-        else ans = min({ans,solve(n+1)+1,solve(n-1)+1});
+        else ans = min({ans,solve(n/2+1)+2,solve(n-1)+1});
         return mp[n] = ans;
     }
     int integerReplacement(int n) {
