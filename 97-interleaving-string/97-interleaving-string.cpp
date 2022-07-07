0d80a7f8f963a30492c6c46e2e99dff986f29ab7
Anand Tripathi <shadarsh23@gmail.com>
2022-07-07 10:39:57 +0530

    Time: 3 ms (85.12%), Space: 6.4 MB (79.41%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
+ bool isInterleave(string s1, string s2, string s3) {
     
-    int dp[111][111];
+    if(s3.length() != s1.length() + s2.length())
+        return false;
     
-    bool solve(string s1, string s2, string s3, int i1, int i2, int i3){
-        if(i3<0) return true;
-        if(dp[i1+10][i2+10]!=-1) return dp[i1+10][i2+10];
-        bool flag = false;
-        if(i1>=0 && s1[i1] == s3[i3])
-            flag = flag or solve(s1,s2,s3,i1-1,i2,i3-1);
-        if(i2>=0 && s2[i2] == s3[i3])
-            flag = flag or solve(s1,s2,s3,i1,i2-1,i3-1);
-        return dp[i1+10][i2+10] = flag;
+    vector<bool> table(s2.size()+1,false);
+    
+    for(int i=0; i<s1.length()+1; i++){
+        vector<bool> temp(s2.size()+1,false);
+        for(int j=0; j< s2.length()+1; j++){
+            if(i==0 && j==0)
+                temp[j] = true;
+            else if(i == 0)
+                temp[j] = ( temp[j-1] && s2[j-1] == s3[i+j-1]);
+            else if(j == 0)
+                temp[j] = ( table[j] && s1[i-1] == s3[i+j-1]);
+            else
+                temp[j] = (table[j] && s1[i-1] == s3[i+j-1] ) || (temp[j-1] && s2[j-1] == s3[i+j-1] );
+        }
+        table = temp;
     }
-    
-    bool isInterleave(string s1, string s2, string s3) {
-        memset(dp,-1,sizeof(dp));
-        if(s1.size() + s2.size() != s3.size()) return false;
-        return solve(s1,s2,s3,s1.size()-1,s2.size()-1,s3.size()-1);
-    }
+    return table[s2.length()];
+}
 };
