93c2fd767f5d8181b6312f889ba7d8d15751d146
Anand Tripathi <shadarsh23@gmail.com>
2022-07-11 09:45:23 +0530

    Time: 1801 ms (5.17%), Space: 141.7 MB (5.44%) - LeetHub

--- 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.cpp
+++ 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.cpp
@@ -1,54 +1,87 @@
 class Solution {
-public:
+    // uf for recording all the roots.
+    vector<int> uf;
+    vector<int> rank;
+
+    // Find the root of x.
+    int find(int x) {
+        if (x != uf[x]) {
+            uf[x] = find(uf[x]);
+        }
+        return uf[x];
+    }  
+
+    // union the roots of x and y.
+    void uni(int x, int y) {
+        int rootX = find(x), rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                uf[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                uf[rootX] = rootY;
+            } else {
+                uf[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+        }
+    }
+
+public:    
     int maximumMinimumPath(vector<vector<int>>& grid) {
         int R = grid.size(), C = grid[0].size();
 
+        // Sort all the cells by their values.
+        vector<vector<int>> vals;
+
         // 4 directions to a cell's possible neighbors.
         vector<vector<int>> dirs{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        
+        // Intialize the rank of all the cells.
+        rank = vector<int>(R * C, 1);
 
-        // Put the top-left cell to the priority queue and mark it as true (visited).
-        priority_queue<pair<int, pair<int, int>>> pq;
-        pq.push({grid[0][0], {0, 0}});
-
+        // Root of all the R * C cells
+        uf = vector<int>(R * C, 0);
+        
         // Initalize the status of all the cells as false (unvisited).
         vector<vector<bool>> visited(R, vector<bool>(C));
 
-        // Mark the top-left cell as visited.
-        visited[0][0] = true;
-        int ans = grid[0][0];
-        
-        // While the priority queue is not empty.
-        while (!pq.empty()) {
-            // Pop the cell with the largest value.
-            int curRow = pq.top().second.first;
-            int curCol = pq.top().second.second;
-            int curVal = pq.top().first;
-            pq.pop();
-
-            // Update the minimum value we have visited so far.
-            ans = min(ans, curVal);
-            
-            // If we reach the bottom-right cell, stop the iteration.
-            if (curRow == R - 1 && curCol == C - 1) {
-                break;
-            }
-            
-            for (vector<int> dir : dirs) {
-                int newRow = curRow + dir[0];
-                int newCol = curCol + dir[1];
-
-                // Check if the current cell has any unvisited neighbors.
-                if (newRow >= 0 && newRow < R && newCol >= 0 
-                    && newCol < C && !visited[newRow][newCol]) {
-                    // If so, we put this neighbor to the priority queue 
-                    // and mark it as true (visited).
-                    pq.push({grid[newRow][newCol], {newRow, newCol}});
-                    visited[newRow][newCol] = true;
-                }
+        // Intialize the root of all the cells.
+        for (int row = 0; row < R; ++row) {
+            for (int col = 0; col < C; ++col) {
+                uf[row * C + col] = row * C + col;
+                vals.push_back({grid[row][col], row, col});
             }
         }
+
+        // Sort all the cells by values from the largest to the smallest.
+        sort(vals.begin(), vals.end(), greater<vector<int>>());
         
-        // Return the minimum value we have seen, which is the value of this path.
-        return ans;
-  }
+        // Iteration over the sorted cells.
+        for (vector<int>& curGrid : vals) {
+            int curRow = curGrid[1], curCol = curGrid[2];
+            int curPos = curRow * C + curCol;
+
+            // Mark the current cell as visited.
+            visited[curRow][curCol] = true;
+            for (vector<int> dir : dirs) {
+                int newRow = curRow + dir[0], newCol = curCol + dir[1];
+                int newPos = newRow * C + newCol;
+
+                // Check if the current cell has any unvisited neighbors 
+                // with value larger than or equal to val.
+                if (newRow >= 0 && newRow < R && newCol >= 0 && newCol < C 
+                    && visited[newRow][newCol]) {
+                    // If so, we connect the current cell with this neighbor.
+                    uni(curPos, newPos);
+                }
+            }
+
+            // Check if the top-left cell is connected with the bottom-right cell.
+            if (find(0) == find(R * C - 1)) { 
+                // If so, return the value of the current cell.
+                return grid[curRow][curCol];
+            }
+        }
+        return 0;
+    }
 };
