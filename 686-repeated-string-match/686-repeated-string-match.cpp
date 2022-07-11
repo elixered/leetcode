0a0dd3e369387eb3d723101478fc309c82dbce2b
Anand Tripathi <shadarsh23@gmail.com>
2022-07-11 13:25:20 +0530

    Time: 599 ms (15.67%), Space: 393.3 MB (5.03%) - LeetHub

---
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    int solve(string a, string b, int l){
+        string curr = b.substr(0,l);
+        int idx = l;
+        int count = 1;
+        while(idx < b.size()){
+            int j = 0;
+            while(idx < b.size() && j < a.size()){
+                curr += a[j++];
+                idx++;
+            }
+            count++;
+        }
+        return curr == b ? count: INT_MAX;
+    }
+    
+    int repeatedStringMatch(string a, string b) {
+        string s = b+"#"+a;
+        int n = s.size();
+        vector<int> lps(n,0);
+        for(int i=1; i<n; ++i){
+            int j = lps[i-1];
+            while(j && s[i] != s[j])
+                j = lps[j-1];
+            if(s[i] == s[j])
+                j++;
+            lps[i] = j;
+            if(lps[i] >= b.size()) return 1;
+        }
+        int l = lps[n-1];
+        int count = INT_MAX;
+        for(int i=1; i<=l; ++i){
+            count = min(count,solve(a,b,i));
+        }
+        return count == INT_MAX ? -1 : count;
+    }
+};
