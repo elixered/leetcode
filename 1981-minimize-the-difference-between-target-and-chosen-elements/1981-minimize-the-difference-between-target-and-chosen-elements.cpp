5826bd186cafbdccdfa79cfcff3bda201f6cb7a0
Anand Tripathi <shadarsh23@gmail.com>
2022-07-12 11:17:07 +0530

    Time: 333 ms (86.87%), Space: 12.9 MB (96.61%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    int m, n;
-    int dp[72][5000];
-    int solve(vector<vector<int>>& mat, int row, int sum, const int& target)
-    {
-		// Base condition
-        if(row == m)
-        {
-            return abs(sum - target);
-        }
-        
-		// Check if already calculated
-        if(dp[row][sum] != -1) return dp[row][sum];
-        
-        int minDiff = INT_MAX;
-        for(int i=0; i<n; i++)
-        {
-            minDiff = min(minDiff, solve(mat, row+1, sum+mat[row][i], target));
-            if(minDiff == 0) break;
-        }
-        return dp[row][sum] = minDiff;
-    }
-    
     int minimizeTheDifference(vector<vector<int>>& mat, int target) {
-        m = mat.size();
-        n = mat[0].size();
-        memset(dp, -1, sizeof(dp));
-        return solve(mat, 0, 0, target);;
+        bitset<5000> p(1);
+        for (auto& r : mat) {
+            bitset<5000> tmp;
+            for (auto& i : r) {
+                tmp = tmp | (p << i);
+            }
+            swap(p,tmp);
+        }
+        int res = 10000;
+        for (int i = 0; i < 5000; ++i) {
+            if (p[i]) res = min(res, abs(i - target));
+        }
+        return res;
     }
 };
