bdcdbb24655eee86f0daeb885e996dba64301b19
Anand Tripathi <shadarsh23@gmail.com>
2022-07-12 19:06:54 +0530

    Time: 103 ms (30.05%), Space: 11.5 MB (91.45%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int minFlips(string s) {
-        int n = s.size();
-        s += s;
-        string s1, s2;
+        int n=s.size();
+        vector<vector<int> > cnt(2, vector<int>(2, 0));
         
-        for(int i = 0; i < s.size(); i++) {
-            s1 += i % 2 ? '0' : '1';
-            s2 += i % 2 ? '1' : '0';
+        // cnt[0][0] = number of '0' in even positions
+        // cnt[0][1] = number of '0' in odd  positions
+        // cnt[1][0] = number of '1' in even positions
+        // cnt[1][1] = number of '1' in odd  positions
+        
+        for(int i=0; i<n; i++){
+            cnt[s[i]-'0'][i%2]++;
         }
-        int ans1 = 0, ans2 = 0, ans = INT_MAX;
-        for(int i = 0; i < s.size(); i++) {
-            if(s1[i] != s[i]) ++ans1;
-            if(s2[i] != s[i]) ++ans2;
-            if(i >= n) { 
-                if(s1[i - n] != s[i - n]) --ans1;
-                if(s2[i - n] != s[i - n]) --ans2;
-            }
-            if(i >= n - 1)
-                ans = min({ans1, ans2, ans});
+        int ans=cnt[1][0]+cnt[0][1]; // cost to make all '0' in even positions and all '1' in odd positions
+        ans=min(ans, cnt[0][0]+cnt[1][1]); // cost to make all '1' in even positions and all '0' in odd positions
+        for(int i=0; i<n; i++){
+            cnt[s[i]-'0'][i%2]--;
+            cnt[s[i]-'0'][(n+i)%2]++;
+            ans=min(ans, cnt[1][0]+cnt[0][1]);
+            ans=min(ans, cnt[0][0]+cnt[1][1]);
         }
         return ans;
     }
