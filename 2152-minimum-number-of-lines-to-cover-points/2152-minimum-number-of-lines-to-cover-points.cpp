bbd28411c83c7659cef3f75cef625653cc318a49
Anand Tripathi <shadarsh23@gmail.com>
2022-07-13 21:05:34 +0530

    Time: 37 ms (76.77%), Space: 11.3 MB (95.96%) - LeetHub

--- 2152-minimum-number-of-lines-to-cover-points/2152-minimum-number-of-lines-to-cover-points.cpp
+++ 2152-minimum-number-of-lines-to-cover-points/2152-minimum-number-of-lines-to-cover-points.cpp
@@ -1,9 +1,5 @@
 class Solution {
 public:
-    int gcd(int x, int y) {
-        if(y == 0) return x;
-        return gcd(y, x % y);
-    }
     
     int minimumLines(vector<vector<int>>& points) {
         if(points.size() < 3) return 1;
