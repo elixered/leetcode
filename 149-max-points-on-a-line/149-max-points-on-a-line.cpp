7f6ba2417b2ad7ca97a7edd1583415d92a5a80ac
Anand Tripathi <shadarsh23@gmail.com>
2022-07-13 21:58:36 +0530

    Time: 148 ms (25.65%), Space: 48 MB (5.00%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -5,20 +5,17 @@ public:
         if(n<3) return n;
         int ans = 0;
         for(int i=0; i<n; ++i){
-            for(int j=i+1; j<n; ++j){
+            map<pair<int,int>,set<int>> mp;
+            for(int j=0; j<n; ++j){
+                if(i==j) continue;
                 int dx = points[j][0] - points[i][0];
                 int dy = points[j][1] - points[i][1];
                 int dz = gcd(dx,dy);
-                int cnt = 2;
-                for(int k=0; k<n; ++k){
-                    if(i==k or j==k) continue;
-                    int x = points[k][0] - points[i][0];
-                    int y = points[k][1] - points[i][1];
-                    int z = gcd(x,y);
-                    if(dx/dz == x/z && dy/dz == y/z)
-                        cnt++;
-                }
-                ans = max(ans,cnt);
+                dx /= dz;
+                dy /= dz;
+                mp[{dx,dy}].insert(i);
+                mp[{dx,dy}].insert(j);
+                ans = max(ans,(int)mp[{dx,dy}].size());
             }
         }
         return ans;
