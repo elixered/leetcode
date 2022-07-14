9274d3d3c815b11081a69c76f4a9bb5b730c6cdb
Anand Tripathi <shadarsh23@gmail.com>
2022-07-15 02:53:35 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (28.09%) - LeetHub

--- 400-nth-digit/400-nth-digit.cpp
+++ 400-nth-digit/400-nth-digit.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int findNthDigit(int n) {
-        vector<long> digits = {0};
-        for(int i=1; i<=9; ++i){
-            long curr = pow(10,i) - pow(10,i-1);
-            digits.push_back(curr*i);
-            digits[i] += digits[i-1];
-            if(digits[i] > INT_MAX)
-                break;
+        int base = 9;
+        long digits = 1;
+        while (n - base * digits > 0) {
+            n -= base * digits;
+            base *= 10;
+            digits++;
         }
-        auto itr = upper_bound(begin(digits),end(digits),n);
-        itr--;
-        int dig = itr - begin(digits);
-        int req = (n - *itr);
-        int move = req/(dig+1);
-        int prev = pow(10,dig) + move - 1;
-        if((req)%(dig+1) == 0)
-            return prev%10;
-        int id = (req)%(dig+1)-1;
-        string s = to_string(prev+1);
-        return s[id] - '0';
+        
+        int index = (n - 1) % digits;
+        int offset = (n - 1) / digits;
+        long start = pow(10, digits - 1);
+        return to_string(start + offset)[index] - '0';
     }
 };
