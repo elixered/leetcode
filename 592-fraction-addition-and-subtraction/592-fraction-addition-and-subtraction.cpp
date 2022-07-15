ca78b1ca7e83074897fc09f30cd7d626f11a990b
Anand Tripathi <shadarsh23@gmail.com>
2022-07-15 15:14:09 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (74.51%) - LeetHub

--- 592-fraction-addition-and-subtraction/592-fraction-addition-and-subtraction.cpp
+++ 592-fraction-addition-and-subtraction/592-fraction-addition-and-subtraction.cpp
@@ -1,50 +1,24 @@
 class Solution {
 public:
-    
-    pair<int,int> getFraction(string& s, int& i, int sign){
-        int num,deno=0;
-        int n = s.size();
-            int curr = 0;
-            while(isdigit(s[i]))
-                curr = curr*10 + s[i++] - '0';
-            num = sign*curr;
-            ++i;
-            curr = 0;
-            while(i<n && isdigit(s[i]))
-                curr = curr*10 + s[i++] - '0';
-            deno = curr == 0? 1 : curr;
-        return {num,deno};
+
+void add(int& a, int& b, int c, int d) {
+    int nume = a*d + c*b, deno = b*d;
+    int gd = gcd(abs(nume), abs(deno));
+    a = nume / gd;
+    b = deno / gd;
+}
+string fractionAddition(string expression) {
+    stringstream ss(expression);
+    char op;
+    int a, b, c, d;
+    ss >> a; 
+    ss >> op; 
+    ss >> b;
+    while(ss >> c) {
+        ss >> op; 
+        ss >> d;
+        add(a, b, c, d);
     }
-    
-    void add(int& num1, int& deno1, int num2, int deno2,int sign){
-        num1 = deno2*num1 + deno1*num2;
-        deno1 = deno1*deno2;
-        int x = gcd(num1,deno1);
-        num1 /= x;
-        deno1 /= x;
-    }
-    
-    string fractionAddition(string s) {
-        int num1 = 0, deno1 = 0;
-        int n = s.size();
-        int i = 0;
-        int sign = 1;
-        while(i<n){
-            if(s[i] == '+')
-                sign = 1;
-            else if(s[i] == '-')
-                sign = -1;
-            else{
-                pair<int,int> num = getFraction(s,i,sign);
-                if(i< n){ sign = s[i] == '+' ? 1 : -1;}
-                if(deno1 == 0){
-                    num1 = num.first;
-                    deno1 = num.second;
-                }
-                else add(num1,deno1,num.first,num.second,sign);
-            }
-            ++i;
-        }
-        return to_string(num1) + "/"+to_string(deno1);
-    }
+    return to_string(a) + "/" + to_string(b);
+}
 };
