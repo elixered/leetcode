5f6ca2def9e02fa2fe95c912dd644b511ad4c365
Anand Tripathi <shadarsh23@gmail.com>
2022-07-28 16:23:18 +0530

    Time: 365 ms (21.80%), Space: 52.7 MB (76.60%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.cpp
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.cpp
@@ -1,61 +1,54 @@
 class Solution {
 public:
-    bool is_valid(int x,int y,vector<vector<int> > &grid)
-    {
-        if(x<0 || y<0 || x>=grid.size() || y>=grid[0].size())
-            return 0;
-        return 1;
+    vector<int>f;
+    bool isconnect(int a,int b,int x){
+        if(x == 0){
+            if((a == 1 || a == 3 || a == 5) && (b == 1 || b == 4 || b == 6)) return true;
+            return false;
+        }
+        if((a == 2 || a == 5 || a == 6) && (b == 2 || b == 3 || b == 4)) return true;
+        return false;
     }
-    void dfs(int x,int y,vector<vector<bool> > &visited,vector<vector<int> > &grid){
-        if(visited[x][y])
-            return;
-        visited[x][y] = 1;
-        int up,down,left,right;
-        up=down=left=right=0;
-        if(grid[x][y] == 1)
-            left=right=1;
-        else if(grid[x][y] == 2)
-            up=down=1;
-        else if(grid[x][y] == 3)
-            left=down=1;
-        else if(grid[x][y] == 4)
-            down=right=1;
-        else if(grid[x][y] == 5)
-            up = left = 1;
-        else
-            right=up=1;
-        if(up)
-        {
-            int new_x=x-1,new_y=y;
-            if(is_valid(new_x,new_y,grid) && (grid[new_x][new_y]==2 || grid[new_x][new_y]==3 || grid[new_x][new_y]==4))
-               dfs(new_x,new_y,visited,grid);
-        }
-        if(down)
-        {
-            int new_x=x+1,new_y=y;
-            if(is_valid(new_x,new_y,grid) && (grid[new_x][new_y]==2 || grid[new_x][new_y]==5 || grid[new_x][new_y]==6))
-               dfs(new_x,new_y,visited,grid);
-        }
-        if(left)
-       {
-            int new_x=x,new_y=y-1;
-            if(is_valid(new_x,new_y,grid) && (grid[new_x][new_y]==1 || grid[new_x][new_y]==4 || grid[new_x][new_y]==6))
-               dfs(new_x,new_y,visited,grid);
-       }
-        if(right)
-       {
-            int new_x=x,new_y=y+1;
-            if(is_valid(new_x,new_y,grid) && (grid[new_x][new_y]==1 || grid[new_x][new_y]==3 || grid[new_x][new_y]==5))
-                dfs(new_x,new_y,visited,grid);
-       }
+    int find(int x){
+        if(f[x] != x) 
+            return f[x] = find(f[x]);
+        return x;
+    }
+    void uni(int x,int y){
+        f[find(x)] = f[y];
     }
     bool hasValidPath(vector<vector<int>>& grid) {
-        int n = grid.size();
-        if(!n)
-            return 1;
-        int m = grid[0].size();
-        vector <vector<bool> > visited(n,vector<bool>(m,0));
-        dfs(0,0,visited,grid);
-        return visited[n-1][m-1];
+        int r = grid.size(),c = grid[0].size();
+        f.resize(r*c);
+        for(int i = 0;i<r;i++){
+            for(int j = 0;j<c;j++) f[i*c+j] = i*c+j;
+        }
+        for(int i = 0;i<r;i++){
+            for(int j = 0;j<c;j++){
+                if(i>0 && j>0){
+                    if(isconnect(grid[i][j],grid[i-1][j],1)&&isconnect(grid[i][j],grid[i][j-1],0)){
+                        uni((i-1)*c+j,i*c+j-1);
+                        f[i*c+j] = find(f[(i-1)*c+j]);
+                    }
+                    else if(isconnect(grid[i][j],grid[i-1][j],1)){
+                        f[i*c+j] = find(f[(i-1)*c+j]);
+                    }
+                    else if(isconnect(grid[i][j],grid[i][j-1],0)){
+                        f[i*c+j] = find(f[i*c+j-1]);
+                    }
+                }
+                else if(i>0){
+                    if(isconnect(grid[i][j],grid[i-1][j],1)){
+                        f[i*c+j] = find(f[(i-1)*c+j]);
+                    }
+                }
+                else if(j>0){
+                    if(isconnect(grid[i][j],grid[i][j-1],0)){
+                        f[i*c+j] = find(f[i*c+j-1]);
+                    }
+                }
+            }
+        }
+        return find(0) == find(r*c-1);
     }
 };
