d3e861ff1af9248ab5d53ac6d8491881c717f35b
Anand Tripathi <shadarsh23@gmail.com>
2022-08-24 16:52:24 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (48.16%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
-    
-    bool check(vector<int>& nums, int idx, int n){
-        if(n == 0)
-            return true;
-        if(idx == nums.size() or n < 0)
-            return false;
-        return check(nums,idx+1,n) or check(nums,idx+1,n-nums[idx]);
-    }
-    
     bool checkPowersOfThree(int n) {
-       vector<int> nums;
-        int curr = 1;
-        while(curr <= 1e7){
-            nums.push_back(curr);
-            curr *= 3;
+        while(n){
+            if(n%3 == 2)
+                return false;
+            else n /= 3;
         }
-        return check(nums,0,n);
+        return true;
     }
 };
