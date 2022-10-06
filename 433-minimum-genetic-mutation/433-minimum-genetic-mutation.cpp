b2c7b3fc597b095691b5b4ed8906a28dd61afd40
Anand Tripathi <shadarsh23@gmail.com>
2022-10-06 15:14:12 +0530

    Time: 4 ms (29.77%), Space: 6.8 MB (18.99%) - LeetHub

--- 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     int minMutation(string start, string end, vector<string>& bank) {
-        queue<string> q;
-        q.push(start);
-        string s = "AGCT";
-        unordered_set<string> st(begin(bank),bank.end()), visited;
-        int steps = 0;
-        while(!q.empty()){
-            int n = q.size();
-            while(n--){
-                auto f = q.front();
-                q.pop();
-                if(f == end)
-                    return steps;
-                if(visited.find(f) == visited.end()){
-                    visited.insert(f);
-                    for(int i=0; i<8; ++i){
-                        char curr = f[i];
-                        for(int j=0; j<4; ++j){
-                            f[i] = s[j];
-                            if(st.find(f) != st.end())
-                                q.push(f);
+        unordered_set<string> dict(bank.begin(), bank.end());
+        if (!dict.count(end)) return -1;
+        unordered_set<string> bset, eset, *set1, *set2;
+        bset.insert(start), eset.insert(end);
+        int step = 0, n = start.size();
+        while (!bset.empty() and !eset.empty()) {
+            if (bset.size() <= eset.size())
+                set1 = &bset, set2 = &eset;
+            else set2 = &bset, set1 = &eset;
+            unordered_set<string> tmp;
+            step ++;
+            for (auto itr = set1->begin(); itr != set1->end(); ++itr) {
+                for (int i = 0; i < n; ++i) {
+                    string dna = *itr;
+                    for (auto g : string("ATGC")) {
+                        dna[i] = g;
+                        if (set2->count(dna)) return step;
+                        if (dict.count(dna)) {
+                            tmp.insert(dna);
+                            dict.erase(dna);
                         }
-                        f[i] = curr;
                     }
                 }
             }
-            ++steps;
+            *set1 = tmp;
         }
         return -1;
     }
