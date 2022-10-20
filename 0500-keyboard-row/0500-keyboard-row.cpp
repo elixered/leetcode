d19911cc779062c8502a51ad110f121dde8bfcc3
Anand Tripathi <shadarsh23@gmail.com>
2022-10-20 22:12:43 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (87.94%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> findWords(vector<string>& words) {
+        vector<int> dict(26);
+        vector<string> rows = {"QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"};
+        for (int i = 0; i < rows.size(); i++) {
+            for (auto c : rows[i]) dict[c-'A'] = 1 << i;
+        }
+        vector<string> res;
+        for (auto w : words) {
+            int r = 7;
+            for (char c : w) {
+                r &= dict[toupper(c)-'A'];
+                if (r == 0) break;
+            }
+            if (r) res.push_back(w);
+        }
+        return res;
+    }
+};
