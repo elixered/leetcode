54d5d5b7aef992ca9ca6ebf78bcc393e5230237a
Anand Tripathi <shadarsh23@gmail.com>
2022-10-20 20:33:13 +0530

    Time: 4 ms (49.83%), Space: 6.4 MB (59.17%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -2,27 +2,24 @@ class Solution {
 public:
     int numSteps(string s) {
         int ans = 0;
-        int n = s.size();
-        for(int i=n-1; i>0; --i){
-            if(s[i] == '0')
-                ans++;
-            else{
-                int j = i-1;
-                while(j >= 0){
-                    if(s[j] == '0'){
-                        s[j] = '1';
-                        break;
-                    }
-                    else
-                        s[j] = '0';
-                    j--;
+        int found = 0;
+        for(int i=s.size()-1; i > 0; --i){
+            if(!found){
+                if(s[i] == '0')
+                    ans++;
+                else{
+                    found = 1;
+                    ans += 2;
                 }
-                ans += 2;
+            }
+            else{
+                if(s[i] == '0'){
+                    ans += 2;
+                }
+                else
+                    ans++;
             }
         }
-        if(s[0] == '0')
-            return ans+1;
-        else
-            return ans;
+        return ans + found;
     }
 };
