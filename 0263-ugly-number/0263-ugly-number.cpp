52f3467fa20f61595fd5c063f63ad217bf0be048
Anand Tripathi <shadarsh23@gmail.com>
2022-11-18 08:18:03 +0530

    Time: 7 ms (20.99%), Space: 5.9 MB (69.56%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<=0) return false;
+        while(n!=1)
+        {
+            if(n%2==0)
+                n = n/2;
+            else if(n%3==0)
+                n = n/3;
+            else if(n%5==0)
+                n = n/5;
+            else if(n!=1)
+                return false;
+        }
+        return true;
+    }
+};
